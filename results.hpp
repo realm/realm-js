beb7c19931751c91223c0abcff3c9097127d6f86
Ari Lazier <alazier@gmail.com>
2015-09-03 15:46:31 -0700

    support sorting by a single property

--- results.hpp
+++ results.hpp
@@ -23,8 +23,19 @@
 #import <realm/table_view.hpp>
 
 namespace realm {
+    struct SortOrder {
+        std::vector<size_t> columnIndices;
+        std::vector<bool> ascending;
+
+        explicit operator bool() const {
+            return !columnIndices.empty();
+        }
+    };
+
+    static SortOrder s_defaultSort = {{}, {}};
+
     struct Results {
-        Results(SharedRealm &r, ObjectSchema &o, Query q);
+        Results(SharedRealm &r, ObjectSchema &o, Query q, SortOrder s = s_defaultSort);
         size_t size();
         Row get(std::size_t row_ndx);
         void verify_attached();
@@ -33,6 +44,9 @@ namespace realm {
         ObjectSchema &object_schema;
         Query backing_query;
         TableView table_view;
+        std::unique_ptr<SortOrder> sort_order;
+
+        void setSort(SortOrder s);
     };
 }
 
