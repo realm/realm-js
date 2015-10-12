48c9336526d1fde9dfb394a7ac31aa77fc870d77
Scott Kyle <scott@appden.com>
2015-10-12 01:59:34 -0700

    Tabs -> Spaces in ObjectTests.js

--- tests/ObjectTests.js
+++ tests/ObjectTests.js
@@ -20,67 +20,67 @@
 
 var ObjectTests = {
     testBasicTypesPropertyGetters: function() {
-    	var basicTypesValues = [true, 1, 1.1, 1.11, 'string', new Date(1), 'DATA'];
-    	var realm = new Realm({schema: [BasicTypesObjectSchema]});
-    	var object = null;
-    	realm.write(function() {
-    		object = realm.create('BasicTypesObject', basicTypesValues);
-    	});
+        var basicTypesValues = [true, 1, 1.1, 1.11, 'string', new Date(1), 'DATA'];
+        var realm = new Realm({schema: [BasicTypesObjectSchema]});
+        var object = null;
+        realm.write(function() {
+            object = realm.create('BasicTypesObject', basicTypesValues);
+        });
 
-    	for (var i = 0; i < BasicTypesObjectSchema.properties.length; i++) {
-    		var prop = BasicTypesObjectSchema.properties[i];
-    		if (prop.type == Realm.Types.FLOAT) {
-    			TestCase.assertEqualWithTolerance(object[prop.name], basicTypesValues[i], 0.000001);
-    		}
-    		else if (prop.type == Realm.Types.DATE) {
-    			TestCase.assertEqual(object[prop.name].getTime(), basicTypesValues[i].getTime());
-    		}
-    		else {
-	    		TestCase.assertEqual(object[prop.name], basicTypesValues[i]);
-	    	}
-    	}
+        for (var i = 0; i < BasicTypesObjectSchema.properties.length; i++) {
+            var prop = BasicTypesObjectSchema.properties[i];
+            if (prop.type == Realm.Types.FLOAT) {
+                TestCase.assertEqualWithTolerance(object[prop.name], basicTypesValues[i], 0.000001);
+            }
+            else if (prop.type == Realm.Types.DATE) {
+                TestCase.assertEqual(object[prop.name].getTime(), basicTypesValues[i].getTime());
+            }
+            else {
+                TestCase.assertEqual(object[prop.name], basicTypesValues[i]);
+            }
+        }
     },
     testBasicTypesPropertySetters: function() {
-    	var basicTypesValues = [true, 1, 1.1, 1.11, 'string', new Date(1), 'DATA'];
-    	var realm = new Realm({schema: [BasicTypesObjectSchema]});
-    	var obj = null;
-    	realm.write(function() {
-    		obj = realm.create('BasicTypesObject', basicTypesValues);
-    		obj.boolCol = false; 
-    		obj.intCol = 2; 
-    		obj.floatCol = 2.2;
-    		obj.doubleCol = 2.22;
-    		obj.stringCol = 'STRING';
-    		obj.dateCol = new Date(2); 
-    		obj.dataCol = 'b';
-   		});
-   		TestCase.assertEqual(obj.boolCol, false, 'wrong bool value');
-    	TestCase.assertEqual(obj.intCol, 2, 'wrong int value');
-    	TestCase.assertEqualWithTolerance(obj.floatCol, 2.2, 0.000001, 'wrong float value');
-    	TestCase.assertEqual(obj.doubleCol, 2.22, 'wrong double value');
-    	TestCase.assertEqual(obj.stringCol, 'STRING', 'wrong string value');
-    	TestCase.assertEqual(obj.dateCol.getTime(), 2, 'wrong date value');
-    	TestCase.assertEqual(obj.dataCol, 'b', 'wrong data value');
+        var basicTypesValues = [true, 1, 1.1, 1.11, 'string', new Date(1), 'DATA'];
+        var realm = new Realm({schema: [BasicTypesObjectSchema]});
+        var obj = null;
+        realm.write(function() {
+            obj = realm.create('BasicTypesObject', basicTypesValues);
+            obj.boolCol = false;
+            obj.intCol = 2;
+            obj.floatCol = 2.2;
+            obj.doubleCol = 2.22;
+            obj.stringCol = 'STRING';
+            obj.dateCol = new Date(2);
+            obj.dataCol = 'b';
+        });
+        TestCase.assertEqual(obj.boolCol, false, 'wrong bool value');
+        TestCase.assertEqual(obj.intCol, 2, 'wrong int value');
+        TestCase.assertEqualWithTolerance(obj.floatCol, 2.2, 0.000001, 'wrong float value');
+        TestCase.assertEqual(obj.doubleCol, 2.22, 'wrong double value');
+        TestCase.assertEqual(obj.stringCol, 'STRING', 'wrong string value');
+        TestCase.assertEqual(obj.dateCol.getTime(), 2, 'wrong date value');
+        TestCase.assertEqual(obj.dataCol, 'b', 'wrong data value');
     },
     testLinkTypesPropertyGetters: function() {
-    	var realm = new Realm({schema: [LinkTypesObjectSchema, TestObjectSchema]});
-    	var obj = null;
-    	realm.write(function() {
-    		obj = realm.create('LinkTypesObject', [[1], null, [[3]]]);
-    	});
+        var realm = new Realm({schema: [LinkTypesObjectSchema, TestObjectSchema]});
+        var obj = null;
+        realm.write(function() {
+            obj = realm.create('LinkTypesObject', [[1], null, [[3]]]);
+        });
 
-    	var objVal = obj.objectCol;
-    	TestCase.assertEqual(typeof objVal, 'object');
-    	TestCase.assertNotEqual(objVal, null);
-    	TestCase.assertEqual(objVal.doubleCol, 1);
+        var objVal = obj.objectCol;
+        TestCase.assertEqual(typeof objVal, 'object');
+        TestCase.assertNotEqual(objVal, null);
+        TestCase.assertEqual(objVal.doubleCol, 1);
 
         TestCase.assertEqual(obj.objectCol1, null);
 
-    	var arrayVal = obj.arrayCol;
-    	TestCase.assertEqual(typeof arrayVal, 'object');
-    	TestCase.assertNotEqual(arrayVal, null);
-    	TestCase.assertEqual(arrayVal.length, 1);
-    	TestCase.assertEqual(arrayVal[0].doubleCol, 3);
+        var arrayVal = obj.arrayCol;
+        TestCase.assertEqual(typeof arrayVal, 'object');
+        TestCase.assertNotEqual(arrayVal, null);
+        TestCase.assertEqual(arrayVal.length, 1);
+        TestCase.assertEqual(arrayVal[0].doubleCol, 3);
     },
     testLinkTypesPropertySetters: function() {
         var realm = new Realm({schema: [LinkTypesObjectSchema, TestObjectSchema]});
