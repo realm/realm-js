d7f80e22c838e89b848451c50aa2999fb3cf1476
Scott Kyle <scott@appden.com>
2016-01-06 15:24:00 -0800

    Update React Tests to support React Native 0.18
    We had to shift to using an event-based architecture to communicate with the page from native code.

--- react-native/RealmReact.h
+++ react-native/RealmReact.h
@@ -5,8 +5,21 @@
 #import <Foundation/Foundation.h>
 #import <JavaScriptCore/JavaScriptCore.h>
 
-extern JSGlobalContextRef RealmReactGetJSGlobalContextForExecutor(id executor, bool create);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (^RealmReactEventHandler)(id message);
+
+JSGlobalContextRef RealmReactGetJSGlobalContextForExecutor(id executor, bool create);
 
 @interface RealmReact : NSObject
 
+- (void)addListenerForEvent:(NSString *)eventName handler:(RealmReactEventHandler)handler;
+- (void)removeListenerForEvent:(NSString *)eventName handler:(RealmReactEventHandler)handler;
+
 @end
+
+#ifdef __cplusplus
+}
+#endif
--- tests/react-test-app/ios/ReactTests/RealmReactTests.m
+++ tests/react-test-app/ios/ReactTests/RealmReactTests.m
@@ -6,11 +6,17 @@
 #import "RCTJavaScriptExecutor.h"
 #import "RCTBridge.h"
 #import "RCTDevMenu.h"
+#import "RCTEventDispatcher.h"
 
 @import RealmReact;
 
 extern void JSGlobalContextSetIncludesNativeCallStackWhenReportingExceptions(JSGlobalContextRef ctx, bool includesNativeCallStack);
 extern NSMutableArray *RCTGetModuleClasses(void);
+
+@interface RCTBridge ()
++ (instancetype)currentBridge;
+- (void)setUp;
+@end
 
 @interface RealmReactTests : RealmJSTests
 @end
@@ -26,41 +32,40 @@ extern NSMutableArray *RCTGetModuleClasses(void);
 }
 
 + (Class)executorClass {
-    return NSClassFromString(@"RCTContextExecutor");
+    return NSClassFromString(@"RCTJSCExecutor");
 }
 
 + (NSString *)classNameSuffix {
     return @"";
 }
 
-+ (id<RCTJavaScriptExecutor>)currentExecutor {
++ (RCTBridge *)currentBridge {
     Class executorClass = [self executorClass];
     if (!executorClass) {
         return nil;
     }
 
-    static RCTBridge *s_bridge;
-    if (!s_bridge) {
-        [[NSNotificationCenter defaultCenter] postNotificationName:RCTReloadNotification object:nil];
+    RCTBridge *bridge = [RCTBridge currentBridge];
+    if (!bridge.valid) {
+        [self waitForNotification:RCTJavaScriptDidLoadNotification];
+        bridge = [RCTBridge currentBridge];
     }
 
-    if (!s_bridge.valid) {
-        NSNotification *notification = [self waitForNotification:RCTJavaScriptDidLoadNotification];
-        s_bridge = notification.userInfo[@"bridge"];
-        assert(s_bridge);
+    if (bridge.executorClass != executorClass) {
+        bridge.executorClass = executorClass;
+
+        RCTBridge *parentBridge = [bridge valueForKey:@"parentBridge"];
+        [parentBridge invalidate];
+        [parentBridge setUp];
+
+        return [self currentBridge];
     }
 
-    if (s_bridge.executorClass != executorClass) {
-        s_bridge.executorClass = executorClass;
-        [s_bridge reload];
+    return bridge;
+}
 
-        // The [RCTBridge reload] method does a dispatch_async that we must run before trying again.
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-
-        return [self currentExecutor];
-    }
-
-    return [s_bridge valueForKey:@"javaScriptExecutor"];
++ (id<RCTJavaScriptExecutor>)currentExecutor {
+    return [[self currentBridge] valueForKey:@"javaScriptExecutor"];
 }
 
 + (XCTestSuite *)defaultTestSuite {
@@ -78,11 +83,9 @@ extern NSMutableArray *RCTGetModuleClasses(void);
         JSGlobalContextSetIncludesNativeCallStackWhenReportingExceptions(ctx, false);
     }
 
-    NSError *error;
-    NSDictionary *testCaseNames = [self invokeMethod:@"getTestNames" arguments:nil error:&error];
-
-    if (error || !testCaseNames.count) {
-        NSLog(@"Error from calling getTestNames() - %@", error ?: @"None returned");
+    NSDictionary *testCaseNames = [self waitForEvent:@"realm-test-names"];
+    if (!testCaseNames.count) {
+        NSLog(@"ERROR: No test names were provided by the JS");
         exit(1);
     }
 
@@ -128,34 +131,24 @@ extern NSMutableArray *RCTGetModuleClasses(void);
     }
 }
 
-+ (id)invokeMethod:(NSString *)method arguments:(NSArray *)arguments error:(NSError * __strong *)outError {
-    id<RCTJavaScriptExecutor> executor = [self currentExecutor];
++ (id)waitForEvent:(NSString *)eventName {
+    __weak RealmReact *realmModule = [[self currentBridge] moduleForClass:[RealmReact class]];
+    assert(realmModule);
 
     __block BOOL condition = NO;
     __block id result;
+    __block RealmReactEventHandler handler;
 
-    [executor executeJSCall:@"realm-tests/index.js" method:method arguments:(arguments ?: @[]) callback:^(id json, NSError *error) {
-        // The React Native debuggerWorker.js very bizarrely returns an array five empty arrays to signify an error.
-        if ([json isKindOfClass:[NSArray class]] && [json isEqualToArray:@[@[], @[], @[], @[], @[]]]) {
-            json = nil;
+    __block __weak RealmReactEventHandler weakHandler = handler = ^(id object) {
+        [realmModule removeListenerForEvent:eventName handler:weakHandler];
 
-            if (!error) {
-                error = [NSError errorWithDomain:@"JS" code:1 userInfo:@{NSLocalizedDescriptionKey: @"unknown JS error"}];
-            }
-        }
+        condition = YES;
+        result = object;
+    };
 
-        dispatch_async(dispatch_get_main_queue(), ^{
-            condition = YES;
-            result = json;
-
-            if (error && outError) {
-                *outError = error;
-            }
-        });
-    }];
+    [realmModule addListenerForEvent:eventName handler:handler];
 
     [self waitForCondition:&condition];
-
     return result;
 }
 
@@ -167,12 +160,12 @@ extern NSMutableArray *RCTGetModuleClasses(void);
         module = [module substringToIndex:(module.length - suffix.length)];
     }
 
-    NSError *error;
-    [self.class invokeMethod:@"runTest" arguments:@[module, method] error:&error];
+    RCTBridge *bridge = [self.class currentBridge];
+    [bridge.eventDispatcher sendAppEventWithName:@"realm-run-test" body:@{@"suite": module, @"name": method}];
 
+    id error = [self.class waitForEvent:@"realm-test-finished"];
     if (error) {
-        // TODO: Parse and use localizedFailureReason info once we can source map the failure location in JS.
-        [self recordFailureWithDescription:error.localizedDescription inFile:@(__FILE__) atLine:__LINE__ expected:YES];
+        [self recordFailureWithDescription:[error description] inFile:@(__FILE__) atLine:__LINE__ expected:YES];
     }
 }
 
