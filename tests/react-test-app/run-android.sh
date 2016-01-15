a086955a8c30b670b3e0b463fa947fbb5348eb35
Ari Lazier <alazier@gmail.com>
2016-01-15 12:34:51 -0800

    path/name fixes

--- tests/react-test-app/run-android.sh
+++ tests/react-test-app/run-android.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf node_modules/realm node_modules/realm-tests
 npm install realm realm-tests
-cp ../../src/object-store/parser/queryTests.json node_modules/realm-tests/
+cp ../../src/object-store/parser/queryTests.json node_modules/realm-tests/query-tests.json
 adb forward tcp:8082 tcp:8082
 adb reverse tcp:8081 tcp:8081
 react-native run-android
