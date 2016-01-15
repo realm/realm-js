9edafe09ac332229d1795232db4811f412dbc25b
Ari Lazier <alazier@gmail.com>
2016-01-15 12:54:46 -0800

    example fixes

--- examples/ReactExample/run-android.sh
+++ examples/ReactExample/run-android.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp ../../react-native/android/app/build/outputs/aar/app-debug.aar android/app/lib/
-rm -rf node_modules/realm node_modules/realm-tests
+rm -rf node_modules/realm
 npm install realm
 adb forward tcp:8082 tcp:8082
 adb reverse tcp:8081 tcp:8081
