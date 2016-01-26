f957db3a3a908acb81c8158440acb73d88c1c12b
Scott Kyle <scott@appden.com>
2016-01-26 15:16:33 -0800

    Update run-android.sh scripts

--- tests/react-test-app/run-android.sh
+++ tests/react-test-app/run-android.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+set -o pipefail
+set -e
+
+PATH="/opt/android-sdk-linux/platform-tools:$PATH"
+
 rm -rf node_modules/realm node_modules/realm-tests
 npm install realm realm-tests
+
 cp ../../src/object-store/parser/queryTests.json node_modules/realm-tests/query-tests.json
-/opt/android-sdk-linux/platform-tools/adb reverse tcp:8081 tcp:8081
-/opt/android-sdk-linux/platform-tools/adb forward tcp:8082 tcp:8082
+
+adb reverse tcp:8081 tcp:8081
+adb forward tcp:8082 tcp:8082
+
 react-native run-android
