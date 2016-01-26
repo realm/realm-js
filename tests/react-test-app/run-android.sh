2eb823810128ee710d0c1614c0e8e54bb08932e3
Nabil Hachicha <nabil.hachicha@gmail.com>
2016-01-26 21:01:00 +0000

    fixing adb path

--- tests/react-test-app/run-android.sh
+++ tests/react-test-app/run-android.sh
@@ -2,6 +2,6 @@
 rm -rf node_modules/realm node_modules/realm-tests
 npm install realm realm-tests
 cp ../../src/object-store/parser/queryTests.json node_modules/realm-tests/query-tests.json
-adb reverse tcp:8081 tcp:8081
-adb forward tcp:8082 tcp:8082
+/opt/android-sdk-linux/platform-tools/adb reverse tcp:8081 tcp:8081
+/opt/android-sdk-linux/platform-tools/adb forward tcp:8082 tcp:8082
 react-native run-android
