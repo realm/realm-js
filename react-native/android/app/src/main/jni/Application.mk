dd9242e58453a3222408ee83e71cc3c7ac8583c9
Nabil Hachicha <nabil.hachicha@gmail.com>
2016-01-28 14:11:27 +0000

    cleaning NDK flags

--- react-native/android/app/src/main/jni/Application.mk
+++ react-native/android/app/src/main/jni/Application.mk
@@ -8,10 +8,12 @@ APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(THIRD_PARTY_NDK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)first-party
 
 APP_STL := gnustl_static
-APP_CFLAGS += -std=c++14
+APP_CPPFLAGS := -std=c++14
 APP_CPPFLAGS += -frtti
+APP_CPPFLAGS += -fexceptions
 
 # Make sure every shared lib includes a .note.gnu.build-id header
 APP_LDFLAGS := -Wl,--build-id
 
 NDK_TOOLCHAIN_VERSION := 4.9
+APP_LDFLAGS := -llog
