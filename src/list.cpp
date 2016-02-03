8e58fc693c90a0d5f66967acbf979af55abcfd4b
Thomas Goyne <tg@realm.io>
2016-02-03 09:28:11 -0800

    Call verify_attached() from verify_in_transaction()

--- src/list.cpp
+++ src/list.cpp
@@ -48,7 +48,6 @@ Row List::get(size_t row_ndx) const
 
 void List::set(size_t row_ndx, size_t target_row_ndx)
 {
-    verify_attached();
     verify_in_transaction();
     verify_valid_row(row_ndx);
     m_link_view->set(row_ndx, target_row_ndx);
@@ -56,14 +55,12 @@ void List::set(size_t row_ndx, size_t target_row_ndx)
 
 void List::add(size_t target_row_ndx)
 {
-    verify_attached();
     verify_in_transaction();
     m_link_view->add(target_row_ndx);
 }
 
 void List::insert(size_t row_ndx, size_t target_row_ndx)
 {
-    verify_attached();
     verify_in_transaction();
     verify_valid_row(row_ndx, true);
     m_link_view->insert(row_ndx, target_row_ndx);
@@ -71,7 +68,6 @@ void List::insert(size_t row_ndx, size_t target_row_ndx)
 
 void List::remove(size_t row_ndx)
 {
-    verify_attached();
     verify_in_transaction();
     verify_valid_row(row_ndx);
     m_link_view->remove(row_ndx);
@@ -102,6 +98,7 @@ void List::verify_attached() const
 
 void List::verify_in_transaction() const
 {
+    verify_attached();
     if (!m_realm->is_in_transaction()) {
         throw std::runtime_error("Can only mutate a list within a transaction.");
     }
