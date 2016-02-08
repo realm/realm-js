8e80733409390ec74615331307a747edb05b75bd
Scott Kyle <scott@appden.com>
2016-02-08 15:55:52 -0800

    Cleanup platform.hpp

--- src/platform.hpp
+++ src/platform.hpp
@@ -1,10 +1,8 @@
-/* Copyright 2015 Realm Inc - All Rights Reserved
+/* Copyright 2016 Realm Inc - All Rights Reserved
  * Proprietary and Confidential
  */
 
-
-#ifndef REALM_PLATFORM_HPP
-#define REALM_PLATFORM_HPP
+#pragma once
 
 #include <string>
 
@@ -13,21 +11,19 @@ extern std::string s_default_realm_directory;
 namespace realm {
 //
 // These methods are used internally and must be implemented
-// separately for eadh platform
+// separately for each platform
 //
 
-    // set the directory where realm files should be stored
-    void set_default_realm_file_directory(std::string dir);
+// set the directory where realm files should be stored
+void set_default_realm_file_directory(std::string dir);
 
-    // return the directory in which realm files can/should be written to
+// return the directory in which realm files can/should be written to
 std::string default_realm_file_directory();
 
 // create the directories for the given filename
 void ensure_directory_exists_for_file(const std::string &file);
 
-// remoave all realm files in the given directory
+// remove all realm files in the given directory
 void remove_realm_files_from_directory(const std::string &directory);
 
 }
-
-#endif /* REALM_PLATFORM_HPP */
