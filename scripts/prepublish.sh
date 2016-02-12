5f7000d8924d78e87cae80b179c87c35380dd8df
Scott Kyle <scott@appden.com>
2016-02-11 16:44:22 -0800

    Skip building Android module under Xcode

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 cd "$(dirname "$0")/.."
 
-if [ -n "$SKIP_ANDROID_BUILD" ]; then
+if [ -n "$SKIP_ANDROID_BUILD" -o -n "$XCODE_VERSION_ACTUAL" ]; then
   echo 'Skipped building Android module'
 else
   rm -rf android
