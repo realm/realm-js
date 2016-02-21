349d2272528e97ba50079627fe13ee06a949e486
Ari Lazier <alazier@gmail.com>
2016-02-21 12:16:42 -0800

    use classes in the examples

--- examples/ReactExample/components/realm.js
+++ examples/ReactExample/components/realm.js
@@ -6,21 +6,20 @@
 
 import Realm from 'realm';
 
-export default new Realm({
-    schema: [
-        {
-            name: 'Todo',
-            properties: {
-                done: {type: Realm.Types.BOOL, default: false},
-                text: Realm.Types.STRING,
-            },
-        },
-        {
-            name: 'TodoList',
-            properties: {
-                name: Realm.Types.STRING,
-                items: {type: Realm.Types.LIST, objectType: 'Todo', default: []},
-            },
-        },
-    ],
-});
+class Todo {}
+Todo.schema = {
+    name: 'Todo',
+    properties: {
+        done: {type: Realm.Types.BOOL, default: false},
+        text: Realm.Types.STRING,
+    },
+};
+class TodoList {}
+TodoList.schema = {
+    name: 'TodoList',
+    properties: {
+        name: Realm.Types.STRING,
+        items: {type: Realm.Types.LIST, objectType: 'Todo', default: []},
+    },
+};
+export default new Realm({schema: [Todo, TodoList]});
