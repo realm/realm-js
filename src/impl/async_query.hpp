tony-sd/realm-cocoa

Use a single AsyncQuery per Results regardless of number of callbacks added

Realm/ObjectStore/impl/async_query.hpp
@@ -23,50 +23,88 @@
 
 #include <realm/group_shared.hpp>
 
+#include <exception>
+#include <mutex>
 #include <functional>
+#include <thread>
+#include <vector>
 
 namespace realm {
 namespace _impl {
-class AsyncQuery : public std::enable_shared_from_this<AsyncQuery> {
+class AsyncQuery {
 public:
-    AsyncQuery(SortOrder sort,
-               std::unique_ptr<SharedGroup::Handover<Query>> handover,
-               std::unique_ptr<AsyncQueryCallback> callback,
-               RealmCoordinator& parent);
+    AsyncQuery(Results& target);
 
-    void get_results(const SharedRealm& realm, SharedGroup& sg, std::vector<std::function<void()>>& ret);
+    AsyncQueryCancelationToken add_callback(std::function<void (std::exception_ptr)>);
+    void remove_callback(size_t token);
 
-    void set_error(std::exception_ptr err);
+    void unregister() noexcept;
 
     // Run/rerun the query if needed
-    void prepare_update();
-    // Update the handover object with the new data produced in prepare_update()
+    void run();
+    // Prepare the handover object if run() did update the TableView
     void prepare_handover();
+    // Update the target results from the handover and call callbacks
+    void deliver(SharedGroup& sg, std::exception_ptr err);
 
-    // Get the version of the current handover object
-    SharedGroup::VersionID version() const noexcept;
-
+    // Attach the handed-over query to `sg`
     void attach_to(SharedGroup& sg);
+    // Create a new query handover object and stop using the previously attached
+    // SharedGroup
     void detatch();
 
-    std::shared_ptr<RealmCoordinator> parent;
+    Realm& get_realm() { return m_target_results->get_realm(); }
+    // Get the version of the current handover object
+    SharedGroup::VersionID version() const noexcept { return m_version; }
 
 private:
-    const SortOrder m_sort;
+    // Target Results to update and a mutex which guards it
+    std::mutex m_target_mutex;
+    Results* m_target_results;
 
+    std::shared_ptr<Realm> m_realm;
+    const SortOrder m_sort;
+    const std::thread::id m_thread_id = std::this_thread::get_id();
+
+    // The source Query, in handover from iff m_sg is null
+    // Only used until the first time the query is actually run, after which
+    // both will be null
     std::unique_ptr<SharedGroup::Handover<Query>> m_query_handover;
     std::unique_ptr<Query> m_query;
 
-    std::unique_ptr<SharedGroup::Handover<TableView>> m_tv_handover;
+    // The TableView resulting from running the query. Will be detached if the
+    // Query has not yet been run, in which case m_query or m_query_handover will
+    // be non-null
     TableView m_tv;
+    std::unique_ptr<SharedGroup::Handover<TableView>> m_tv_handover;
+    SharedGroup::VersionID m_version;
 
-    const std::unique_ptr<AsyncQueryCallback> m_callback;
+    struct Callback {
+        std::function<void (std::exception_ptr)> fn;
+        std::unique_ptr<SharedGroup::Handover<TableView>> handover;
+        size_t token;
+        bool first_run;
+    };
+
+    // Currently registered callbacks and a mutex which must always be held
+    // while doing anything with them
+    std::mutex m_callback_mutex;
+    std::vector<Callback> m_callbacks;
+
+    // Callbacks which the user has asked to have removed whose removal has been
+    // deferred until after we're done looping over m_callbacks
+    std::vector<size_t> m_callbacks_to_remove;
 
     SharedGroup* m_sg = nullptr;
 
-    std::exception_ptr m_error;
-
     bool m_did_update = false;
+    bool m_initial_run_complete = false;
+    bool m_calling_callbacks = false;
+    bool m_error = false;
+
+    void do_remove_callback(size_t token) noexcept;
+
+    bool is_for_current_thread() const { return m_thread_id == std::this_thread::get_id(); }
 };
 
 } // namespace _impl

Realm/ObjectStore/results.cpp
@@ -18,6 +18,7 @@
 
 #include "results.hpp"
 
+#include "async_query.hpp"
 #include "object_store.hpp"
 #include "realm_coordinator.hpp"
 
@@ -60,6 +61,13 @@ Results::Results(SharedRealm r, SortOrder s, TableView tv)
 , m_sort(std::move(s))
 , m_mode(Mode::TableView)
 {
+}
+
+Results::~Results()
+{
+    if (m_background_query) {
+        m_background_query->unregister();
+    }
 }
 
 void Results::validate_read() const
@@ -340,36 +348,59 @@ Results Results::filter(Query&& q) const
     return Results(m_realm, get_query().and_query(std::move(q)), get_sort());
 }
 
-Results::UnsupportedColumnTypeException::UnsupportedColumnTypeException(size_t column, const Table* table) {
+AsyncQueryCancelationToken Results::async(std::function<void (std::exception_ptr)> target)
+{
+    if (m_realm->config().read_only) {
+        throw InvalidTransactionException("Cannot create asynchronous query for read-only Realms");
+    }
+    if (m_realm->is_in_transaction()) {
+        throw InvalidTransactionException("Cannot create asynchronous query while in a write transaction");
+    }
+
+    if (!m_background_query) {
+        m_background_query = std::make_shared<_impl::AsyncQuery>(*this);
+        _impl::RealmCoordinator::register_query(m_background_query);
+    }
+    return m_background_query->add_callback(std::move(target));
+}
+
+void Results::AsyncFriend::set_table_view(Results& results, realm::TableView &&tv)
+{
+    results.m_table_view = std::move(tv);
+    results.m_mode = Mode::TableView;
+    // needs https://github.com/realm/realm-core/pull/1392
+//    REALM_ASSERT(results.m_table_view.is_in_sync());
+}
+
+Results::UnsupportedColumnTypeException::UnsupportedColumnTypeException(size_t column, const Table* table)
+{
     column_index = column;
     column_name = table->get_column_name(column);
     column_type = table->get_column_type(column);
 }
 
-AsyncQueryCancelationToken Results::async(std::unique_ptr<AsyncQueryCallback> target)
-{
-    return _impl::RealmCoordinator::register_query(*this, std::move(target));
-}
-
 AsyncQueryCancelationToken::~AsyncQueryCancelationToken()
 {
-    if (m_registration) {
-        _impl::RealmCoordinator::unregister_query(*m_registration);
+    if (m_query) {
+        m_query->remove_callback(m_token);
     }
 }
 
 AsyncQueryCancelationToken::AsyncQueryCancelationToken(AsyncQueryCancelationToken&& rgt)
-: m_registration(std::move(rgt.m_registration))
+: m_query(rgt.m_query), m_token(rgt.m_token)
 {
+    rgt.m_query = nullptr;
 }
 
 AsyncQueryCancelationToken& AsyncQueryCancelationToken::operator=(realm::AsyncQueryCancelationToken&& rgt)
 {
     if (this != &rgt) {
-        if (m_registration) {
-            _impl::RealmCoordinator::unregister_query(*m_registration);
+        if (m_query) {
+            m_query->remove_callback(m_token);
         }
-        m_registration = std::move(rgt.m_registration);
+        m_query = std::move(rgt.m_query);
+        m_token = rgt.m_token;
+        rgt.m_query = nullptr;
     }
     return *this;
 }

realm/realm-js

src/impl/async_query.hpp
@@ -23,50 +23,88 @@
 
 #include <realm/group_shared.hpp>
 
+#include <exception>
+#include <mutex>
 #include <functional>
+#include <thread>
+#include <vector>
 
 namespace realm {
 namespace _impl {
-class AsyncQuery : public std::enable_shared_from_this<AsyncQuery> {
+class AsyncQuery {
 public:
-    AsyncQuery(SortOrder sort,
-               std::unique_ptr<SharedGroup::Handover<Query>> handover,
-               std::unique_ptr<AsyncQueryCallback> callback,
-               RealmCoordinator& parent);
+    AsyncQuery(Results& target);
 
-    void get_results(const SharedRealm& realm, SharedGroup& sg, std::vector<std::function<void()>>& ret);
+    size_t add_callback(std::function<void (std::exception_ptr)>);
+    void remove_callback(size_t token);
 
-    void set_error(std::exception_ptr err);
+    void unregister() noexcept;
 
     // Run/rerun the query if needed
-    void prepare_update();
-    // Update the handover object with the new data produced in prepare_update()
+    void run();
+    // Prepare the handover object if run() did update the TableView
     void prepare_handover();
+    // Update the target results from the handover and call callbacks
+    void deliver(SharedGroup& sg, std::exception_ptr err);
 
-    // Get the version of the current handover object
-    SharedGroup::VersionID version() const noexcept;
-
+    // Attach the handed-over query to `sg`
     void attach_to(SharedGroup& sg);
+    // Create a new query handover object and stop using the previously attached
+    // SharedGroup
     void detatch();
 
-    std::shared_ptr<RealmCoordinator> parent;
+    Realm& get_realm() { return *m_target_results->get_realm(); }
+    // Get the version of the current handover object
+    SharedGroup::VersionID version() const noexcept { return m_version; }
 
 private:
-    const SortOrder m_sort;
+    // Target Results to update and a mutex which guards it
+    std::mutex m_target_mutex;
+    Results* m_target_results;
 
+    std::shared_ptr<Realm> m_realm;
+    const SortOrder m_sort;
+    const std::thread::id m_thread_id = std::this_thread::get_id();
+
+    // The source Query, in handover from iff m_sg is null
+    // Only used until the first time the query is actually run, after which
+    // both will be null
     std::unique_ptr<SharedGroup::Handover<Query>> m_query_handover;
     std::unique_ptr<Query> m_query;
 
-    std::unique_ptr<SharedGroup::Handover<TableView>> m_tv_handover;
+    // The TableView resulting from running the query. Will be detached if the
+    // Query has not yet been run, in which case m_query or m_query_handover will
+    // be non-null
     TableView m_tv;
+    std::unique_ptr<SharedGroup::Handover<TableView>> m_tv_handover;
+    SharedGroup::VersionID m_version;
 
-    const std::unique_ptr<AsyncQueryCallback> m_callback;
+    struct Callback {
+        std::function<void (std::exception_ptr)> fn;
+        std::unique_ptr<SharedGroup::Handover<TableView>> handover;
+        size_t token;
+        bool first_run;
+    };
+
+    // Currently registered callbacks and a mutex which must always be held
+    // while doing anything with them
+    std::mutex m_callback_mutex;
+    std::vector<Callback> m_callbacks;
+
+    // Callbacks which the user has asked to have removed whose removal has been
+    // deferred until after we're done looping over m_callbacks
+    std::vector<size_t> m_callbacks_to_remove;
 
     SharedGroup* m_sg = nullptr;
 
-    std::exception_ptr m_error;
-
     bool m_did_update = false;
+    bool m_initial_run_complete = false;
+    bool m_calling_callbacks = false;
+    bool m_error = false;
+
+    void do_remove_callback(size_t token) noexcept;
+
+    bool is_for_current_thread() const { return m_thread_id == std::this_thread::get_id(); }
 };
 
 } // namespace _impl

src/results.cpp
@@ -18,8 +18,9 @@
 
 #include "results.hpp"
 
-#include "object_store.hpp"
+#include "impl/async_query.hpp"
 #include "impl/realm_coordinator.hpp"
+#include "object_store.hpp"
 
 #include <stdexcept>
 
@@ -53,6 +54,13 @@ Results::Results(SharedRealm r, const ObjectSchema &o, Table& table)
 , m_table(&table)
 , m_mode(Mode::Table)
 {
+}
+
+Results::~Results()
+{
+    if (m_background_query) {
+        m_background_query->unregister();
+    }
 }
 
 void Results::validate_read() const
@@ -344,6 +352,29 @@ Results Results::filter(Query&& q) const
     return Results(m_realm, get_object_schema(), get_query().and_query(std::move(q)), get_sort());
 }
 
+AsyncQueryCancelationToken Results::async(std::function<void (std::exception_ptr)> target)
+{
+    if (m_realm->config().read_only) {
+        throw InvalidTransactionException("Cannot create asynchronous query for read-only Realms");
+    }
+    if (m_realm->is_in_transaction()) {
+        throw InvalidTransactionException("Cannot create asynchronous query while in a write transaction");
+    }
+
+    if (!m_background_query) {
+        m_background_query = std::make_shared<_impl::AsyncQuery>(*this);
+        _impl::RealmCoordinator::register_query(m_background_query);
+    }
+    return {m_background_query, m_background_query->add_callback(std::move(target))};
+}
+
+void Results::AsyncFriend::set_table_view(Results& results, realm::TableView &&tv)
+{
+    results.m_table_view = std::move(tv);
+    results.m_mode = Mode::TableView;
+    REALM_ASSERT(results.m_table_view.is_in_sync());
+}
+
 Results::UnsupportedColumnTypeException::UnsupportedColumnTypeException(size_t column, const Table* table)
 : std::runtime_error((std::string)"Operation not supported on '" + table->get_column_name(column).data() + "' columns")
 , column_index(column)
@@ -352,30 +383,37 @@ Results::UnsupportedColumnTypeException::UnsupportedColumnTypeException(size_t c
 {
 }
 
-AsyncQueryCancelationToken Results::async(std::unique_ptr<AsyncQueryCallback> target)
+AsyncQueryCancelationToken::AsyncQueryCancelationToken(std::shared_ptr<_impl::AsyncQuery> query, size_t token)
+: m_query(std::move(query)), m_token(token)
 {
-    return _impl::RealmCoordinator::register_query(*this, std::move(target));
 }
 
 AsyncQueryCancelationToken::~AsyncQueryCancelationToken()
 {
-    if (m_registration) {
-        _impl::RealmCoordinator::unregister_query(*m_registration);
+    // m_query itself (and not just the pointed-to thing) needs to be accessed
+    // atomically to ensure that there are no data races when the token is
+    // destroyed after being modified on a different thread.
+    // This is needed despite the token not being thread-safe in general as
+    // users find it very surpringing for obj-c objects to care about what
+    // thread they are deallocated on.
+    if (auto query = std::atomic_load(&m_query)) {
+        query->remove_callback(m_token);
     }
 }
 
 AsyncQueryCancelationToken::AsyncQueryCancelationToken(AsyncQueryCancelationToken&& rgt)
-: m_registration(std::move(rgt.m_registration))
+: m_query(std::atomic_exchange(&rgt.m_query, {})), m_token(rgt.m_token)
 {
 }
 
 AsyncQueryCancelationToken& AsyncQueryCancelationToken::operator=(realm::AsyncQueryCancelationToken&& rgt)
 {
     if (this != &rgt) {
-        if (m_registration) {
-            _impl::RealmCoordinator::unregister_query(*m_registration);
+        if (auto query = std::atomic_load(&m_query)) {
+            query->remove_callback(m_token);
         }
-        m_registration = std::move(rgt.m_registration);
+        std::atomic_store(&m_query, std::atomic_exchange(&rgt.m_query, {}));
+        m_token = rgt.m_token;
     }
     return *this;
 }