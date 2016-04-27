ca87f5a3e0a4576979ce34c3a155fc08a6621d81
Scott Kyle <scott@appden.com>
2016-04-27 10:42:42 -0700

    Fix Node crash from calling function with empty this

--- src/node/node_function.hpp
+++ src/node/node_function.hpp
@@ -26,7 +26,9 @@ namespace js {
 template<>
 inline v8::Local<v8::Value> node::Function::call(v8::Isolate* isolate, const v8::Local<v8::Function> &function, const v8::Local<v8::Object> &this_object, size_t argc, const v8::Local<v8::Value> arguments[]) {
     Nan::TryCatch trycatch;
-    auto result = Nan::Call(function, this_object, (int)argc, const_cast<v8::Local<v8::Value>*>(arguments));
+
+    auto recv = this_object.IsEmpty() ? isolate->GetCurrentContext()->Global() : this_object;
+    auto result = Nan::Call(function, recv, (int)argc, const_cast<v8::Local<v8::Value>*>(arguments));
 
     if (trycatch.HasCaught()) {
         throw node::Exception(isolate, trycatch.Exception());
