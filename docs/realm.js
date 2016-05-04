9fff367ce05eef63e4f1ef472632af7f13d867c5
Scott Kyle <scott@appden.com>
2016-05-04 11:40:06 -0700

    Reword close() API doc based on PR feedback

--- docs/realm.js
+++ docs/realm.js
@@ -71,7 +71,7 @@ class Realm {
 
     /**
      * Closes this Realm so it may be re-opened with a newer schema version.
-     * All objects and collections in this Realm will no longer be valid until it is re-opened.
+     * All objects and collections from this Realm are no longer valid after calling this method.
      */
     close() {}
 
