4abc0173338f38f824a9059e6072bb08850ecf05
kristiandupont <kristian@kristiandupont.com>
2017-01-31 12:28:33 +0100

    Run async-tests in node

--- tests/js/index.js
+++ tests/js/index.js
@@ -27,12 +27,17 @@ var TESTS = {
     ResultsTests: require('./results-tests'),
     QueryTests: require('./query-tests'),
     EncryptionTests: require('./encryption-tests'),
-    MigrationTests: require('./migration-tests'),
-    /*AsyncTests: require('./async-tests')*/
+    MigrationTests: require('./migration-tests')
 };
 
+// If sync is enabled, run the user tests
 if (Realm.Sync) {
     TESTS.UserTests = require('./user-tests');
+}
+
+// If on node, run the async tests
+if (typeof process === 'object' && process + '' === '[object process]') {
+    TESTS.AsyncTests = require('./async-tests');
 }
 
 var SPECIAL_METHODS = {
