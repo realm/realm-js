d23d60f2726e2cc845d60061ac5711e1e2973955
blagoev <lubo@blagoev.com>
2017-03-22 14:01:09 +0200

    refactor test.sh to use npm commands fixed license-check npm command added test-runners npm command

--- tests/test-runners/ava/test.js
+++ tests/test-runners/ava/test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 
 test('can require Realm', t => {
-    var realm = require('realm');
+    var realm = require('../../../../realm-js');
     t.is('function', typeof realm);
     t.is('Realm', realm.name);
 });
--- tests/test-runners/mocha/test.js
+++ tests/test-runners/mocha/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 describe('Realm', function() {
    it('should be requirable', function() {
-       var realm = require('realm');
+       var realm = require('../../../../realm-js');
        assert.equal(typeof realm, 'function');
        assert.equal(realm.name, 'Realm');
    });
