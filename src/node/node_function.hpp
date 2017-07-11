fe756bdc8666e4baf46901e27d7fb1880209dba6
Yavor Georgiev <yavor.georgiev@outlook.com>
2017-07-11 13:17:41 +0200

    Rethrow callback errors as fatal Node.js errors
    Instead of rethrowing the JavaScript error emitted from a callback function, we should instead pass it to `node::FatalException`.
    
    Callbacks are employed when there is no other JavaScript frame on the stack prior so rethrowing the JS error as a C++ exception is not going to propagate the error back to JavaScript. `node::FatalException` will raise the `uncaughtError` event on the `process` object , print the error and stacktrace, and alert the debugger is there is one attached. This would make our async callbacks behave consistently with Node’s own async callbacks such as `setTimeout` when encountering an error.

--- src/node/node_function.hpp
+++ src/node/node_function.hpp
@@ -38,13 +38,13 @@ inline v8::Local<v8::Value> node::Function::call(v8::Isolate* isolate, const v8:
 
 template<>
 inline v8::Local<v8::Value> node::Function::callback(v8::Isolate* isolate, const v8::Local<v8::Function> &function, const v8::Local<v8::Object> &this_object, size_t argc, const v8::Local<v8::Value> arguments[]) {
-    Nan::TryCatch trycatch;
+    v8::TryCatch trycatch(isolate);
     
     auto recv = this_object.IsEmpty() ? isolate->GetCurrentContext()->Global() : this_object;
-    auto result = Nan::MakeCallback(recv, function, (int)argc, const_cast<v8::Local<v8::Value>*>(arguments));
+    auto result = ::node::MakeCallback(isolate, recv, function, (int)argc, const_cast<v8::Local<v8::Value>*>(arguments));
 
     if (trycatch.HasCaught()) {
-        throw node::Exception(isolate, trycatch.Exception());
+        ::node::FatalException(isolate, trycatch);
     }
     return result;
 }
