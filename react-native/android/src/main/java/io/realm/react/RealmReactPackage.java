7f10891f33335ddadbec1e530a3c330f94ab4b11
zphhhhh <zphhhhh@gmail.com>
2017-08-06 16:02:59 +0800

    Remove createJSModules @overide marker - RN 0.47 compatibility

--- react-native/android/src/main/java/io/realm/react/RealmReactPackage.java
+++ react-native/android/src/main/java/io/realm/react/RealmReactPackage.java
@@ -16,11 +16,6 @@ public class RealmReactPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
