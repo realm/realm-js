c70c5e32c0106bd96ea9da30eb12f9b1cdcc5c02
kristiandupont <kristian@kristiandupont.com>
2017-08-25 15:00:54 +0200

    Streamline permission tests

--- tests/js/permission-tests.js
+++ tests/js/permission-tests.js
@@ -29,12 +29,6 @@ function uuid() {
   });
 }
 
-function failOnError(error) {
-  if (error) {
-    throw new Error(`Error ${error} was not expected`);
-  }
-}
-
 function createUsersWithTestRealms(count) {
   const createUserWithTestRealm = username => new Promise((resolve, reject) => {
     Realm.Sync.User.register('http://localhost:9080', username, 'password', (error, user) => {
@@ -55,118 +49,66 @@ function createUsersWithTestRealms(count) {
   return Promise.all(userPromises);
 }
 
+function wait(t) {
+   return new Promise(resolve => setTimeout(resolve, t));
+}
 
 module.exports = {
     testApplyAndGetGrantedPermissions() {
       return createUsersWithTestRealms(1)
         .then(([user]) => {
           return user.applyPermissions({ userId: '*' }, `/${user.identity}/test`, 'Read')
-            .then((result) => {
-              return user.getGrantedPermissions('Any')
-                .then(permissions => {
-                  TestCase.assertEqual(permissions[1].path, `/${user.identity}/test`);
-                  TestCase.assertEqual(permissions[1].mayRead, true);
-                  TestCase.assertEqual(permissions[1].mayWrite, false);
-                  TestCase.assertEqual(permissions[1].mayManage, false);
-                })
+            .then(() => user.getGrantedPermissions('Any'))
+            .then(permissions => {
+              TestCase.assertEqual(permissions[1].path, `/${user.identity}/test`);
+              TestCase.assertEqual(permissions[1].mayRead, true);
+              TestCase.assertEqual(permissions[1].mayWrite, false);
+              TestCase.assertEqual(permissions[1].mayManage, false);
             });
         });
     },
 
-    _testApplyAndGetGrantedPermissions() {
-      var username = uuid();
-      // Create user, logout the new user, then login
-      return new Promise((resolve, reject) => {
-        Realm.Sync.User.register('http://localhost:9080', username, 'password', (error, user) => {
-          failOnError(error);
-          const r = new Realm({sync: {user, url: 'realm://localhost:9080/~/test'}})
-          r.close();
-
-          user.applyPermissions({ userId: '*' }, `/${user.identity}/test`, 'Read')
-            .then((result) => {
-              return user.getGrantedPermissions('Any')
+    testOfferPermissions() {
+      return createUsersWithTestRealms(2)
+        .then(([user1, user2]) => {
+          return user1.offerPermissions(`/${user1.identity}/test`, 'Read')
+            .then(token => user2.acceptPermissionOffer(token))
+            .then(realmUrl => {
+              TestCase.assertEqual(realmUrl, `/${user1.identity}/test`);
+              return user2.getGrantedPermissions('Any')
                 .then(permissions => {
-                  TestCase.assertEqual(permissions[1].path, `/${user.identity}/test`);
+                  TestCase.assertEqual(permissions[1].path, `/${user1.identity}/test`);
                   TestCase.assertEqual(permissions[1].mayRead, true);
                   TestCase.assertEqual(permissions[1].mayWrite, false);
                   TestCase.assertEqual(permissions[1].mayManage, false);
-                  resolve();
-                })
-            })
-            .catch(reject);
+                });
+            });
         });
-      });
-    },
-
-    testOfferPermissions() {
-      var username1 = uuid();
-      var username2 = uuid();
-      // Create user, logout the new user, then login
-      return new Promise((resolve, reject) => {
-        Realm.Sync.User.register('http://localhost:9080', username1, 'password', (error, user1) => {
-          failOnError(error);
-          const r = new Realm({ sync: { user: user1, url: 'realm://localhost:9080/~/test' } })
-          r.close();
-
-          Realm.Sync.User.register('http://localhost:9080', username2, 'password', (error, user2) => {
-            user1.offerPermissions(`/${user1.identity}/test`, 'Read')
-              .then((token) => {
-                return user2.acceptPermissionOffer(token)
-                  .then(realmUrl => {
-                    TestCase.assertEqual(realmUrl, `/${user1.identity}/test`);
-                    return user2.getGrantedPermissions('Any')
-                      .then(permissions => {
-                        TestCase.assertEqual(permissions[0].path, `/${user1.identity}/test`);
-                        TestCase.assertEqual(permissions[0].mayRead, true);
-                        TestCase.assertEqual(permissions[0].mayWrite, false);
-                        TestCase.assertEqual(permissions[0].mayManage, false);
-                        resolve();
-                      });
-                  });
-              })
-              .catch(reject);
-          });
-        });
-      });
     },
     
     testInvalidatePermissionOffer() {
-      var username1 = uuid();
-      var username2 = uuid();
-      // Create user, logout the new user, then login
-      return new Promise((resolve, reject) => {
-        Realm.Sync.User.register('http://localhost:9080', username1, 'password', (error, user1) => {
-          failOnError(error);
-          const r = new Realm({ sync: { user: user1, url: 'realm://localhost:9080/~/test' } })
-          r.close();
-
-          Realm.Sync.User.register('http://localhost:9080', username2, 'password', (error, user2) => {
+      return createUsersWithTestRealms(2)
+        .then(([user1, user2]) => {
             user1.offerPermissions(`/${user1.identity}/test`, 'Read')
               .then((token) => {
                 return user1.invalidatePermissionOffer(token)
-                  .then(() => {
                     // Since we don't yet support notification when the invalidation has gone through,
                     // wait for a bit and hope the server is done processing.
-                    setTimeout(() => {
-                      user2.acceptPermissionOffer(token)
-                        .then(() => reject("User was able to accept an invalid permission offer token"))
-                        .catch(error => {
-                          try {
-                            TestCase.assertEqual(error.message, 'The permission offer is expired.');
-                            TestCase.assertEqual(error.statusCode, 701);
-                          }
-                          catch (e) {
-                            reject(e);
-                          }
-                          resolve();
-                        });
-                    }, 100);
-                  })
-              })
-              .catch(reject);
+                  .then(wait(100))
+                  .then(user2.acceptPermissionOffer(token))
+                  // We want the call to fail, i.e. the catch() below should be called.
+                  .then(() => { throw new Error("User was able to accept an invalid permission offer token"); })
+                  .catch(error => {
+                    try {
+                      TestCase.assertEqual(error.message, 'The permission offer is expired.');
+                      TestCase.assertEqual(error.statusCode, 701);
+                    }
+                    catch (e) {
+                      throw new Error(e);
+                    }
+                  });
+              });
           });
-        });
-      });
     },
 }
 
