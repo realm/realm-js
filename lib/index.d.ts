115a2c99023a30060145f88040d1c1a2a25c29fb
blagoev <lubo@blagoev.com>
2017-09-12 20:38:43 +0300

    add deprecated warnings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -264,12 +264,25 @@ declare namespace Realm.Sync {
         readonly server: string;
         readonly token: string;
         static adminUser(adminToken: string, server?: string): User;
-        static login(server: string, username: string, password: string): Promise<Realm.Sync.User>;
+              
+        /**
+         * @deprecated, to be removed in future versions
+         */
         static login(server: string, username: string, password: string, callback: (error: any, user: User) => void): void;
-        static register(server: string, username: string, password: string): Promise<Realm.Sync.User>;
+        static login(server: string, username: string, password: string): Promise<Realm.Sync.User>;
+       
+        /**
+         * @deprecated, to be removed in future versions
+         */        
         static register(server: string, username: string, password: string, callback: (error: any, user: User) => void): void;
-        static registerWithProvider(server: string, options: { provider: string, providerToken: string, userInfo: any }): Promise<Realm.Sync.User>;
+        static register(server: string, username: string, password: string): Promise<Realm.Sync.User>;
+        
+        /**
+         * @deprecated, to be removed in versions
+         */        
         static registerWithProvider(server: string, options: { provider: string, providerToken: string, userInfo: any }, callback: (error: Error | null, user: User | null) => void): void;
+        static registerWithProvider(server: string, options: { provider: string, providerToken: string, userInfo: any }): Promise<Realm.Sync.User>;
+        
         logout(): void;
         openManagementRealm(): Realm;
         retrieveAccount(provider: string, username: string): Promise<Account>;
@@ -377,6 +390,7 @@ declare namespace Realm.Sync {
     function removeListener(regex: string, name: string, changeCallback: (changeEvent: ChangeEvent) => void): void;
     function setLogLevel(logLevel: 'all' | 'trace' | 'debug' | 'detail' | 'info' | 'warn' | 'error' | 'fatal' | 'off'): void;
     function setFeatureToken(token: string): void;
+    
     /**
      * @deprecated, to be removed in 2.0
      */
