aed1ea104bdd710366283921363423c75f4b5f50
Kenneth Geisshirt <geisshirt@gmail.com>
2017-12-13 10:09:26 +0100

    Adding property Realm.isClosed. (#1560)
    * Adding property Realm.isClosed.

--- docs/realm.js
+++ docs/realm.js
@@ -73,6 +73,14 @@ class Realm {
     get isInTransaction() {}
 
     /**
+     * Indicates if this Realm has been closed.
+     * @type {boolean}
+     * @readonly
+     * @since 2.1.0
+     */
+     get isClosed() {}
+
+    /**
      * Gets the sync session if this is a synced Realm
      * @type {Session}
      */
