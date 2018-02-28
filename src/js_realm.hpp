9f69fcba7199389e89b7e98bceb2091559a85415
Kenneth Geisshirt <geisshirt@gmail.com>
2018-02-28 17:14:00 +0100

    use tableview directly

--- src/js_realm.hpp
+++ src/js_realm.hpp
@@ -900,7 +900,7 @@ void RealmClass<T>::delete_all(ContextType ctx, ObjectType this_object, Argument
     for (auto objectSchema : realm->schema()) {
         auto table = ObjectStore::table_for_object_type(realm->read_group(), objectSchema.name);
         if (realm->is_partial()) {
-            realm::Results(realm, table->where()).clear();
+            table->where().find_all().clear(realm::RemoveMode::unordered);
         }
         else {
             table->clear();
