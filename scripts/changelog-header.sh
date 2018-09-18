e914472ceb52eb8b4da3552d5e8951c17bba8a10
Brian Munkholm <bmunkholm@users.noreply.github.com>
2018-09-18 14:44:13 +0200

    Update changelog-header.sh

--- scripts/changelog-header.sh
+++ scripts/changelog-header.sh
@@ -10,6 +10,7 @@ x.x.x Release notes (yyyy-MM-dd)
 * None
 
 ### Fixes
+* <How to hit and notice issue? what was the impact?> ([#????](https://github.com/realm/realm-js/issues/????), since v?.?.?)
 * None
 
 ### Compatibility
