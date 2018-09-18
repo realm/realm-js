5c04d50191729aad815e941b6270d8879142d744
Brian Munkholm <bmunkholm@users.noreply.github.com>
2018-09-18 13:46:14 +0200

    Update changelog-header.sh

--- scripts/changelog-header.sh
+++ scripts/changelog-header.sh
@@ -6,13 +6,18 @@ set -o pipefail
 CHANGELOG=$(cat <<EOF
 x.x.x Release notes (yyyy-MM-dd)
 =============================================================
-### Breaking changes
+## Enhancements
 * None
 
-### Enhancements
+### Fixes
 * None
 
-### Bugfixes
+### Compatibility
+* File format: ver. 7 (upgrades automatically from previous formats)
+* Realm Object Server: 3.11.0 or later.
+* APIs are backwards compatible with all previous release of realm in the 2.x.y series.
+ 
+ ### Internal
 * None
 
 $(cat CHANGELOG.md)
