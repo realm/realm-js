637f9361dff55addf21974340fc2a50ea39140a9
Brian Munkholm <bmunkholm@users.noreply.github.com>
2018-10-02 09:55:06 +0200

    Aligned release note template

--- scripts/changelog-header.sh
+++ scripts/changelog-header.sh
@@ -7,11 +7,11 @@ CHANGELOG=$(cat <<EOF
 x.x.x Release notes (yyyy-MM-dd)
 =============================================================
 ## Enhancements
-* None
+* None.
 
-### Fixes
+### Fixed
 * <How to hit and notice issue? what was the impact?> ([#????](https://github.com/realm/realm-js/issues/????), since v?.?.?)
-* None
+* None.
 
 ### Compatibility
 * File format: ver. 7 (upgrades automatically from previous formats)
@@ -19,7 +19,7 @@ x.x.x Release notes (yyyy-MM-dd)
 * APIs are backwards compatible with all previous release of realm in the 2.x.y series.
  
  ### Internal
-* None
+* None.
 
 $(cat CHANGELOG.md)
 EOF
