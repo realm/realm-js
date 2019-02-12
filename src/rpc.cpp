6cb74801c3617e340fce8ac5ac6bef4e1b60b66a
Thomas Goyne <tg@realm.io>
2019-02-12 11:24:05 -0800

    Fix RPC (de)serialization of User objects

--- src/rpc.cpp
+++ src/rpc.cpp
@@ -608,9 +608,6 @@ json RPCServer::serialize_json_value(JSValueRef js_value) {
     else if (jsc::Object::is_instance<js::UserClass<jsc::Types>>(m_context, js_object)) {
         auto user = *jsc::Object::get_internal<js::UserClass<jsc::Types>>(js_object);
         json user_dict {
-            {"identity", user->identity()},
-            {"token", user->refresh_token()},
-            {"server", user->server_url()},
             {"isAdmin", user->is_admin()}
         };
         return {
