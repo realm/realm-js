9147fc563393ec121e8bd39dc88ef22ff9055ab5
Kræn Hansen <kraen.hansen@realm.io>
2019-03-04 15:55:22 +0100

    Pinning the electron version for the integration tests

--- integration-tests/environments/electron/rebuild-realm.sh
+++ integration-tests/environments/electron/rebuild-realm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Determine the current electron version
-ELECTRON_VERSION=`node -e "console.log(require('electron/package.json').version)"`;
+# Keep this version of Electron up-to-date with the major version used in the package.json
+ELECTRON_VERSION="v4.0.0";
 # Rebuild the Realm package
 cd node_modules/realm;
 HOME=~/.electron-gyp npx node-pre-gyp rebuild --build-from-source --runtime=electron --target=$ELECTRON_VERSION --arch=x64 --dist-url=https://atom.io/download/electron
