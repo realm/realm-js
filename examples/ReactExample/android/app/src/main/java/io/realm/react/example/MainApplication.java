b080de0696a063ac24af05923c167dbf2495ae29
Christian Melchior <christian@ilios.dk>
2019-04-08 17:34:11 +0200

    Fix Android build + deploy (app still doesn't work). Attempt to fix iOS build

--- examples/ReactExample/android/app/src/main/java/io/realm/react/example/MainApplication.java
+++ examples/ReactExample/android/app/src/main/java/io/realm/react/example/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
+import com.masteratul.exceptionhandler.ReactNativeExceptionHandlerPackage;
 import com.github.wumke.RNExitApp.RNExitAppPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -28,6 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
+            new ReactNativeExceptionHandlerPackage(),
             new RNExitAppPackage(),
           new RealmReactPackage()
       );
