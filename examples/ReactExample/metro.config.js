d017cd88442472f2399d772f3f61578684886887
Christian Melchior <christian@ilios.dk>
2019-04-09 15:23:56 +0200

    Contemplating if this is fixed before the heat death of the universe

--- examples/ReactExample/metro.config.js
+++ examples/ReactExample/metro.config.js
@@ -5,7 +5,14 @@
  * @format
  */
 
+const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
+    resolver: {
+        // Work-around for metro bug: https://github.com/facebook/metro/issues/265#issuecomment-446636417
+        // Only works if checked out folder is named realm-js
+        blacklistRE: blacklist([/realm-js\/react-native\/.*/]),
+    },
     transformer: {
         getTransformOptions: async () => ({
             transform: {
