e4be38132054afa7c2813916f49637525bce8436
blagoev <lubo@blagoev.com>
2019-09-15 11:57:04 +0300

    Enable Realm as a NAPI module

--- src/node/node_init.cpp
+++ src/node/node_init.cpp
@@ -16,15 +16,19 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
+//NAPI: Enable NAPI Realm module convertion
+//NAPI: FIXME: tidy up when convertion is complete
+
 #include "node_init.hpp"
+#include "napi.h"
 
 #include "js_realm.hpp"
 #if REALM_ENABLE_SYNC
 #include "js_adapter.hpp"
 #endif
 
-namespace realm {
-namespace node {
+ namespace realm {
+ namespace node {
 
 static void init(v8::Local<v8::Object> exports, v8::Local<v8::Value> module,
                  v8::Local<v8::Context> context) {
@@ -34,7 +38,47 @@ static void init(v8::Local<v8::Object> exports, v8::Local<v8::Value> module,
     Nan::Set(exports, realm_constructor->GetName(), realm_constructor);
 }
 
+static void napi_init(Napi::Env env, v8::Isolate* isolate, Napi::Object exports) {
+	v8::Local<v8::Function> realm_constructor = js::RealmClass<Types>::create_constructor(isolate);
+
+	auto ctorName = realm_constructor->GetName().As<v8::String>();
+	v8::String::Utf8Value value(ctorName);
+	char* v = *value;
+	Napi::String::New(env, v);
+	//Nan::Set(exports, realm_constructor->GetName(), realm_constructor);
+	//exports.Set(Napi::String::New(env, v), Napi::Function::New(env, Method));
+}
+
+
 } // node
 } // realm
 
-NODE_MODULE_CONTEXT_AWARE(Realm, realm::node::init);
+static Napi::String Method(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  return Napi::String::New(env, "world");
+}
+
+static Napi::Object NAPI_Init(Napi::Env env, Napi::Object exports) {
+//NAPI: Enable NAPI Realm module convertion
+//NAPI: FIXME: remove when NAPI complete
+/////////////////////
+  napi_env e  =  env;
+  v8::Isolate* isolate = (v8::Isolate*)e + 3;
+  //the following two will fail or context will be null if isolate is not found at the expected location
+  auto currentIsolate = isolate->GetCurrent();
+  auto context = currentIsolate->GetCurrentContext();
+/////////////////////
+
+  realm::node::napi_init(env, currentIsolate, exports);
+  exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, Method));
+  return exports;
+}
+
+NODE_API_MODULE(hello, NAPI_Init)
+
+//NODE_MODULE_CONTEXT_AWARE(Realm, realm::node::init);
+
+
+
+
+
