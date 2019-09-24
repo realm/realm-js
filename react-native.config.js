e784087c18b5de8980c2ca43678a0b364b06b072
Sam Drew <sam-drew@outlook.com>
2019-09-24 11:20:12 +0100

    Migrate away from rnpm in package.json, use react-native.config.js instead

---
+++ react-native.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // config for a library is scoped under "dependency" key
+  dependency: {
+    platforms: {
+      ios: {
+        project: 'react-native/ios/RealmReact.xcodeproj',
+        sharedLibraries: [
+          'libc++',
+          'libz'
+        ]
+      },
+    },
+  }
+};
