d7011525358594929178ae12963eaefba83f17ba
Kræn Hansen <kraen.hansen@realm.io>
2019-11-29 16:31:05 +0100

    Explicitly creating a simulator

--- integration-tests/environments/react-native/harness/runner.js
+++ integration-tests/environments/react-native/harness/runner.js
@@ -20,6 +20,55 @@ const { MochaRemoteServer } = require("mocha-remote-server");
 
 const rn = require("./react-native-cli");
 const android = require("./android-cli");
+const xcode = require("./xcode-cli");
+
+/**
+ * Ensure the simulator is created and booted
+ */
+function ensureSimulator(deviceName, deviceTypeId) {
+    const version = xcode.xcrun("--version").stdout.trim();
+    console.log(`Using ${version}`);
+
+    // Determine if the system supports the device type id
+    const { devicetypes } = xcode.simctl.list('devicetypes');
+    const deviceType = devicetypes.find(({ identifier }) => identifier === deviceTypeId);
+    if (!deviceType) {
+        throw new Error(`System doesn't have the "${deviceTypeId}" device type`);
+    }
+
+    // Determine if the device exists and has the correct deviceTypeId and latest runtime
+    const { devices: devicesByType } = xcode.simctl.list('devices', 'available');
+    const availableDevices = [].concat(...Object.keys(devicesByType).map(type => devicesByType[type]));
+    // Filter devices, so we're only focussing on devices of the expected name
+    const devices = availableDevices.filter(({ name }) => name === deviceName);
+
+    // Delete any existing devices with the expected name
+    for (const device of devices) {
+        if (device.state !== "Shutdown") {
+            console.log(`Shutting down simulator (id = ${device.udid})`);
+            xcode.simctl.shutdown(device.udid);
+        }
+        console.log(`Deleting simulator (id = ${device.udid})`);
+        xcode.simctl.delete(device.udid);
+    }
+
+    const { runtimes } = xcode.simctl.list('runtimes', 'ios');
+    const [ runtime ] = runtimes.filter(runtime => runtime.isAvailable);
+    if (!runtime) {
+        throw new Error("No available iOS runtimes");
+    }
+    // Create the device
+    const { stdout } = xcode.simctl.create(deviceName, deviceTypeId, runtime.identifier);
+    const deviceId = stdout.trim();
+    console.log(`Created simulator device (id = ${deviceId})`);
+    // Boot up the device
+    console.log(`Booting simulator`);
+    xcode.simctl.boot(deviceId);
+    console.log(`Waiting for simulator`);
+    // Wait for "com.apple.springboard" launching, to ensure device is ready ...
+    xcode.simctl.launch(deviceName, "com.apple.springboard");
+    return deviceId;
+}
 
 async function runApp(platform, junitFilePath) {
     const mochaConfig = {};
@@ -64,8 +113,11 @@ async function runApp(platform, junitFilePath) {
         // Ask React Native to run the android app
         rn.sync("run-android", "--no-packager");
     } else if (platform === "ios") {
+        const deviceName = 'realm-js-integration-tests';
+        ensureSimulator(deviceName, 'com.apple.CoreSimulator.SimDeviceType.iPhone-11');
+        console.log("Simulator is ready 🚀");
         // Ask React Native to run the ios app
-        rn.sync("run-ios", "--no-packager");
+        rn.sync("run-ios", "--no-packager", `--simulator`, deviceName);
     } else {
         throw new Error(`Unexpected platform: '${platform}'`);
     }
---
+++ integration-tests/environments/react-native/harness/xcode-cli.js
@@ -0,0 +1,110 @@
+const cp = require("child_process");
+
+function xcrun(...args) {
+    const p = cp.spawnSync("xcrun", args, { encoding: "utf8" });
+    if (p.status !== 0) {
+        throw new Error(`Failed running "xcrun ${args.join(" ")}" (status = ${p.status}):\n${p.stderr.trim()}`);
+    } else {
+        return p;
+    }
+}
+
+function simctl(...args) {
+    return xcrun("simctl", ...args);
+}
+
+/**
+ * List available devices, device types, runtimes, or device pairs
+ * @param {string} type One of devices, devicetypes, runtimes or pairs
+ */
+function list(type, searchterm, asJson = true) {
+    const args = [ type ];
+    if (searchterm && !type) {
+        throw new Error("You must provide a type when providing a search term");
+    } else if (searchterm) {
+        args.push(searchterm);
+    }
+    if (asJson) {
+        args.push("--json");
+    }
+    const p = simctl("list", ...args);
+    if (asJson) {
+        return JSON.parse(p.stdout);
+    } else {
+        return p;
+    }
+}
+
+/**
+ * Create a new device
+ * @param {string} name The name of the new device
+ * @param {string} deviceTypeId The type of device to use (run `xcrun simctl list devicetypes` to get these)
+ * @param {string} runtimeId The id of the runtime of the new device (run `xcrun simctl list runtimes` to get these)
+ */
+function create(name, deviceTypeId, runtimeId) {
+    return simctl("create", name, deviceTypeId, runtimeId);
+}
+
+/**
+ * Delete spcified devices, unavailable devices, or all devices.
+ * @param {string} device Device to delete
+ */
+function deleteDevice(device) {
+    return simctl("delete", device);
+}
+
+/**
+ * Erase a device's contents and settings.
+ * @param {string} device Device to erase
+ */
+function erase(device) {
+    return simctl("erase", device);
+}
+
+/**
+ * Boot up a device
+ * @param {string} device Device to boot
+ */
+function boot(device) {
+    return simctl("boot", device);
+}
+
+/**
+ * Shutdown a device
+ * @param {string} device Device to shut down
+ */
+function shutdown(device) {
+    return simctl("shutdown", device);
+}
+
+/**
+ * Terminate an app running on a device
+ * @param {string} device Device on which to terminate app
+ * @param {string} appBundleIdentifier The bundle id of the app to terminate
+ */
+function terminate(device, appBundleIdentifier) {
+    return simctl("terminate", device, appBundleIdentifier);
+}
+
+/**
+ * Launch an app on the device
+ * @param {string} device Device to launch on
+ * @param {string} appBundleIdentifier The bundle id of the app to launch
+ */
+function launch(device, appBundleIdentifier) {
+    return simctl("launch", device, appBundleIdentifier);
+}
+
+module.exports = {
+    xcrun,
+    simctl: {
+        list,
+        create,
+        delete: deleteDevice,
+        erase,
+        boot,
+        shutdown,
+        terminate,
+        launch,
+    }
+};
