3dd3ab451dc218182d0144acbe88617a8ff522ff
Kræn Hansen <kraen.hansen@realm.io>
2020-04-18 00:24:13 +0200

    Exporting by the App type instead of the Realm.App

--- packages/realm-web/src/index.ts
+++ packages/realm-web/src/index.ts
@@ -30,18 +30,14 @@ export function app(id: string) {
     if (id in appCache) {
         return appCache[id];
     } else {
-        const instance = new App(id);
+        // Ensures the App has the correct constructor type signature
+        const AppConstructor: Realm.AppConstructor = App;
+        const instance = new AppConstructor(id);
         appCache[id] = instance;
         return instance;
     }
 }
 
-// Ensure the App has the correct constructor type signature
-/**
- * The constructor of MongoDB Realm App.
- */
-const AppConstructor = App as Realm.AppConstructor;
-export { AppConstructor as App };
-
+export { App };
 export { Credentials } from "./Credentials";
 export { User } from "./User";
