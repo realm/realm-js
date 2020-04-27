4718b2e23b621a1ea5ebbae3946a8692b95f51f2
Kræn Hansen <kraen.hansen@realm.io>
2020-04-27 20:31:30 +0200

    Fixed linting errors (adding headers)

--- packages/realm-web-integration-tests/src/api-key-auth-provider.test.ts
+++ packages/realm-web-integration-tests/src/api-key-auth-provider.test.ts
@@ -1,6 +1,24 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
 import { expect } from "chai";
 
-import { App, Credentials } from "realm-web";
+import { Credentials } from "realm-web";
 
 import { createApp } from "./utils";
 
@@ -13,9 +31,9 @@ describe("ApiKeyAuthProvider", () => {
         await app.logIn(credentials);
         // List all existing keys
         const keys = await app.auth.apiKey.list();
-        console.log(keys);
+        // console.log(keys);
         // Create an api key
         const key = await app.auth.apiKey.create("my-key");
-        console.log(key);
+        // console.log(key);
     });
 });
--- packages/realm-web-integration-tests/src/http-service.test.ts
+++ packages/realm-web-integration-tests/src/http-service.test.ts
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
 import { expect } from "chai";
 import { Credentials } from "realm-web";
 
@@ -17,7 +35,7 @@ describe("HTTP", () => {
     it("can send GET request", async () => {
         const http = app.services.http("custom-http-service");
         const response = await http.get(
-            "https://api.github.com/repos/realm/realm-js"
+            "https://api.github.com/repos/realm/realm-js",
         );
         expect(response.status).equals("200 OK");
         expect(response.statusCode).equals(200);
