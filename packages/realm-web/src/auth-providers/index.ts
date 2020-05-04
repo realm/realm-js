e46f70146ac348c62baff41ff0d72aa2eaef236f
Kræn Hansen <kraen.hansen@realm.io>
2020-05-04 15:51:26 +0200

    Update packages/realm-web/src/auth-providers/index.ts
    Co-authored-by: Kenneth Geisshirt <kenneth.geisshirt@mongodb.com>

--- packages/realm-web/src/auth-providers/index.ts
+++ packages/realm-web/src/auth-providers/index.ts
@@ -24,7 +24,7 @@ import { ApiKeyAuthProvider } from "./ApiKeyAuthProvider";
 // TODO: Consider if we should query the service for enabled authentication providers before creating clients.
 
 /**
- * Create an apps authentication providers.
+ * Create an app's authentication providers.
  *
  * @param transport The transport used when sending requests to the service.
  * @returns An object with interfaces to all possible authentication provider the app might have.
