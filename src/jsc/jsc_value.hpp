657503b0e1401b43ba0ce18d4ff61454d83e16b4
Kenneth Geisshirt <kenneth.geisshirt@mongodb.com>
2020-06-06 12:28:14 +0200

    JSC: Better check for instance of ObjectId and Decimal128

--- src/jsc/jsc_value.hpp
+++ src/jsc/jsc_value.hpp
@@ -41,6 +41,37 @@ static inline bool is_object_of_type(JSContextRef ctx, JSValueRef value, jsc::St
     return result;
 }
 
+// Look up and verify against Realm._bson.<type>
+static inline bool is_object_of_bson_type(JSContextRef ctx, JSValueRef value, jsc::String type) {
+    static const jsc::String realm_name = "Realm";
+    static const jsc::String bson_name = "_bson";
+
+    JSObjectRef global_object = JSContextGetGlobalObject(ctx);
+    JSValueRef exception = nullptr;
+
+    JSValueRef realm = JSObjectGetProperty(ctx, global_object, realm_name, &exception);
+    if (exception) {
+        throw jsc::Exception(ctx, exception);
+    }
+
+    JSValueRef bson = JSObjectGetProperty(ctx, jsc::Value::validated_to_object(ctx, realm), bson_name, &exception);
+    if (exception) {
+        throw jsc::Exception(ctx, exception);
+    }
+
+    JSValueRef constructor = JSObjectGetProperty(ctx, jsc::Value::validated_to_object(ctx, bson), type, &exception);
+    if (exception) {
+        throw jsc::Exception(ctx, exception);
+    }
+
+    bool result = JSValueIsInstanceOfConstructor(ctx, value, jsc::Value::validated_to_constructor(ctx, constructor), &exception);
+    if (exception) {
+        throw jsc::Exception(ctx, exception);
+    }
+
+    return result;
+}
+
 template<>
 inline const char *jsc::Value::typeof(JSContextRef ctx, const JSValueRef &value) {
     switch (JSValueGetType(ctx, value)) {
@@ -125,12 +156,14 @@ inline bool jsc::Value::is_valid(const JSValueRef &value) {
 
 template<>
 inline bool jsc::Value::is_decimal128(JSContextRef ctx, const JSValueRef &value) {
-    return JSValueIsObject(ctx, value); // FIXME: can we do better?
+    static const jsc::String type = "Decimal128";
+    return is_object_of_bson_type(ctx, value, type);
 }
 
 template<>
 inline bool jsc::Value::is_object_id(JSContextRef ctx, const JSValueRef &value) {
-    return JSValueIsObject(ctx, value); // FIXME: can we do better?
+    static const jsc::String type = "ObjectId";
+    return is_object_of_bson_type(ctx, value, type);
 }
 
 template<>
