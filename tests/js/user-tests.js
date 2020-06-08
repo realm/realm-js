dea79fb2b5a41860d2dfa3bd376c0d9a1fbffab5
James Stone <js@realm.io>
2020-06-08 15:55:53 -0700

    beef up the user tests

--- tests/js/user-tests.js
+++ tests/js/user-tests.js
@@ -83,31 +83,51 @@ function randomVerifiableEmail() {
     return `realm_tests_do_autoverify_${Utils.uuid()}_@test.com`;
 }
 
+function randomNonVerifiableEmail() {
+  // according to the custom register function, emails will not register if they don't contain "realm_tests_do_autoverify"
+  return `should-not-register-${Utils.uuid()}_@test.com`;
+}
+
+async function registerAndLogInEmailUser(app) {
+  const validEmail = randomVerifiableEmail();
+  const validPassword = "test1234567890";
+  await app.auth.emailPassword.registerEmail(validEmail, validPassword);
+  let user = await app.logIn(Realm.Credentials.emailPassword(validEmail, validPassword))
+  assertIsUser(user);
+  assertIsSameUser(user, app.currentUser());
+  return user;
+}
+
+async function logOutExistingUsers(app) {
+  let users = app.allUsers();
+  Object.keys(app.allUsers()).forEach(async id => await users[id].logOut());
+}
+
 module.exports = {
 
   // tests also logIn() and currentUser()
-  testLogout() {
+  async testLogout() {
     let app = new Realm.App(appConfig);
+    await logOutExistingUsers(app);
     let credentials = Realm.Credentials.anonymous();
 
-    return app.logIn(credentials).then(user => {
-      assertIsUser(user);
-
-      assertIsSameUser(user, app.currentUser());
-      user.logOut();
-
-      // Is now logged out.
-      TestCase.assertNull(app.currentUser());
-    });
+    let user = await app.logIn(credentials);
+    assertIsUser(user);
+    assertIsSameUser(user, app.currentUser());
+    await user.logOut();
+    // Is now logged out.
+    TestCase.assertNull(app.currentUser());
   },
 
   testEmailPasswordMissingUsername() {
-    TestCase.assertThrows(() => Realm.Credentials.emailPassword(undefined, 'password'));
+    let err = TestCase.assertThrows(() => Realm.Credentials.emailPassword(undefined, 'password'));
+    TestCase.assertEqual(err.message, "email must be of type 'string', got (undefined)");
   },
 
   testEmailPasswordMissingPassword() {
     const username = Utils.uuid();
-    TestCase.assertThrows(() => Realm.Credentials.emailPassword(username, undefined));
+    let err = TestCase.assertThrows(() => Realm.Credentials.emailPassword(username, undefined));
+    TestCase.assertEqual(err.message, "password must be of type 'string', got (undefined)");
   },
 
   testLoginNonExistingUser() {
@@ -120,15 +140,67 @@ module.exports = {
     });
   },
 
-  // testRegisterAutoVerifyEmailPassword() {
-  //   let app = new Realm.App(appConfig);
-  //   let credentials = Realm.Credentials.emailPassword(randomVerifiableEmail(), 'pass');
-  //   return app.logIn(credentials).then((user) => {
-  //     throw new Error("Login should have failed");
-  //   }).catch(err => {
-  //     TestCase.assertEqual(err.message, "invalid username/password");
-  //   });
-  // },
+  async testEmailPasswordProvider() {
+    let app = new Realm.App(appConfig);
+    let provider = app.auth.emailPassword;
+    TestCase.assertTrue(provider instanceof Realm.Auth.EmailPasswordProvider);
+  },
+
+  async testRegisterAutoVerifyEmailPassword() {
+    let app = new Realm.App(appConfig);
+    const validEmail = randomVerifiableEmail();
+    const invalidEmail = randomNonVerifiableEmail();
+    const invalidPassword = 'pass'; // too short
+    const validPassword = "password123456";
+
+    { // invalid email, invalid password
+      let credentials = Realm.Credentials.emailPassword(invalidEmail, invalidPassword);
+      let err = await TestCase.assertThrowsAsync(async() => app.logIn(credentials));
+      TestCase.assertEqual(err.message, "invalid username/password"); // this user does not exist yet
+      err = await TestCase.assertThrowsAsync(async() => app.auth.emailPassword.registerEmail(invalidEmail, invalidPassword));
+      TestCase.assertEqual(err.message, "password must be between 6 and 128 characters");
+      err = await TestCase.assertThrowsAsync(async() => app.logIn(credentials));
+      TestCase.assertEqual(err.message, "invalid username/password"); // this user did not register
+    }
+    { // invalid email, valid password
+      let credentials = Realm.Credentials.emailPassword(invalidEmail, validPassword);
+      let err = await TestCase.assertThrowsAsync(async() => app.logIn(credentials));
+      TestCase.assertEqual(err.message, "invalid username/password"); // this user does not exist yet
+      err = await TestCase.assertThrowsAsync(async() => app.auth.emailPassword.registerEmail(invalidEmail, validPassword));
+      TestCase.assertEqual(err.message, `failed to confirm user ${invalidEmail}`);
+      err = await TestCase.assertThrowsAsync(async() => app.logIn(credentials));
+      TestCase.assertEqual(err.message, "invalid username/password"); // this user did not register
+    }
+    { // valid email, invalid password
+      let credentials = Realm.Credentials.emailPassword(validEmail, invalidPassword);
+      let err = await TestCase.assertThrowsAsync(async() => app.logIn(credentials));
+      TestCase.assertEqual(err.message, "invalid username/password"); // this user does not exist yet
+      err = await TestCase.assertThrowsAsync(async() => app.auth.emailPassword.registerEmail(validEmail, invalidPassword));
+      TestCase.assertEqual(err.message, "password must be between 6 and 128 characters");
+      err = await TestCase.assertThrowsAsync(async() => app.logIn(credentials));
+      TestCase.assertEqual(err.message, "invalid username/password"); // this user did not register
+    }
+    { // valid email, valid password
+      let credentials = Realm.Credentials.emailPassword(validEmail, validPassword);
+      let err = await TestCase.assertThrowsAsync(async() => app.logIn(credentials));
+      TestCase.assertEqual(err.message, "invalid username/password"); // this user does not exist yet
+      await app.auth.emailPassword.registerEmail(validEmail, validPassword);
+      let user = await app.logIn(credentials)
+      assertIsUser(user);
+      assertIsSameUser(user, app.currentUser());
+      await user.logOut();
+    }
+  },
+
+  async testUserAPIKeyProvider() {
+    let app = new Realm.App(appConfig);
+
+    let credentials = Realm.Credentials.anonymous();
+    let user = await app.logIn(credentials);
+    let provider = user.auth.apiKeys;
+    TestCase.assertTrue(provider instanceof Realm.Auth.UserAPIKeyProvider);
+    await user.logOut();
+  },
 
   async testFunctions() {
     let app = new Realm.App(appConfig);
@@ -150,107 +222,95 @@ module.exports = {
     TestCase.assertEqual(err.message, "function not found: 'error'");
   },
 
-  testAll() {
+  async testAllWithAnonymous() {
     let app = new Realm.App(appConfig);
-    Object.keys(app.allUsers()).forEach(id => users[id].logOut()); // FIXME: we need to reset users for each test
+    await logOutExistingUsers(app);
 
-    const all = app.allUsers();
+    let all = app.allUsers();
     TestCase.assertArrayLength(Object.keys(all), 0, "Noone to begin with");
 
     let credentials = Realm.Credentials.anonymous();
-    return app.logIn(credentials).then(user1 => {
-      const all = app.allUsers();
-      TestCase.assertArrayLength(Object.keys(all), 1, "One user");
-      assertIsSameUser(all[user1.identity], user1);
+    let user1 = await app.logIn(credentials);
+    all = app.allUsers();
+    TestCase.assertArrayLength(Object.keys(all), 1, "One user");
+    assertIsSameUser(all[user1.identity], user1);
+    let user2 = await app.logIn(Realm.Credentials.anonymous());
+    all = app.allUsers();
+    TestCase.assertArrayLength(Object.keys(all), 1, "still one user");
+    // NOTE: the list of users is in latest-first order.
+    assertIsSameUser(all[user2.identity], user2);
+    assertIsSameUser(all[user1.identity], user1);
 
-      return app.logIn(Realm.Credentials.anonymous()).then(user2 => {
-        let all = app.allUsers();
-        TestCase.assertArrayLength(Object.keys(all), 1, "still one user");
-        // NOTE: the list of users is in latest-first order.
-        assertIsSameUser(all[user2.identity], user2);
-        assertIsSameUser(all[user1.identity], user1);
-
-        user2.logOut(); // logs out the shared anonymous session
-        all = app.allUsers();
-        TestCase.assertArrayLength(Object.keys(all), 0, "All gone");
-      });
-    });
+    await user2.logOut(); // logs out the shared anonymous session
+    all = app.allUsers();
+    TestCase.assertArrayLength(Object.keys(all), 0, "All gone");
   },
 
-  testCurrentWithAnonymous() {
+  async testCurrentWithAnonymous() {
     let app = new Realm.App(appConfig);
+    await logOutExistingUsers(app);
     TestCase.assertNull(app.currentUser());
 
-    let firstUser;
-    return app.logIn(Realm.Credentials.anonymous()).then(user1 => {
-      assertIsSameUser(app.currentUser(), user1);
-      firstUser = user1;
-      return app.logIn(Realm.Credentials.anonymous());
-    }).then(user2 => {
-      // the most recently logged in user is considered current
-      TestCase.assertTrue(firstUser.isLoggedIn);
-      TestCase.assertTrue(user2.isLoggedIn);
-      assertIsSameUser(app.currentUser(), user2);
-      user2.logOut();
-      // since anonymous user sessions are shared, user1 is logged out as well
-      TestCase.assertNull(app.currentUser());
-      TestCase.assertFalse(firstUser.isLoggedIn);
-      TestCase.assertFalse(user2.isLoggedIn);
-    });
+    let firstUser = await app.logIn(Realm.Credentials.anonymous());
+    assertIsSameUser(app.currentUser(), firstUser);
+    let secondUser = await app.logIn(Realm.Credentials.anonymous());
+    // the most recently logged in user is considered current
+    TestCase.assertTrue(firstUser.isLoggedIn);
+    TestCase.assertTrue(secondUser.isLoggedIn);
+    assertIsSameUser(app.currentUser(), secondUser);
+    secondUser.logOut();
+    // since anonymous user sessions are shared, firstUser is logged out as well
+    TestCase.assertNull(app.currentUser());
+    TestCase.assertFalse(firstUser.isLoggedIn);
+    TestCase.assertFalse(secondUser.isLoggedIn);
   },
 
-  /* do this with a email/pw user 
-  testCurrentWithEmail() {
+  async testCurrentWithEmail() {
     let app = new Realm.App(appConfig);
+    await logOutExistingUsers(app);
     TestCase.assertNull(app.currentUser());
 
-    let firstUser;
-    return app.logIn(Realm.Credentials.anonymous()).then(user1 => {
-      assertIsSameUser(app.currentUser(), user1);
-      firstUser = user1;
-      return app.logIn(Realm.Credentials.anonymous());
-    }).then(user2 => {
-      // the most recently logged in user is considered current
-      assertIsSameUser(app.currentUser(), user2);
-      user2.logOut();
-      // auto change back to another logged in user
-      assertIsSameUser(app.currentUser(), firstUser);
+    let firstUser = await registerAndLogInEmailUser(app);
+    assertIsSameUser(app.currentUser(), firstUser);
+    let secondUser = await registerAndLogInEmailUser(app);
+    assertIsSameUser(app.currentUser(), secondUser); // the most recently logged in user is considered current
+    await secondUser.logOut();
+    assertIsSameUser(app.currentUser(), firstUser); // auto change back to another logged in user
+    await firstUser.logOut();
+    TestCase.assertNull(app.currentUser());
+  },
 
-      firstUser.logOut();
-      TestCase.assertNull(app.currentUser());
-    });
-  },*/
-
-  /* FIXME: do this with an email/pw user
-  testAll() {
+  async testAllWithEmail() {
     let app = new Realm.App(appConfig);
-    Object.keys(app.allUsers()).forEach(id => users[id].logOut()); // FIXME: we need to reset users for each test
+    await logOutExistingUsers(app);
 
-    const all = app.allUsers();
+    let all = app.allUsers();
     TestCase.assertArrayLength(Object.keys(all), 0, "Noone to begin with");
 
     let credentials = Realm.Credentials.anonymous();
-    return app.logIn(credentials).then(user1 => {
-      const all = app.allUsers();
-      TestCase.assertArrayLength(Object.keys(all), 1, "One user");
-      assertIsSameUser(all[user1.identity], user1);
+    let user1 = await registerAndLogInEmailUser(app);
+    all = app.allUsers();
+    TestCase.assertArrayLength(Object.keys(all), 1, "One user");
+    assertIsSameUser(all[user1.identity], user1);
+    let user2 = await registerAndLogInEmailUser(app);
+    all = app.allUsers();
+    TestCase.assertArrayLength(Object.keys(all), 2, "Two users");
+    // NOTE: the list of users is in latest-first order.
+    assertIsSameUser(all[user2.identity], user2);
+    assertIsSameUser(all[user1.identity], user1);
 
-      return app.logIn(Realm.Credentials.anonymous()).then(user2 => {
-        let all = app.allUsers();
-        TestCase.assertArrayLength(Object.keys(all), 2, "Two users");
-        // NOTE: the list of users is in latest-first order.
-        assertIsSameUser(all[user2.identity], user2);
-        assertIsSameUser(all[user1.identity], user1);
+    await user2.logOut();
+    all = app.allUsers();
+    assertIsSameUser(all[user2.identity], user2);
+    assertIsSameUser(all[user1.identity], user1);
+    TestCase.assertFalse(user2.isLoggedIn);
+    TestCase.assertTrue(user1.isLoggedIn);
+    TestCase.assertArrayLength(Object.keys(all), 2, "still holds references to both users");
 
-        user2.logOut();
-        all = app.allUsers();
-        TestCase.assertArrayLength(Object.keys(all), 1, "Back to one user");
-        assertIsSameUser(all[user1.identity], user1);
-
-        user1.logOut();
-        all = app.allUsers();
-        TestCase.assertArrayLength(Object.keys(all), 0, "All gone");
-      });
-    });
-  },*/
+    await user1.logOut();
+    all = app.allUsers();
+    TestCase.assertFalse(user1.isLoggedIn);
+    TestCase.assertFalse(user2.isLoggedIn);
+    TestCase.assertArrayLength(Object.keys(all), 2, "still holds references to both users"); // FIXME: is this actually expected?
+  }
 };
