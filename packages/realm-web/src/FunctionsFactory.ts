43770a0cafe101285727b73572881625d6166a85
Kræn Hansen <kraen.hansen@realm.io>
2020-06-25 10:45:38 +0200

    Adding dots to the end of docs sentencens

--- packages/realm-web/src/FunctionsFactory.ts
+++ packages/realm-web/src/FunctionsFactory.ts
@@ -28,11 +28,11 @@ const RESERVED_NAMES = ["inspect", "callFunction"];
  * The body of the request sent to call a remote function.
  */
 interface CallFunctionBody {
-    /** Name of the function */
+    /** Name of the function. */
     name: string;
-    /** An array of arguments to pass to the function */
+    /** An array of arguments to pass to the function. */
     arguments: any[];
-    /** An optional name of the service in which the function is defined */
+    /** An optional name of the service in which the function is defined. */
     service?: string;
 }
 
@@ -40,19 +40,19 @@ interface CallFunctionBody {
  * Pass an object implementing this interface when constructing a functions factory.
  */
 export interface FunctionsFactoryConfiguration {
-    /** An optional name of the service in which functions are defined */
+    /** An optional name of the service in which functions are defined. */
     serviceName?: string;
-    /** Call this function to transform the arguments before they're sent to the service */
+    /** Call this function to transform the arguments before they're sent to the service. */
     argsTransformation?: (args: any[]) => any[];
-    /** Call this function to transform a response before it's returned to the caller */
+    /** Call this function to transform a response before it's returned to the caller. */
     responseTransformation?: (response: any) => any;
 }
 
 /**
- * Remove the key for any fields with undefined values
+ * Remove the key for any fields with undefined values.
  *
- * @param args The arguments to clean
- * @returns The cleaned arguments
+ * @param args The arguments to clean.
+ * @returns The cleaned arguments.
  */
 function cleanArgs(args: any[]) {
     for (const arg of args) {
@@ -85,8 +85,8 @@ export class FunctionsFactory {
     /**
      * Create a factory of functions, wrapped in a Proxy that returns bound copies of `callFunction` on any property.
      *
-     * @param transport The underlying transport to use when requesting
-     * @param config Additional configuration parameters
+     * @param transport The underlying transport to use when requesting.
+     * @param config Additional configuration parameters.
      * @returns The newly created factory of functions.
      */
     public static create<
