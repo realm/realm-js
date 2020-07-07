8a080bfbfe983cffebeb6c01a40b8fc5c31a4d9a
Steffen Agger <steffen.agger@gmail.com>
2020-07-07 08:18:21 +0200

    Realm.Object declared as an abstract class, so TS allows you to extend it.

--- types/index.d.ts
+++ types/index.d.ts
@@ -157,7 +157,7 @@ declare namespace Realm {
      * Object
      * @see { @link https://realm.io/docs/javascript/latest/api/Realm.Object.html }
      */
-    interface Object {
+    abstract class Object {
         /**
          * @returns An array of the names of the object's properties.
          */
@@ -206,10 +206,6 @@ declare namespace Realm {
         removeListener(callback: ObjectChangeCallback): void;
 
         removeAllListeners(): void;
-    }
-
-    const Object: {
-        readonly prototype: Object;
     }
 
     /**
