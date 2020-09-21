357576ef536cb2ded916d924e746c89c9b7499c5
Franck Franck <franck.franck@mongodb.com>
2020-09-21 14:31:32 +0200

    Fixed nullptr segfault

--- src/js_realm.hpp
+++ src/js_realm.hpp
@@ -544,9 +544,11 @@ bool RealmClass<T>::get_realm_config(ContextType ctx, size_t argc, const ValueTy
                 if (config.schema_mode == SchemaMode::Immutable) {
                     throw std::invalid_argument("Cannot set 'deleteRealmIfMigrationNeeded' when 'readOnly' is set.");
                 }
-                if (config.sync_config->partition_value != "") {
+                std::cout << "Bleep" << std::endl;
+                if (config.sync_config && config.sync_config->partition_value != "") {
                    throw std::invalid_argument("Cannot set 'deleteRealmIfMigrationNeeded' when sync is enabled ('sync.partitionValue' is set).");
                 }
+                std::cout << "Bloop" << std::endl;
 
                 config.schema_mode = SchemaMode::ResetFile;
             }
