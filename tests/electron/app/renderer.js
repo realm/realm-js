418cc0d9b268944c618cffb4e3b2ad5723ab85e1
blagoev <lubo@blagoev.com>
2020-10-07 12:08:27 +0300

    fix Electron tests to use electron cli. (No more spectron)

--- tests/electron/app/renderer.js
+++ tests/electron/app/renderer.js
@@ -1,11 +1,22 @@
 "use strict";
 
+const { app } = require("electron");
+
 const remote = require("electron").remote;
+
+process.on("uncaughtException", (error) => {
+  console.log(error);
+  process.exit(-1);
+});
 
 const options = remote.getGlobal("options");
 if (options.runIn === "render") {
   const jasmine = require("./jasmine.js").execute(options.filter);
   jasmine.onComplete((passed) => {
-    console.log(`Testing completed with status ${passed ? 0 : -1}`)
+    let success = passed ? 0 : -1;
+    console.log(`\nTesting completed in renderer process with status ${success}`)
+    if (success === -1) {
+      process.exit(-1);
+    }
   });
 }
