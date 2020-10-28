79bfbcbc9bea85b8ac751ec805cab6fec2c4b6d3
Kræn Hansen <kraen.hansen@mongodb.com>
2020-10-28 13:21:59 +0100

    Updated the bson package

--- packages/realm-web/src/dom/detect-browser.d.ts
+++ packages/realm-web/src/dom/detect-browser.d.ts
@@ -16,16 +16,6 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-// Our version of Node.js types are very restricted
-// This file will get resolved when realm's dependency on bson resolves a dependency on @types/node.
-// We need this file to prevent the NodeJS globals to be accessable from within the test source files.
-
-/**
- * The simplest buffer we can come up with.
- * NOTE: This is needed because the "bson" package's types depends on it.
- */
-type Buffer = Uint8Array;
-
 /**
  * Used by the "detect-browser" package.
  */
