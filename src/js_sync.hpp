37a525a3c75bc3f8a63c04c19bd070bed27faead
Kenneth Geisshirt <geisshirt@gmail.com>
2021-02-08 12:47:00 +0100

    initial proxy support

--- src/js_sync.hpp
+++ src/js_sync.hpp
@@ -830,6 +830,35 @@ void SyncClass<T>::populate_sync_config(ContextType ctx, ObjectType realm_constr
             config.sync_config->custom_http_headers = std::move(http_headers);
         }
 
+        // HTTP proxy: only node is supported
+#ifdef REALM_PLATFORM_NODE
+        SyncConfig::ProxyConfig proxy_config;
+        std::vector<std::string> env_vars = { "http_proxy", "HTTP_PROXY", "https_proxy", "HTTPS_PROXY" };
+        for (auto env_var : env_vars) {
+            char *http_proxy = getenv(env_var.c_str());
+            if (http_proxy != NULL) {
+                std::string url(http_proxy);
+
+                std::size_t index1 = url.find_first_of(":");
+                std::string protocol = url.substr(0, index1);
+
+                std::string url_new = url.substr(index1 + 3); // skip http(s)
+                std::size_t index2 = url_new.find_first_of(":");
+                std::string host = url_new.substr(0, index2);
+
+                std::size_t index3 = url_new.find_first_of("/");
+                std::string port = url_new.substr(index2 + 1, index3 - index2 - 1);
+                std::string path = url_new.substr(index3);
+
+                proxy_config.type = (protocol == "http") ? SyncConfig::ProxyConfig::Type::HTTP : SyncConfig::ProxyConfig::Type::HTTPS;
+                proxy_config.address = std::move(host);
+                proxy_config.port = static_cast<std::uint_fast16_t>(atoi(port.c_str()));
+            }
+        }
+
+        config.sync_config->proxy_config = util::Optional<SyncConfig::ProxyConfig>(std::move(proxy_config));
+#endif
+
         if (!config.encryption_key.empty()) {
             config.sync_config->realm_encryption_key = std::array<char, 64>();
             std::copy_n(config.encryption_key.begin(), config.sync_config->realm_encryption_key->size(), config.sync_config->realm_encryption_key->begin());
