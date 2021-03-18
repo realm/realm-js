fceba46c49c66c37bdba08a0460fd8d30fa14abb
Steffen Agger <steffen.agger@gmail.com>
2021-03-18 15:24:05 +0100

    List<UUID> test added to uuid-sync-tests

--- tests/js/uuid-sync-tests.js
+++ tests/js/uuid-sync-tests.js
@@ -30,7 +30,8 @@ const SCHEMA = {
     properties: {
         _id: "uuid",
         mandatory: "uuid",
-        optional: "uuid?"
+        optional: "uuid?",
+        list: "uuid[]"
     }
 }
 
@@ -58,6 +59,12 @@ module.exports = {
             realm1.deleteAll();
         });
 
+        const uuidTestArray = [
+          new UUID("183f85f5-9fbc-4010-8566-85b9b2a91464"),
+          new UUID("283f85f5-9fbc-4010-8566-85b9b2a91464"),
+          new UUID("383f85f5-9fbc-4010-8566-85b9b2a91464")
+        ]
+
         realm1.write(() => {
             realm1.create(SCHEMA.name, { 
               _id: new UUID("a2a26ae1-3ba0-4c2c-a730-52f7e0825b85"),
@@ -70,7 +77,8 @@ module.exports = {
             });
             realm1.create(SCHEMA.name, { 
               _id: new UUID("c2a26ae1-3ba0-4c2c-a730-52f7e0825b85"),
-              mandatory: new UUID("3ecad6e1-3fab-4a51-ade9-d0f733221adb")
+              mandatory: new UUID("3ecad6e1-3fab-4a51-ade9-d0f733221adb"),
+              list: uuidTestArray
             });
         });
 
@@ -98,6 +106,7 @@ module.exports = {
         TestCase.assertEqual(first.mandatory.toString(), "176845e7-b463-4735-a125-82a926e298b4");
         TestCase.assertInstanceOf(first.optional, UUID);
         TestCase.assertEqual(first.optional.toString(), "1af1761a-566f-434d-b1a9-2ad38165db68");
+        TestCase.assertArraysEqual(first.list, []);
 
         const second = objects[1];
         TestCase.assertInstanceOf(second._id, UUID);
@@ -105,13 +114,15 @@ module.exports = {
         TestCase.assertInstanceOf(second.mandatory, UUID);
         TestCase.assertEqual(second.mandatory.toString(), "22515690-186b-4c24-a2a6-a84d991ace9e");
         TestCase.assertNull(second.optional);
-        
+        TestCase.assertArraysEqual(second.list, []);
+
         const third = objects[2];
         TestCase.assertInstanceOf(third._id, UUID);
         TestCase.assertEqual(third._id.toString(), "c2a26ae1-3ba0-4c2c-a730-52f7e0825b85");
         TestCase.assertInstanceOf(third.mandatory, UUID);
         TestCase.assertEqual(third.mandatory.toString(), "3ecad6e1-3fab-4a51-ade9-d0f733221adb");
         TestCase.assertNull(third.optional);
+        TestCase.assertArraysEqual(third.list, uuidTestArray);
 
         realm2.close();
     }
