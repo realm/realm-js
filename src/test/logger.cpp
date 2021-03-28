89833f7e7592fa274b879c551fa968392c63781c
Cesar Valdez <cvaldezrx@gmail.com>
2021-03-28 18:00:49 +0200

    Adding C++ testing framework and class test to the project.

---
+++ src/test/logger.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <vector>
+#include "logger.hpp"
+
+using Catch::Matchers::Contains;
+
+TEST_CASE( "Testing Logger#get_level" ) {
+    REQUIRE( realm::common::logger::Logger::get_level("all") == realm::common::logger::LoggerLevel::all );
+    REQUIRE( realm::common::logger::Logger::get_level("debug") == realm::common::logger::LoggerLevel::debug );
+    REQUIRE_THROWS_WITH(realm::common::logger::Logger::get_level("coffeebabe"), Contains("Bad log level"));
+}
