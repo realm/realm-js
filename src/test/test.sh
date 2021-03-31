8a4d6fdc90a2e51e41592c1eec20b7c58cc45181
cesar <cesarv01@gmail.com>
2021-03-31 08:32:30 +0200

    Removing test binary

--- src/test/test.sh
+++ src/test/test.sh
@@ -3,11 +3,11 @@ g++ -std=c++17 -Wall -I catch.hpp -I test_bed.hpp -I ../ \
     -I ../../vendor/realm-core/src/ \
     -I ../..//cmake-build-debug/vendor/realm-core/src/ \
     -I ../object-store/external/json \
-    -framework Foundation -framework JavaScriptCore -o testing main_testing.cpp catch.cpp \
+    -framework Foundation -framework JavaScriptCore -o testing.o main_testing.cpp catch.cpp \
     $(find ../../ -name "librealm-dbg.a") \
     $(find ../../cmake-build-debug -name "librealm-sync-dbg.a") \
     $(find ../../cmake-build-debug -name "librealm-object-store-dbg.a") \
     && 
 
-./testing  
+./testing.o  
 # missing realm::ConstLstIf
