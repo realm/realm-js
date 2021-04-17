175b0753e8dc36f2ab5d785e1dfc0733f85a6e5f
Kræn Hansen <kraen.hansen@mongodb.com>
2021-04-17 23:41:02 +0200

    Fixed running ios tests via a --prefix

--- integration-tests/environments/react-native/harness/react-native-cli.js
+++ integration-tests/environments/react-native/harness/react-native-cli.js
@@ -18,11 +18,18 @@
 
 const cp = require("child_process");
 
+// Filtering out any npm related environment variables,
+// since they mess up React Natives XCode PhaseScriptExecution
+// Specifically the npm_config_prefix makes it impossible execute the build if "npm --prefix" is used by upstream commands.
+const filteredEnv = Object.fromEntries(
+    Object.entries(process.env).filter(([k]) => !k.startsWith("npm_")),
+);
+
 function async(...args) {
     return cp.spawn(
         "node",
         [require.resolve("react-native/local-cli/cli.js"), ...args],
-        { stdio: ["inherit", "inherit", "inherit"] },
+        { stdio: ["inherit", "inherit", "inherit"], env: filteredEnv },
     );
 }
 
@@ -30,7 +37,7 @@ function sync(...args) {
     const process = cp.spawnSync(
         "node",
         [require.resolve("react-native/local-cli/cli.js"), ...args],
-        { stdio: ["inherit", "inherit", "inherit"] },
+        { stdio: ["inherit", "inherit", "inherit"], env: filteredEnv },
     );
     if (process.status !== 0) {
         throw new Error(`Failed running "react-native ${args.join(" ")}"`);
