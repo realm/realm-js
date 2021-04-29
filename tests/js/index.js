30c5ebf017cd77646335340cf42c2bf9676781f6
Kenneth Geisshirt <k@zigzak.net>
2021-04-29 11:17:49 +0200

    Split tests of mixed into sync and non-sync

--- tests/js/index.js
+++ tests/js/index.js
@@ -68,6 +68,7 @@ if (global.enableSyncTests) {
         TESTS.UUIDSyncTests= node_require("./uuid-sync-tests");
         TESTS.PartitionValueTests = node_require("./partition-value-tests");
         TESTS.SetSyncTests = node_require("./set-sync-tests");
+        TESTS.MixedSyncTests = node_require("./mixed-sync-tests");
     }
 }
 
