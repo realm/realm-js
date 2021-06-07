00ee502934dfc90a27a4c917237a18d48f55fdf9
Mathias Stearn <redbeard0531@gmail.com>
2021-06-07 15:32:34 +0200

    initial port of abstraction layer to hermes

---
+++ src/hermes/hermes_init.hpp
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2021 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "hermes_string.hpp"
+#include "hermes_protected.hpp"
+#include "hermes_function.hpp"
+#include "hermes_value.hpp"
+#include "hermes_object.hpp"
+#include "hermes_return_value.hpp"
+#include "hermes_class.hpp"
+
+// FIXME: js_object_accessor.hpp includes js_list.hpp which includes js_object_accessor.hpp.
+#include "js_object_accessor.hpp"
