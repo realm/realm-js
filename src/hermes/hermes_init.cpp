5191c0001d480babbd9d3a3afe547bfd5f19e909
Mathias Stearn <redbeard0531@gmail.com>
2021-06-07 15:32:34 +0200

    initial port of abstraction layer to hermes

---
+++ src/hermes/hermes_init.cpp
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2021 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#include "hermes_init.hpp"
+
+#if !REALM_ENABLE_SYNC
+#pragma comment( lib, "ws2_32.lib")
+#pragma comment (lib, "crypt32")
+#endif
+
+#include "js_realm.hpp"
+
+namespace realm::js::hermes {
+void hermes_init(JsiEnv env, JsiObj& exports) {
+    jsi::Function realm_constructor = js::RealmClass<Types>::create_constructor(env);
+    auto name = realm_constructor.getProperty(env, "name").asString(env);
+    exports->setProperty(env, std::move(name), std::move(realm_constructor));
+
+    // Only calling to populate static cache. Eventually this should be stored somewhere non-static.
+    (void)js::ObjectWrap<Types, js::RealmObjectClass<Types>>::create_constructor(env);
+}
+}
+
+// TODO hook up as TurboModule
