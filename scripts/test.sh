dc261a43e4630360c097522e2a236e8890058802
Kenneth Geisshirt <k@zigzak.net>
2021-09-09 11:14:40 +0200

    wip

--- scripts/test.sh
+++ scripts/test.sh
@@ -409,7 +409,7 @@ case "$TARGET" in
   test_temp_dir=$PWD # set it to be cleaned at exit
 
   pushd "$SRCROOT/tests"
-  npm ci --build-from-source=realm --realm_enable_sync=${USE_REALM_SYNC} --use_realm_debug=${USE_REALM_DEBUG}
+  npm ci
   npm run test
   popd
   stop_server
