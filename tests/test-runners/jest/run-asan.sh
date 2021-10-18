f61b6b491a909c47f7353935f9f6fd39b2853859
Tom Duncalf <tom.duncalf@mongodb.com>
2021-10-18 17:21:34 +0100

    Add ASAN to build

---
+++ tests/test-runners/jest/run-asan.sh
@@ -0,0 +1,1 @@
+DYLD_INSERT_LIBRARIES=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/lib/darwin/libclang_rt.asan_osx_dynamic.dylib node ./node_modules/.bin/jest
