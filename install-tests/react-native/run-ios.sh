967256c45e6880e8e3a6b82771cd6d185f0bde7e
Kræn Hansen <kraen.hansen@mongodb.com>
2021-11-12 10:43:46 +0100

    Renamed run script and pre-booting simulator

--- install-tests/react-native/run-ios.sh
+++ install-tests/react-native/run-ios.sh
@@ -4,16 +4,21 @@ set -e
 
 APP_DIR="$PWD/`dirname $0`/app"
 
-platform="${1:-ios}"
-
 # Manually delete any app previously created
 if [ ! -d "$APP_DIR" ]; then
   echo "Couldn't locate an app directory, did you run install.sh?"
   exit 1
 fi
 
+# Determine the simulator's UDID
+DEVICE_UDID=`npx -y ios-simulator -n 'iPhone 13'`
+# Open the simulator
+open -a Simulator --args -CurrentDeviceUDID $DEVICE_UDID
+# Await the boot of the device
+xcrun simctl bootstatus $DEVICE_UDID
+
 cd $APP_DIR
 # Run the app
-npx react-native run-$platform
+npx react-native run-ios --udid $DEVICE_UDID
 # Start listening for the app
 node ../listen.js
