564e004ef438a480e2360cf8dfa15f3bf8584d23
Andrew Meyer <andrew.meyer@mongodb.com>
2021-11-22 14:32:15 -0600

    Add tests for immutable realm.

--- integration-tests/tests/src/tests/immutable.ts
+++ integration-tests/tests/src/tests/immutable.ts
@@ -19,12 +19,28 @@ import { expect } from "chai";
 import Realm from "realm";
 import { PersonSchema, IPerson } from "../schemas/person-and-dogs";
 
+const schema = [
+  {
+    name: "Person",
+    primaryKey: "name",
+    properties: { name: "string", age: "int?", friend: "Person?" },
+  },
+];
+
+function createPersons(realm: Realm) {
+  realm.write(() => {
+    realm.create("Person", { name: "alice" });
+    realm.create("Person", { name: "bob" });
+    realm.create("Person", { name: "charlie" });
+  });
+}
+
 describe("Immutable Realm", () => {
   beforeEach(() => {
     Realm.clearTestState();
   });
 
-  describe("objects", () => {
+  describe("Objects", () => {
     it("to be freezable", () => {
       const realm = new Realm({ schema: [PersonSchema] });
       let john: IPerson & Realm.Object;
@@ -52,6 +68,187 @@ describe("Immutable Realm", () => {
       expect(frozenJohn._isFrozen).equals(true);
       expect(john._version).equals(4);
       expect(frozenJohn._version).equals(3);
+    });
+    it("are imutable", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      // aliceA is {name: "alice", age: undefined}
+      const aliceA = realm.objectForPrimaryKey<IPerson>("Person", "alice");
+
+      expect(aliceA.age).equals(undefined);
+      // Object is only mutable inside a write transaction
+      realm.write(() => {
+        // TODO: So this is where we will add the writeable code when its ready
+        // const mutableAlice = realm.writeable(aliceA);
+        // mutableAlice.age = 21;
+        // TODO: Consider if we should allow objects to be mutable when in a write transaction?
+        // expect(mutableAlice.age).toEqual(21);
+        // expect(aliceA.age).toEqual(undefined);
+
+        aliceA.age = 21;
+      });
+
+      expect(aliceA.age).equals(undefined);
+
+      // If we pull it out again the priperties gets updated
+      const aliceB = realm.objectForPrimaryKey<IPerson>("Person", "alice");
+      expect(aliceB.age).equals(21);
+
+      // TODO: When latest is availble, uncomment
+      // Convenience to get alice again, with the updated properties (without primary key)
+      // aliceC is immutable
+      // const aliceC = Realm.latest(aliceA);
+      // expect(aliceC.age).toEqual(21);
+
+      // expect(aliceB).equals(aliceC);
+      // expect(aliceC).not.equals(aliceA);
+    });
+  });
+  describe("Collections", () => {
+    it("returns the same Collection if objects didn't change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const collectionA = realm.objects("Person");
+      const collectionB = realm.objects("Person");
+      expect(collectionB).equals(collectionA);
+    });
+
+    it("returns a different Collection if objects change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const collectionA = realm.objects<IPerson>("Person");
+      realm.write(() => {
+        collectionA[0].name = "ali";
+      });
+      const collectionB = realm.objects("Person");
+      expect(collectionB).not.equals(collectionA);
+    });
+
+    it("returns a different Collection if objects are added or removed", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const collectionA = realm.objects("Person");
+      realm.write(() => {
+        realm.create("Person", { name: "daniel" });
+      });
+      const collectionB = realm.objects("Person");
+      expect(collectionB).not.equals(collectionA);
+      realm.write(() => {
+        realm.delete(collectionA[0]);
+      });
+      const collectionC = realm.objects("Person");
+      expect(collectionC).not.equals(collectionB);
+    });
+
+    it("returns the same Object from a Collection if it didn't change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const aliceA = realm.objects("Person")[0];
+      const aliceB = realm.objects("Person")[0];
+      expect(aliceB).equals(aliceA);
+    });
+
+    it("returns different Objects from a Collection if it changes", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const alice = realm.objects<IPerson>("Person")[0];
+      realm.write(() => {
+        alice.name = "ali";
+      });
+      const ali = realm.objects<IPerson>("Person")[0];
+      expect(ali).not.equals(alice);
+    });
+
+    // TODO: This might be too strict of a guarentee?
+    it("returns the same object via different Collection, if it didn't change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const aliceA = realm.objects("Person").sorted("name")[0];
+      const aliceB = realm.objects("Person").filtered("name = $0", "alice")[0];
+      expect(aliceB).equals(aliceA);
+    });
+
+    it("returns the same object if Collection change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const personsA = realm.objects("Person").sorted("name");
+      const aliceA = personsA[0];
+      realm.write(() => {
+        realm.create("Person", { name: "daniel" });
+      });
+      const personsB = realm.objects("Person").sorted("name");
+      const aliceB = personsA[0];
+
+      expect(personsB).not.equals(personsA);
+      expect(aliceB).equals(aliceA);
+    });
+
+    it("returns different objects via different Collection, if it changes", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const aliceA = realm.objects<IPerson>("Person").sorted("name")[0];
+      realm.write(() => {
+        aliceA.name = "ali";
+      });
+      const aliceB = realm.objects("Person").filtered("name = $0", "alice")[0];
+      expect(aliceB).not.equals(aliceA);
+    });
+  });
+  describe("objectForPrimaryKey", () => {
+    it("returns the same object if it didn't change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const aliceA = realm.objectForPrimaryKey("Person", "alice");
+      const aliceB = realm.objectForPrimaryKey("Person", "alice");
+      expect(aliceB).equals(aliceA);
+    });
+
+    it("returns a different object if it change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const aliceA = realm.objectForPrimaryKey("Person", "alice");
+      realm.write(() => {
+        // TODO: We would need to apply the `realm.writeable` here?
+        aliceA.age = 21;
+      });
+      const aliceB = realm.objectForPrimaryKey("Person", "alice");
+      expect(aliceB).not.equals(aliceA);
+    });
+
+    it("returns a different object if a linked object change", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const aliceA = realm.objectForPrimaryKey<IPerson>("Person", "alice");
+      realm.write(() => {
+        //TODO: impelement writable and uncomment
+        // const writableAlice = realm.writeable(aliceA);
+        // writableAlice.friend.age = 21;
+        aliceA.friends[0].age = 21;
+      });
+      const aliceB = realm.objectForPrimaryKey("Person", "alice");
+      expect(aliceB).not.equals(aliceA);
+    });
+
+    // TODO: This might be too strict of a guarentee?
+    it("returns the same object as a Collection", () => {
+      const realm = new Realm({ schema });
+      createPersons(realm);
+
+      const aliceA = realm.objectForPrimaryKey("Person", "alice");
+      const aliceB = realm.objects("Person").filtered("name = $0", "alice");
+      expect(aliceB).equals(aliceA);
     });
   });
 });
