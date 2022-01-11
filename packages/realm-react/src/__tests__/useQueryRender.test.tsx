d9fb07fb01dd3432c5020b6776f49a89ef77466c
Andrew Meyer <andrew.meyer@mongodb.com>
2022-01-11 15:55:56 +0100

    Write failing tests

--- packages/realm-react/src/__tests__/useQueryRender.test.tsx
+++ packages/realm-react/src/__tests__/useQueryRender.test.tsx
@@ -16,7 +16,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
 import Realm from "realm";
 import { render, waitFor, fireEvent, act } from "@testing-library/react-native";
 import { View, TextInput, TouchableHighlight, Text, FlatList, ListRenderItem } from "react-native";
@@ -38,7 +38,7 @@ interface IObject {
   name: string;
 }
 
-const testCollection = new Array(15).fill(undefined).map((_, index) => ({ id: index, name: `${index}` }));
+const testCollection = new Array(100).fill(undefined).map((_, index) => ({ id: index, name: `${index}` }));
 
 const configuration: Realm.Configuration = {
   schema: [ObjectSchema],
@@ -48,8 +48,11 @@ const configuration: Realm.Configuration = {
 
 const renderCounter = jest.fn();
 
+let testRealm: Realm = new Realm(configuration);
+
 const useRealm = () => {
-  const [realm] = useState(new Realm(configuration));
+  testRealm = new Realm(configuration);
+  const [realm] = useState(testRealm);
 
   useEffect(() => {
     return () => {
@@ -62,11 +65,17 @@ const useRealm = () => {
 
 const useQuery = createUseQuery(useRealm);
 
-const App = () => {
+enum QueryType {
+  filtered,
+  sorted,
+  normal,
+}
+
+const App = ({ type = QueryType.normal }) => {
   return (
     <SetupComponent>
       <View testID="testContainer">
-        <TestComponent />
+        <TestComponent type={type} />
       </View>
     </SetupComponent>
   );
@@ -121,14 +130,25 @@ const Item: React.FC<{ item: IObject & Realm.Object }> = React.memo(({ item }) =
   );
 });
 
-const TestComponent = () => {
+const TestComponent = ({ type }: { type: QueryType }) => {
   const collection = useQuery<IObject & Realm.Object>("Object");
+
+  const result = useMemo(() => {
+    switch (type) {
+      case QueryType.filtered:
+        return collection.filtered("id > 10");
+      case QueryType.sorted:
+        return collection.sorted("id", true);
+      case QueryType.normal:
+        return collection;
+    }
+  }, [type, collection]);
 
   const renderItem = useCallback(({ item }) => <Item item={item} />, []);
 
   const keyExtractor = useCallback((item) => item.id, []);
 
-  return <FlatList testID={"list"} data={collection} keyExtractor={keyExtractor} renderItem={renderItem} />;
+  return <FlatList testID={"list"} data={result} keyExtractor={keyExtractor} renderItem={renderItem} />;
 };
 
 describe("useQueryRender", () => {
@@ -136,47 +156,167 @@ describe("useQueryRender", () => {
     renderCounter.mockClear();
     Realm.clearTestState();
   });
-  it("renders data in one render cycle per visible object in collection", async () => {
-    const { getByTestId } = render(<App />);
 
-    await waitFor(() => getByTestId("list"));
+  describe("normal collections", () => {
+    it("renders data in one render cycle per visible object in collection", async () => {
+      const { getByTestId } = render(<App />);
 
-    expect(renderCounter).toHaveBeenCalledTimes(10);
+      await waitFor(() => getByTestId("list"));
+
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+    });
+    it("change to data will rerender", async () => {
+      const { getByTestId, getByText } = render(<App />);
+
+      const nameElement = getByTestId("name1");
+      const input = getByTestId("input1");
+
+      expect(nameElement).toHaveTextContent("1");
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+
+      fireEvent.changeText(input as ReactTestInstance, "apple");
+
+      await waitFor(() => getByText("apple"));
+
+      expect(nameElement).toHaveTextContent("apple");
+      expect(renderCounter).toHaveBeenCalledTimes(11);
+    });
+
+    it("handles deletions", async () => {
+      const { getByTestId } = render(<App />);
+
+      const deleteButton = getByTestId("deleteButton1");
+      const nameElement = getByTestId("name1");
+
+      expect(nameElement).toHaveTextContent("1");
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+
+      fireEvent.press(deleteButton as ReactTestInstance);
+
+      await waitFor(() => getByTestId("result10"));
+
+      expect(renderCounter).toHaveBeenCalledTimes(11);
+    });
+    it("an implicit update to an item in the FlatList view area causes a rerender", async () => {
+      render(<App />);
+
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+
+      const collection = testRealm.objects<IObject>("Object");
+
+      testRealm.write(() => {
+        collection[0].name = "apple";
+      });
+
+      // One could wait for "apple", but I want to mirror the underlying non-rerender test
+      await act(async () => {
+        // wait a bit to make sure there have been no changes
+        await new Promise((resolve) => setTimeout(resolve, 10));
+      });
+
+      expect(renderCounter).toHaveBeenCalledTimes(11);
+    });
+
+    it("does not rerender if the update is out of the FlatList view area", async () => {
+      render(<App />);
+
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+
+      const collection = testRealm.objects<IObject>("Object");
+
+      testRealm.write(() => {
+        const lastIndex = collection.length - 1;
+        collection[lastIndex].name = "apple";
+      });
+
+      await act(async () => {
+        // wait a bit to make sure there have been no changes
+        await new Promise((resolve) => setTimeout(resolve, 10));
+      });
+
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+    });
   });
-  it("change to data will rerender", async () => {
-    const { getByTestId, getByText } = render(<App />);
+  describe("filtered collections", () => {
+    it("an implicit update to an item in the FlatList view area causes a rerender", async () => {
+      render(<App type={QueryType.filtered} />);
 
-    const nameElement = getByTestId("name1");
-    const input = getByTestId("input1");
+      expect(renderCounter).toHaveBeenCalledTimes(10);
 
-    expect(nameElement).toHaveTextContent("1");
-    expect(renderCounter).toHaveBeenCalledTimes(10);
+      const collection = testRealm.objects<IObject>("Object").filtered("id > 10");
 
-    fireEvent.changeText(input as ReactTestInstance, "apple");
+      testRealm.write(() => {
+        collection[0].name = "apple";
+      });
 
-    await waitFor(() => getByText("apple"));
+      // One could wait for "apple", but I want to mirror the underlying non-rerender test
+      await act(async () => {
+        // wait a bit to make sure there have been no changes
+        await new Promise((resolve) => setTimeout(resolve, 10));
+      });
 
-    expect(nameElement).toHaveTextContent("apple");
-    expect(renderCounter).toHaveBeenCalledTimes(11);
+      expect(renderCounter).toHaveBeenCalledTimes(11);
+    });
+
+    it("does not rerender if the update is out of the FlatList view area", async () => {
+      render(<App type={QueryType.filtered} />);
+
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+
+      const collection = testRealm.objects<IObject>("Object").filtered("id > 10");
+
+      testRealm.write(() => {
+        const lastIndex = collection.length - 1;
+        collection[lastIndex].name = "apple";
+      });
+
+      await act(async () => {
+        // wait a bit to make sure there have been no changes
+        await new Promise((resolve) => setTimeout(resolve, 10));
+      });
+
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+    });
   });
+  describe("sorted collections", () => {
+    it("an implicit update to an item in the FlatList view area causes a rerender", async () => {
+      render(<App type={QueryType.sorted} />);
 
-  it("handles deletions", async () => {
-    const { getByTestId } = render(<App />);
+      expect(renderCounter).toHaveBeenCalledTimes(10);
 
-    const deleteButton = getByTestId("deleteButton1");
-    const nameElement = getByTestId("name1");
+      const collection = testRealm.objects<IObject>("Object").sorted("id", true);
 
-    expect(nameElement).toHaveTextContent("1");
-    expect(renderCounter).toHaveBeenCalledTimes(10);
+      testRealm.write(() => {
+        collection[0].name = "apple";
+      });
 
-    fireEvent.press(deleteButton as ReactTestInstance);
+      // One could wait for "apple", but I want to mirror the underlying non-rerender test
+      await act(async () => {
+        // wait a bit to make sure there have been no changes
+        await new Promise((resolve) => setTimeout(resolve, 10));
+      });
 
-    await waitFor(() => getByTestId("result10"));
+      expect(renderCounter).toHaveBeenCalledTimes(11);
+    });
 
-    expect(renderCounter).toHaveBeenCalledTimes(11);
-  });
+    it("does not rerender if the update is out of the FlatList view area", async () => {
+      render(<App />);
 
-  xit("handles implicit updates", () => {
-    expect(true).toEqual(true);
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+
+      const collection = testRealm.objects<IObject>("Object").sorted("id", true);
+
+      testRealm.write(() => {
+        const lastIndex = collection.length - 1;
+        collection[lastIndex].name = "apple";
+      });
+
+      await act(async () => {
+        // wait a bit to make sure there have been no changes
+        await new Promise((resolve) => setTimeout(resolve, 10));
+      });
+
+      expect(renderCounter).toHaveBeenCalledTimes(10);
+    });
   });
 });
