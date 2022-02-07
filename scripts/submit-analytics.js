32a6f2f3487c5f13d95ed93f135bf4b816409924
Kenneth Geisshirt <k@zigzak.net>
2022-02-07 17:33:01 +0100

    wip

--- scripts/submit-analytics.js
+++ scripts/submit-analytics.js
@@ -118,7 +118,7 @@ function isAnalyticsDisabled() {
 }
 
 function getRealmVersion() {
-  const packageJson = fse.readJsonSync([getPackageJson(), "node_modules", "realm", "package.json"].join(path.sep));
+  const packageJson = fse.readJsonSync([getProjectRoot(), "node_modules", "realm", "package.json"].join(path.sep));
   return packageJson["version"];
 }
 
