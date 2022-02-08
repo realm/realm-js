44ac30484abea4bdfeae804a8a6c3ffd79028eee
Tom Duncalf <tom.duncalf@mongodb.com>
2022-02-08 16:45:56 +0000

    WIP

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
         },
         disableSources: true,
         includeVersion: true,
-        readme: "none",
+        // readme: "none",
       },
     ],
   ],
@@ -78,21 +78,21 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
+        title: "Realm JS",
         logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
+          alt: "Realm Logo",
+          src: "img/realm.svg",
         },
         items: [
+          // {
+          //   type: "doc",
+          //   docId: "intro",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Tutorial",
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/realm/realm-js",
             label: "GitHub",
             position: "right",
           },
@@ -141,7 +141,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} MongoDB, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
