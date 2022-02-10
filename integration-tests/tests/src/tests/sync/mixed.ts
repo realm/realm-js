5a4a8f87752460d02da947989486d3d17d8d7dfa
Tom Duncalf <tom.duncalf@mongodb.com>
2022-02-10 15:10:09 +0000

    Disable flexible sync tests with skipFlexibleSync context option

--- integration-tests/tests/src/tests/sync/mixed.ts
+++ integration-tests/tests/src/tests/sync/mixed.ts
@@ -216,7 +216,7 @@ describe.skipIf(environment.missingServer, "mixed", () => {
     describeTypes(false);
   });
 
-  describe("flexible sync", function () {
+  describe.skipIf(environment.skipFlexibleSync, "flexible sync", function () {
     importAppBefore("with-db-flx");
     describeTypes(true);
   });
