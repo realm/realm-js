e1a66a6313b8821440945ff8e535acc033ec4f77
Andrew Meyer <andrew.meyer@mongodb.com>
2022-03-04 09:03:07 +0100

    Add tests for lists within a collection

--- packages/realm-react/src/__tests__/useQueryRender.test.tsx
+++ packages/realm-react/src/__tests__/useQueryRender.test.tsx
@@ -22,14 +22,30 @@ import { render, waitFor, fireEvent, act } from "@testing-library/react-native";
 import { View, TextInput, TouchableHighlight, Text, FlatList } from "react-native";
 import "@testing-library/jest-native/extend-expect";
 import { ReactTestInstance } from "react-test-renderer";
-import { createUseQuery } from "../useQuery";
+import { createRealmContext } from "..";
 
-class TestObject extends Realm.Object {
+class Item extends Realm.Object {
+  id!: number;
+  name!: string;
+  tags!: Realm.List<Tag>;
+
+  static schema = {
+    name: "Item",
+    primaryKey: "id",
+    properties: {
+      id: "int",
+      name: "string",
+      tags: "Tag[]",
+    },
+  };
+}
+
+class Tag extends Realm.Object {
   id!: number;
   name!: string;
 
   static schema = {
-    name: "TestObject",
+    name: "Tag",
     primaryKey: "id",
     properties: {
       id: "int",
@@ -38,32 +54,28 @@ class TestObject extends Realm.Object {
   };
 }
 
-const testCollection = [...new Array(100)].map((_, index) => ({ id: index, name: `${index}` }));
+// TODO: It would be better not to have to rely on this, but at the moment I see no other alternatives
+function forceSynchronousNotifications(realm: Realm) {
+  // Starting a transaction will force all listeners to advance to the latest version
+  // and deliver notifications. We don't need to commit the transaction for this to work.
+  realm.beginTransaction();
+  realm.cancelTransaction();
+}
+
+const testCollection = [...new Array(30)].map((_, index) => ({ id: index, name: `${index}` }));
 
 const configuration: Realm.Configuration = {
-  schema: [TestObject],
+  schema: [Item, Tag],
   inMemory: true,
   path: "testArtifacts/use-query-rerender.realm",
 };
 
-const renderCounter = jest.fn();
+const itemRenderCounter = jest.fn();
+const tagRenderCounter = jest.fn();
 
 let testRealm: Realm = new Realm(configuration);
 
-const useRealm = () => {
-  testRealm = new Realm(configuration);
-  const [realm] = useState(testRealm);
-
-  useEffect(() => {
-    return () => {
-      realm.close();
-    };
-  }, [realm]);
-
-  return realm;
-};
-
-const useQuery = createUseQuery(useRealm);
+const { useQuery, RealmProvider, useRealm } = createRealmContext(configuration);
 
 enum QueryType {
   filtered,
@@ -73,11 +85,13 @@ enum QueryType {
 
 const App = ({ queryType = QueryType.normal }) => {
   return (
-    <SetupComponent>
-      <View testID="testContainer">
-        <TestComponent queryType={queryType} />
-      </View>
-    </SetupComponent>
+    <RealmProvider>
+      <SetupComponent>
+        <View testID="testContainer">
+          <TestComponent queryType={queryType} />
+        </View>
+      </SetupComponent>
+    </RealmProvider>
   );
 };
 
@@ -87,7 +101,7 @@ const SetupComponent = ({ children }: { children: JSX.Element }): JSX.Element | 
   useEffect(() => {
     realm.write(() => {
       realm.deleteAll();
-      testCollection.forEach((object) => realm.create(TestObject, object));
+      testCollection.forEach((object) => realm.create(Item, object));
     });
     setSetupComplete(true);
   }, [realm]);
@@ -99,9 +113,13 @@ const SetupComponent = ({ children }: { children: JSX.Element }): JSX.Element | 
   return children;
 };
 
-const Item: React.FC<{ item: TestObject & Realm.Object }> = React.memo(({ item }) => {
-  renderCounter();
+const ItemComponent: React.FC<{ item: Item & Realm.Object }> = React.memo(({ item }) => {
+  itemRenderCounter();
   const realm = useRealm();
+  const renderItem = useCallback(({ item }) => <TagComponent tag={item} />, []);
+
+  const keyExtractor = useCallback((item) => `tag-${item.id}`, []);
+
   return (
     <View testID={`result${item.id}`}>
       <View testID={`name${item.id}`}>
@@ -126,20 +144,38 @@ const Item: React.FC<{ item: TestObject & Realm.Object }> = React.memo(({ item }
       >
         <Text>Delete</Text>
       </TouchableHighlight>
+      <FlatList
+        horizontal={true} // Make this internal list horizontal to avoid invariant error
+        testID={`tagList-${item.id}`}
+        data={item.tags}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+      />
     </View>
   );
 });
 
-const FILTERED_ARGS: [string] = ["id > 10"];
+const TagComponent: React.FC<{ tag: Tag & Realm.Object }> = React.memo(({ tag }) => {
+  tagRenderCounter();
+  return (
+    <View testID={`tagResult${tag.id}`}>
+      <View testID={`tagName${tag.id}`}>
+        <Text>{tag.name}</Text>
+      </View>
+    </View>
+  );
+});
+
+const FILTER_ARGS: [string] = ["id < 20"];
 const SORTED_ARGS: [string, boolean] = ["id", true];
 
 const TestComponent = ({ queryType }: { queryType: QueryType }) => {
-  const collection = useQuery(TestObject);
+  const collection = useQuery(Item);
 
   const result = useMemo(() => {
     switch (queryType) {
       case QueryType.filtered:
-        return collection.filtered(...FILTERED_ARGS);
+        return collection.filtered(...FILTER_ARGS);
       case QueryType.sorted:
         return collection.sorted(...SORTED_ARGS);
       case QueryType.normal:
@@ -147,32 +183,46 @@ const TestComponent = ({ queryType }: { queryType: QueryType }) => {
     }
   }, [queryType, collection]);
 
-  const renderItem = useCallback(({ item }) => <Item item={item} />, []);
+  const renderItem = useCallback(({ item }) => <ItemComponent item={item} />, []);
 
   const keyExtractor = useCallback((item) => item.id, []);
 
-  return <FlatList testID="list" data={result} keyExtractor={keyExtractor} renderItem={renderItem} />;
+  return <FlatList testID={"list"} data={result} keyExtractor={keyExtractor} renderItem={renderItem} />;
 };
 
 function getTestCollection(queryType: QueryType) {
   switch (queryType) {
     case QueryType.filtered:
-      return testRealm.objects(TestObject).filtered(...FILTERED_ARGS);
+      return testRealm.objects(Item).filtered(...FILTER_ARGS);
     case QueryType.sorted:
-      return testRealm.objects(TestObject).sorted(...SORTED_ARGS);
+      return testRealm.objects(Item).sorted(...SORTED_ARGS);
     case QueryType.normal:
-      return testRealm.objects(TestObject);
+      return testRealm.objects(Item);
   }
+}
+
+async function setupTest(queryType: QueryType) {
+  const renderResult = render(<App queryType={queryType} />);
+  await waitFor(() => renderResult.getByTestId("testContainer"));
+
+  const collection = getTestCollection(queryType);
+  expect(itemRenderCounter).toHaveBeenCalledTimes(10);
+
+  return { ...renderResult, collection };
 }
 
 describe.each`
   queryTypeName | queryType
+  ${"sorted"}   | ${QueryType.sorted}
   ${"normal"}   | ${QueryType.normal}
   ${"filtered"} | ${QueryType.filtered}
-  ${"sorted"}   | ${QueryType.sorted}
 `("useQueryRender: $queryTypeName", ({ queryType }) => {
+  beforeEach(() => {
+    testRealm = new Realm(configuration);
+  });
   afterEach(() => {
-    renderCounter.mockClear();
+    itemRenderCounter.mockClear();
+    tagRenderCounter.mockClear();
     Realm.clearTestState();
   });
 
@@ -181,12 +231,11 @@ describe.each`
 
     await waitFor(() => getByTestId("list"));
 
-    expect(renderCounter).toHaveBeenCalledTimes(10);
+    expect(itemRenderCounter).toHaveBeenCalledTimes(10);
   });
   it("change to data will rerender", async () => {
-    const { getByTestId, getByText } = render(<App queryType={queryType} />);
+    const { getByTestId, getByText, collection } = await setupTest(queryType);
 
-    const collection = getTestCollection(queryType);
     const firstItem = collection[0];
     const id = firstItem.id;
 
@@ -194,22 +243,22 @@ describe.each`
     const input = getByTestId(`input${id}`);
 
     expect(nameElement).toHaveTextContent(`${id}`);
-    expect(renderCounter).toHaveBeenCalledTimes(10);
 
     fireEvent.changeText(input as ReactTestInstance, "apple");
 
+    // TODO: This line throws an `act` warning.  Keep an eye on this issue and see if there's a solution
+    // https://github.com/callstack/react-native-testing-library/issues/379
     await waitFor(() => getByText("apple"));
 
     expect(nameElement).toHaveTextContent("apple");
-    expect(renderCounter).toHaveBeenCalledTimes(11);
+    expect(itemRenderCounter).toHaveBeenCalledTimes(11);
   });
 
   // TODO:  This is a known issue that we have to live with until it is possible
   // to retrieve the objectId from a deleted object in a listener callback
   it.skip("handles deletions", async () => {
-    const { getByTestId } = render(<App queryType={queryType} />);
+    const { getByTestId, collection } = await setupTest(queryType);
 
-    const collection = getTestCollection(queryType);
     const firstItem = collection[0];
     const id = firstItem.id;
 
@@ -219,22 +268,16 @@ describe.each`
     const nameElement = getByTestId(`name${id}`);
 
     expect(nameElement).toHaveTextContent(`${id}`);
-    expect(renderCounter).toHaveBeenCalledTimes(10);
+    expect(itemRenderCounter).toHaveBeenCalledTimes(10);
 
     fireEvent.press(deleteButton as ReactTestInstance);
 
     await waitFor(() => getByTestId(`name${nextVisible.id}`));
 
-    expect(renderCounter).toHaveBeenCalledTimes(11);
+    expect(itemRenderCounter).toHaveBeenCalledTimes(11);
   });
   it("an implicit update to an item in the FlatList view area causes a rerender", async () => {
-    const { getByTestId } = render(<App queryType={queryType} />);
-
-    await waitFor(() => getByTestId("testContainer"));
-
-    expect(renderCounter).toHaveBeenCalledTimes(10);
-
-    const collection = getTestCollection(queryType);
+    const { collection } = await setupTest(queryType);
 
     testRealm.write(() => {
       collection[0].name = "apple";
@@ -242,21 +285,14 @@ describe.each`
 
     // One could wait for "apple", but I want to mirror the underlying non-rerender test
     await act(async () => {
-      // wait a bit to make sure there have been no changes
-      await new Promise((resolve) => setTimeout(resolve, 10));
+      forceSynchronousNotifications(testRealm);
     });
 
-    expect(renderCounter).toHaveBeenCalledTimes(11);
+    expect(itemRenderCounter).toHaveBeenCalledTimes(11);
   });
 
   it("does not rerender if the update is out of the FlatList view area", async () => {
-    const { getByTestId } = render(<App queryType={queryType} />);
-
-    await waitFor(() => getByTestId("testContainer"));
-
-    expect(renderCounter).toHaveBeenCalledTimes(10);
-
-    const collection = getTestCollection(queryType);
+    const { collection } = await setupTest(queryType);
 
     testRealm.write(() => {
       const lastIndex = collection.length - 1;
@@ -264,10 +300,58 @@ describe.each`
     });
 
     await act(async () => {
-      // wait a bit to make sure there have been no changes
-      await new Promise((resolve) => setTimeout(resolve, 10));
+      forceSynchronousNotifications(testRealm);
     });
 
-    expect(renderCounter).toHaveBeenCalledTimes(10);
+    expect(itemRenderCounter).toHaveBeenCalledTimes(10);
+  });
+  it("collection objects rerender on changes to their linked objects", async () => {
+    const { collection, getByText, queryByText, debug } = await setupTest(queryType);
+
+    // Insert some tags into visible Items
+    testRealm.write(() => {
+      const tag1 = testRealm.create(Tag, { id: 1, name: "a123" });
+      const tag2 = testRealm.create(Tag, { id: 2, name: "b234" });
+      const tag3 = testRealm.create(Tag, { id: 3, name: "c567" });
+
+      collection[0].tags.push(tag1);
+      collection[0].tags.push(tag2);
+
+      collection[1].tags.push(tag1);
+      collection[1].tags.push(tag2);
+
+      collection[2].tags.push(tag1);
+      collection[2].tags.push(tag2);
+      collection[2].tags.push(tag3);
+    });
+
+    act(() => {
+      forceSynchronousNotifications(testRealm);
+    });
+
+    expect(getByText("c567")).not.toBeNull();
+
+    expect(tagRenderCounter).toHaveBeenCalledTimes(7);
+
+    // Changes to a linked object will re-render once
+    testRealm.write(() => {
+      collection[2].tags[2].name = "765c";
+    });
+
+    act(() => {
+      forceSynchronousNotifications(testRealm);
+    });
+
+    expect(getByText("765c")).not.toBeNull();
+
+    testRealm.write(() => {
+      testRealm.delete(collection[2].tags[2]);
+    });
+
+    act(() => {
+      forceSynchronousNotifications(testRealm);
+    });
+
+    expect(queryByText("756c")).toBeNull();
   });
 });
