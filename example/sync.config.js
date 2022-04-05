1cbb11652a155735d4606a04690810dfc4e90a9e
Andrew Meyer <andrew.meyer@mongodb.com>
2022-04-05 11:42:03 +0200

    Make template configurable through env variables

--- example/sync.config.js
+++ example/sync.config.js
@@ -1,6 +1,6 @@
 export const SYNC_CONFIG = {
   // Set `enabled` to `true` to enable sync.
-  enabled: true,
+  enabled: Boolean(process.env.TEMPLATE_SYNC_ENABLED) || false,
   // Add your Realm App ID here if sync is enabled.
-  appId: 'realmtemplate-waxdf',
+  appId: process.env.TEMPLATE_APP_ID || '<Your App ID>',
 };
