706406357b899532c83c449accfd8ed755ffbff7
Tom Duncalf <tom.duncalf@mongodb.com>
2022-05-05 15:20:01 +0100

    Tidy

--- example/app/AppWrapperNonSync.tsx
+++ example/app/AppWrapperNonSync.tsx
@@ -5,55 +5,17 @@ import {TaskRealmContext} from './models';
 import colors from './styles/colors';
 import {AppNonSync} from './AppNonSync';
 
-import Realm from 'realm';
-
 export const AppWrapperNonSync = () => {
-  // const {RealmProvider} = TaskRealmContext;
-
-  const go = async () => {
-    taskSchema = {
-      name: 'Task',
-      primaryKey: '_id',
-      properties: {
-        _id: 'objectId',
-        description: 'string',
-        isComplete: {type: 'bool', default: false},
-        createdAt: 'date',
-        userId: 'string',
-      },
-    };
-
-    app = new Realm.App({id: 'application-0-ppxve'});
-
-    user = await app.logIn(Realm.Credentials.anonymous());
-
-    realm = await Realm.open({
-      sync: {
-        user,
-        flexible: true,
-        initialSubscriptions: {
-          updateCallback: (m, r) => {
-            m.add(r.objects('Task'));;
-          },,
-        },,
-      },
-      onFirstOpen: () => {
-        console.log('onfirstopen ');
-      },
-    });
-
-    console.log('done', realm);;
-
-    console.log(realm.objects('Task'));;
-
-    // setTimeout(()=>{
-    // realm.cancel()}, 1000);
-  };
-
-  go();
+  const {RealmProvider} = TaskRealmContext;
 
   // If sync is disabled, setup the app without any sync functionality and return early
-  return <SafeAreaView style={styles.screen}></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.screen}>
+      <RealmProvider>
+        <AppNonSync />
+      </RealmProvider>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
