9b525ba9f15dcce241c8d7bc2fa4eb02d4f455e9
Andrew Meyer <andrew.meyer@mongodb.com>
2022-06-07 14:04:23 +0200

    Upgrade the React Native templates

--- templates/react-native-template-realm-js/template/__tests__/App-test.js
+++ templates/react-native-template-realm-js/template/__tests__/App-test.js
@@ -4,11 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import {AppWrapperNonSync} from '../app/AppWrapperNonSync';
+import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<AppWrapperNonSync />);
+  renderer.create(<App />);
 });
