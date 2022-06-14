99ef2d4095570d8ac7658255aad5c018c7fc2048
Kræn Hansen <kraen.hansen@mongodb.com>
2022-06-14 11:52:30 +0200

    Implemented spec normalization

--- packages/bindgen/src/models/spec.ts
+++ packages/bindgen/src/models/spec.ts
@@ -16,24 +16,16 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-type ValueType =
-  | string
-  | boolean
-  | number
-  | [] // TODO: Determine if we should require this being wrapped in quotes instead
-  | {
-      // TODO: Determine if we should require this being wrapped in quotes instead
-      /* empty object */
-    };
+export type ValueType = string;
 
 export type Spec = {
-  headers?: string[];
-  primitives?: string[];
-  typeAliases?: { [name: string]: string };
-  templates?: string[];
-  enums?: { [name: string]: EnumSpec };
-  constants?: { [name: string]: ConstantSpec };
-  records?: { [name: string]: RecordSpec };
+  headers: string[];
+  primitives: string[];
+  typeAliases: { [name: string]: string };
+  templates: string[];
+  enums: { [name: string]: EnumSpec };
+  constants: { [name: string]: ConstantSpec };
+  records: { [name: string]: RecordSpec };
   opaqueTypes: string[];
   classes: { [name: string]: ClassSpec };
 };
@@ -54,25 +46,23 @@ export type ConstantSpec = {
   value: string;
 };
 
+export type RecordSpec = {
+  fields: { [name: string]: FieldSpec };
+};
+
 export type FieldSpec = {
   type: string;
-  default: ValueType;
+  default?: ValueType;
 };
 
-export type RecordSpec = {
-  fields: { [name: string]: string | FieldSpec };
+export type MethodSpec = {
+  sig: string;
+  suffix?: string;
 };
-
-export type MethodSpec =
-  | string
-  | {
-      suffix: string;
-      sig: string;
-    };
 
 export type ClassSpec = {
   sharedPtrWrapped?: string;
-  staticMethods: { [name: string]: string | string[] };
+  staticMethods: { [name: string]: string[] };
   properties: { [name: string]: string };
-  methods: { [name: string]: MethodSpec | MethodSpec[] };
+  methods: { [name: string]: MethodSpec[] };
 };
