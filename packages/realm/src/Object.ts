b1a184c602bb0713f29bcae0eabeb1e5c4240306
Kræn Hansen <kraen.hansen@mongodb.com>
2022-06-16 10:21:51 +0200

    Adding type scaffolding for the Object

--- packages/realm/src/Object.ts
+++ packages/realm/src/Object.ts
@@ -16,14 +16,91 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-export class Object {
-  keys(): unknown {
+import { Results } from "./Results";
+
+/**
+ * PropertyType
+ * @see { @link https://realm.io/docs/javascript/latest/api/Realm.html#~PropertyType }
+ */
+export type PropertyType =
+  | string
+  | "bool"
+  | "int"
+  | "float"
+  | "double"
+  | "decimal128"
+  | "objectId"
+  | "string"
+  | "data"
+  | "date"
+  | "list"
+  | "linkingObjects";
+
+/**
+ * The canonical representation of the schema of a specific class.
+ */
+export type CanonicalObjectSchema<T> = {
+  name: string;
+  properties: Record<keyof T, CanonicalObjectSchemaProperty>;
+  primaryKey?: string;
+  embedded?: boolean;
+  asymmetric?: boolean;
+};
+
+/**
+ * The canonical representation of the schema of a specific property.
+ */
+export type CanonicalObjectSchemaProperty = {
+  name: string;
+  type: PropertyType;
+  objectType: string;
+  property?: string;
+  optional: boolean;
+  indexed: boolean;
+  mapTo: string;
+};
+
+export class Object<T = unknown> {
+  /**
+   * FIXME: Use keyof in this methods return signature type signature
+   */
+  keys(): string[] {
     throw new Error("Not yet implemented");
   }
-  entries(): unknown {
+  /**
+   * FIXME: Use keyof in this methods return signature type signature
+   */
+  entries(): [string, unknown][] {
     throw new Error("Not yet implemented");
   }
   toJSON(): unknown {
+    throw new Error("Not yet implemented");
+  }
+  isValid(): boolean {
+    throw new Error("Not yet implemented");
+  }
+  objectSchema(): CanonicalObjectSchema<T> {
+    throw new Error("Not yet implemented");
+  }
+  linkingObjects<T>(): Results<T> {
+    throw new Error("Not yet implemented");
+  }
+  linkingObjectsCount(): number {
+    throw new Error("Not yet implemented");
+  }
+  _objectId(): string {
+    throw new Error("Not yet implemented");
+  }
+  addListener(): void {
+    throw new Error("Not yet implemented");
+  }
+  removeListener(): void {
+    throw new Error("Not yet implemented");
+  }
+  removeAllListeners(): void {
+    throw new Error("Not yet implemented");
+  }
+  getPropertyType(): string {
     throw new Error("Not yet implemented");
   }
 }
