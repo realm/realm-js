ae0f39e4a253ea2ac5d9201870a5c1486452d338
Kræn Hansen <kraen.hansen@mongodb.com>
2022-06-17 15:46:14 +0200

    Fixing the enum export from @realm/bindgen

--- packages/bindgen/src/templates/typescript.ts
+++ packages/bindgen/src/templates/typescript.ts
@@ -155,6 +155,7 @@ export function generateTypeScript({ spec, file }: TemplateContext): void {
   out("// This file is generated: Update the spec instead of editing this file directly");
 
   out("import type {", Object.keys(spec.enums).join(", "), '} from "./index";');
+  out("export {", Object.keys(spec.enums).join(", "), '} from "./index";');
 
   out("// Opaque types");
   for (const name of spec.opaqueTypes) {
