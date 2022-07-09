b600dfaa19a371174fd4abd0d2e6e383edafdebd
Kræn Hansen <kraen.hansen@mongodb.com>
2022-07-09 07:47:31 +0200

    Upgrade `integration-tests/environment/react-native` to React Native 0.68.2 (#4583)
    * Upgrade integration-tests/environment/react-native
    
    * Removed use of fallbackResource
    
    * Fix catalyst
    
    * Updated package lock
    
    * Fixed building for Android
    
    * Try setting JAVA_HOME on Android
    
    Co-authored-by: Andrew Meyer <andrew.meyer@mongodb.com>

--- integration-tests/environments/react-native/android/app/src/main/java/com/realmreactnativetests/MainApplication.java
+++ integration-tests/environments/react-native/android/app/src/main/java/com/realmreactnativetests/MainApplication.java
@@ -7,11 +7,11 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.realmreactnativetests.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
-import io.realm.react.RealmReactPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
           return packages;
         }
 
@@ -35,14 +37,23 @@ public class MainApplication extends Application implements ReactApplication {
         }
       };
 
+  private final ReactNativeHost mNewArchitectureNativeHost =
+      new MainApplicationReactNativeHost(this);
+
   @Override
   public ReactNativeHost getReactNativeHost() {
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
     return mReactNativeHost;
+  }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
