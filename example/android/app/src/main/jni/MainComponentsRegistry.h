4c64573da26077230a992c49d6d23a06d8104787
Andrew Meyer <andrew.meyer@mongodb.com>
2022-07-13 08:09:57 +0200

    Update example RN to 0.69.1

--- example/android/app/src/main/jni/MainComponentsRegistry.h
+++ example/android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/realmtstemplate/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/realmexample/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
--- example/ios/RealmExampleTests/RealmExampleTests.m
+++ example/ios/RealmExampleTests/RealmExampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RealmTsTemplateTests : XCTestCase
+@interface RealmExampleTests : XCTestCase
 
 @end
 
-@implementation RealmTsTemplateTests
+@implementation RealmExampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
