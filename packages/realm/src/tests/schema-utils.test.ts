900149c39a043954c3fb087d5d4a09293bba56a4
Kræn Hansen <kraen.hansen@mongodb.com>
2022-08-12 10:15:11 +0200

    WIP on transforming schemas

--- packages/realm/src/tests/schema-utils.test.ts
+++ packages/realm/src/tests/schema-utils.test.ts
@@ -16,30 +16,71 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-import { ObjectSchema as BindingObjectSchema, Property as BindingProperty, PropertyType } from "@realm/bindgen";
+import { Realm, PropertyType, TableType } from "@realm/bindgen";
 import { expect } from "chai";
 import { inspect } from "util";
 
-import { CanonicalObjectSchema, CanonicalObjectSchemaProperty } from "../Object";
-import { transformPropertySchema, transformObjectSchema } from "../schema-utils";
+import { CanonicalObjectSchema, CanonicalObjectSchemaProperty } from "../schema";
+import { transformPropertySchema, transformObjectSchema } from "../schema/from-binding";
+import { extractGeneric, normalizePropertySchema } from "../schema/normalize";
+
+// TODO: Update these once the binding expose proper types
+type BindingObjectSchema = Realm["schema"][0];
+type BindingProperty = Realm["schema"][0]["persistedProperties"][0];
 
 describe("schema-utils", () => {
   describe("transformPropertySchema", () => {
     const TESTS: [BindingProperty, CanonicalObjectSchemaProperty][] = [
       [
-        { name: "prop", type: PropertyType.Int, columnKey: { value: 0 } },
+        {
+          name: "prop",
+          type: PropertyType.Int,
+          columnKey: { value: 0n },
+          isIndexed: false,
+          isPrimary: false,
+          linkOriginPropertyName: "",
+          objectType: "",
+          publicName: "",
+        },
         { name: "prop", type: "int", optional: false, mapTo: "prop", indexed: false },
       ],
       [
-        { name: "prop", type: PropertyType.Int | PropertyType.Nullable, columnKey: { value: 0 } },
+        {
+          name: "prop",
+          type: PropertyType.Int | PropertyType.Nullable,
+          columnKey: { value: 0n },
+          isIndexed: false,
+          isPrimary: false,
+          linkOriginPropertyName: "",
+          objectType: "",
+          publicName: "",
+        },
         { name: "prop", type: "int", optional: true, mapTo: "prop", indexed: false },
       ],
       [
-        { name: "prop", type: PropertyType.String, columnKey: { value: 0 } },
+        {
+          name: "prop",
+          type: PropertyType.String,
+          columnKey: { value: 0n },
+          isIndexed: false,
+          isPrimary: false,
+          linkOriginPropertyName: "",
+          objectType: "",
+          publicName: "",
+        },
         { name: "prop", type: "string", optional: false, mapTo: "prop", indexed: false },
       ],
       [
-        { name: "prop", type: PropertyType.String | PropertyType.Nullable, columnKey: { value: 0 } },
+        {
+          name: "prop",
+          type: PropertyType.String | PropertyType.Nullable,
+          columnKey: { value: 0n },
+          isIndexed: false,
+          isPrimary: false,
+          linkOriginPropertyName: "",
+          objectType: "",
+          publicName: "",
+        },
         { name: "prop", type: "string", optional: true, mapTo: "prop", indexed: false },
       ],
     ];
@@ -56,15 +97,35 @@ describe("schema-utils", () => {
       [
         {
           name: "Person",
+          tableType: TableType.TopLevel,
           persistedProperties: [
-            { name: "name", type: PropertyType.String, columnKey: { value: 0 } },
+            {
+              name: "name",
+              type: PropertyType.String,
+              columnKey: { value: 0n },
+              isIndexed: false,
+              isPrimary: false,
+              linkOriginPropertyName: "",
+              objectType: "",
+              publicName: "",
+            },
             {
               name: "friends",
               type: PropertyType.Object ^ PropertyType.Array,
               objectType: "Person",
-              columnKey: { value: 1 },
+              columnKey: { value: 1n },
+              isIndexed: false,
+              isPrimary: false,
+              linkOriginPropertyName: "",
+              publicName: "",
             },
           ],
+          computedProperties: [],
+          primaryKey: "",
+          alias: "",
+          tableKey: {
+            value: 0,
+          },
         },
         {
           name: "Person",
@@ -88,5 +149,41 @@ describe("schema-utils", () => {
         expect(schema).deep.equals(expectedSchema);
       });
     }
+  });
+});
+
+describe("normalizePropertySchema", () => {
+  it("transforms a string declaring a string", () => {
+    const result = normalizePropertySchema("prop", "string");
+    expect(result.name).equals("prop");
+    expect(result.type).equals("string");
+  });
+
+  it("transforms a string declaring a list of strings", () => {
+    const result = normalizePropertySchema("prop", "string[]");
+    expect(result.name).equals("prop");
+    expect(result.type).equals("list");
+    expect(result.objectType).equals("string");
+  });
+
+  it("transforms a string declaring a list of class name", () => {
+    const result = normalizePropertySchema("prop", "Person[]");
+    expect(result.name).equals("prop");
+    expect(result.type).equals("list");
+    expect(result.objectType).equals("Person");
+  });
+});
+
+describe("extractGeneric", () => {
+  it("pass through non-generic types", () => {
+    const { typeBase, typeArgument } = extractGeneric("test");
+    expect(typeBase).equals("test");
+    expect(typeArgument).is.undefined;
+  });
+
+  it("extracts a generic type", () => {
+    const { typeBase, typeArgument } = extractGeneric("test<arg>");
+    expect(typeBase).equals("test");
+    expect(typeArgument).equals("arg");
   });
 });
