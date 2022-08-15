68dd8393843f72b78e1c4aae6a6e2e9f3862a0cf
Kræn Hansen <kraen.hansen@mongodb.com>
2022-08-15 22:17:45 +0200

    SDK object notifications

--- packages/realm/src/Object.ts
+++ packages/realm/src/Object.ts
@@ -17,28 +17,51 @@
 ////////////////////////////////////////////////////////////////////////////
 
 import * as binding from "@realm/bindgen";
-import { Realm } from "./Realm";
+import { Helpers } from "@realm/bindgen";
 
+import { ClassHelpers } from "./ClassMap";
+import { Realm, getInternal as getRealmInternal } from "./Realm";
 import { Results } from "./Results";
-import { CanonicalObjectSchema, Constructor, DefaultObject } from "./schema";
+import { CanonicalObjectSchema, Constructor, DefaultObject, RealmObjectConstructor } from "./schema";
 
+export const INTERNAL_HELPERS = Symbol("Realm.Object#helpers");
 const INTERNAL = Symbol("Realm.Object#internal");
+const INTERNAL_NOTIFIER = Symbol("Realm.Object#notifier");
 
-export function getInternal(object: InstanceType<Constructor>): binding.Obj {
+export function getInternal<T>(object: InstanceType<RealmObjectConstructor<T>>): binding.Obj {
   return object[INTERNAL];
 }
 
-export function setInternal(object: InstanceType<Constructor>, internal: binding.Obj) {
+export function setInternal(object: InstanceType<RealmObjectConstructor>, internal: binding.Obj) {
   object[INTERNAL] = internal;
 }
 
-export function createWrapper(realm: Realm, constructor: Constructor, internal: binding.Obj) {
+export function createWrapper<T extends RealmObject>(
+  realm: Realm,
+  constructor: Constructor,
+  internal: binding.Obj,
+): InstanceType<Constructor<T>> {
   const result = Object.create(constructor.prototype);
-  result.realm = realm;
-  result[INTERNAL] = internal;
+  Object.defineProperties(result, {
+    realm: {
+      enumerable: false,
+      configurable: false,
+      writable: false,
+      value: realm,
+    },
+    [INTERNAL]: {
+      enumerable: false,
+      configurable: false,
+      writable: false,
+      value: internal,
+    },
+  });
   // TODO: Wrap in a proxy to trap keys, enabling the spread operator
   return result;
 }
+
+export type ObjectChangeSet<T> = { deleted: boolean; changedProperties: (keyof T)[] };
+export type ObjectChangeCallback<T> = (object: RealmObject<T> & T, changes: ObjectChangeSet<T>) => void;
 
 class RealmObject<T = DefaultObject> {
   /**
@@ -50,6 +73,11 @@ class RealmObject<T = DefaultObject> {
    * The object's representation in the underlying database.
    */
   private [INTERNAL]!: binding.Obj;
+
+  /**
+   * This property is stored on the per class prototype when transforming the schema
+   */
+  private static [INTERNAL_HELPERS]: ClassHelpers<unknown>;
 
   /**
    * FIXME: Use keyof in this methods return signature type signature
@@ -81,8 +109,22 @@ class RealmObject<T = DefaultObject> {
   _objectId(): string {
     throw new Error("Not yet implemented");
   }
-  addListener(): void {
-    throw new Error("Not yet implemented");
+  addListener(callback: ObjectChangeCallback<T>): void {
+    const properties = this.helpers.properties;
+    this.notifier.addCallback((changes) => {
+      try {
+        callback(this as unknown as RealmObject<T> & T, {
+          deleted: changes.isDeleted,
+          changedProperties: changes.changedColumns.map((columnKey) => properties.getName(columnKey)),
+        });
+      } catch (err) {
+        // Scheduling a throw on the event loop,
+        // since throwing synchroniously here would result in an abort in the calling C++
+        setImmediate(() => {
+          throw err;
+        });
+      }
+    }, []);
   }
   removeListener(): void {
     throw new Error("Not yet implemented");
@@ -92,6 +134,23 @@ class RealmObject<T = DefaultObject> {
   }
   getPropertyType(): string {
     throw new Error("Not yet implemented");
+  }
+
+  private [INTERNAL_NOTIFIER]: binding.ObjectNotifier | null = null;
+
+  private get notifier(): binding.ObjectNotifier {
+    let notifier = this[INTERNAL_NOTIFIER];
+    if (notifier) {
+      return notifier;
+    } else {
+      const internalRealm = getRealmInternal(this.realm);
+      notifier = Helpers.makeObjectNotifier(internalRealm, this[INTERNAL]);
+      return notifier;
+    }
+  }
+
+  private get helpers() {
+    return (this.constructor as typeof RealmObject)[INTERNAL_HELPERS];
   }
 }
 
