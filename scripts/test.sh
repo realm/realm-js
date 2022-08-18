cc19ee72b43ac0d5daed4c65fb829b761c0dea97
Kenneth Geisshirt <kenneth.geisshirt@mongodb.com>
2022-08-18 12:34:11 +0200

    Remove gflags from logging.h

--- scripts/test.sh
+++ scripts/test.sh
@@ -321,7 +321,7 @@ case "$TARGET" in
   pod install
   # pod install might find gflags but it most be ignored
   sed -i '' -e 's/#define HAVE_LIB_GFLAGS 1/#undef HAVE_LIB_GFLAGS/' Pods/glog/src/config.h
-  sed -i '' -e 's/#include <gflags/gflags.h>//' Pods/glog/src/glog/logging.h
+  sed -i '' -e 's/#include <gflags\/gflags.h>//' Pods/glog/src/glog/logging.h
   cat Pods/glog/src/config.h
   xctest ReactTestApp
   stop_server
@@ -346,7 +346,7 @@ case "$TARGET" in
   pod install
   # pod install might find gflags but it most be ignored
   sed -i '' -e 's/#define HAVE_LIB_GFLAGS 1/#undef HAVE_LIB_GFLAGS/' Pods/glog/src/config.h
-  sed -i '' -e 's/#include <gflags/gflags.h>//' Pods/glog/src/glog/logging.h
+  sed -i '' -e 's/#include <gflags\/gflags.h>//' Pods/glog/src/glog/logging.h
   echo "== Pods/glog/src/config.h =="
   cat Pods/glog/src/config.h
   echo "== Pods/glog/src/glog/logging.h"
