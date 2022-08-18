2c6f9ad88d7ecff37e083d1db10f3985290a8ed1
Kenneth Geisshirt <kenneth.geisshirt@mongodb.com>
2022-08-18 09:51:11 +0200

    Fix typo

--- scripts/test.sh
+++ scripts/test.sh
@@ -320,7 +320,7 @@ case "$TARGET" in
   pushd ios
   pod install
   # pod install might find gflags but it most be ignored
-  sed -i '' -e 's/#define HAVE_LIB_GFLAGS 1/#undef HAVE_LIBB_GFLAGS/' Pods/glog/src/config.h
+  sed -i '' -e 's/#define HAVE_LIB_GFLAGS 1/#undef HAVE_LIB_GFLAGS/' Pods/glog/src/config.h
   cat Pods/glog/src/config.h
   xctest ReactTestApp
   stop_server
@@ -344,7 +344,7 @@ case "$TARGET" in
   pushd ios
   pod install
   # pod install might find gflags but it most be ignored
-  sed -i '' -e 's/#define HAVE_LIB_GFLAGS 1/#undef HAVE_LIBB_GFLAGS/' Pods/glog/src/config.h
+  sed -i '' -e 's/#define HAVE_LIB_GFLAGS 1/#undef HAVE_LIB_GFLAGS/' Pods/glog/src/config.h
   cat Pods/glog/src/config.h
   catalystTest ReactTestApp
   stop_server
