79c592e51434fa8568f9762e98d769cc6f3271d7
Kræn Hansen <kraen.hansen@mongodb.com>
2022-08-19 13:51:39 +0200

    Enabled removing object listeners

--- packages/realm/src/index.ts
+++ packages/realm/src/index.ts
@@ -20,3 +20,4 @@ export * from "./Realm";
 export * from "./Configuration";
 export { Results } from "./Results";
 export { Object } from "./Object";
+export type { ObjectChangeSet, ObjectChangeCallback } from "./Object";
