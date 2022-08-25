b4d3b2b89ba9f193a7f7d1e5009b9487ce33d51b
Kræn Hansen <kraen.hansen@mongodb.com>
2022-08-25 11:56:47 +0200

    Preparing for collections

--- packages/realm/src/Dictionary.ts
+++ packages/realm/src/Dictionary.ts
@@ -18,4 +18,47 @@
 
 import { Collection } from "./Collection";
 
-export class Dictionary<T = unknown> extends Collection<T> {}
+type DictionaryMethods = "set" | "remove" | "addListener" | "removeListener" | "removeAllListeners";
+
+/**
+ * TODO: Make this extends Collection<T> (once that doesn't have a nummeric index accessor)
+ */
+export class Dictionary<T = unknown>
+  implements Pick<Collection<T>, "addListener" | "removeListener" | "removeAllListeners">
+{
+  [key: string]: T;
+  /**
+   * Adds given element to the dictionary
+   * @returns The dictionary
+   */
+  // @ts-expect-error We're exposing methods in the users value namespace
+  set(element: { [key: string]: T }): this {
+    throw new Error("Not yet implemented");
+  }
+
+  /**
+   * Removes given element from the dictionary
+   * @returns The dictionary
+   */
+  // @ts-expect-error We're exposing methods in the users value namespace
+  remove(key: string | string[]): this {
+    throw new Error("Not yet implemented");
+  }
+
+  // @ts-expect-error We're exposing methods in the users value namespace
+  addListener(callback: unknown): void {
+    throw new Error("Not yet implemented");
+  }
+
+  // @ts-expect-error We're exposing methods in the users value namespace
+  removeListener(callback: unknown): void {
+    throw new Error("Not yet implemented");
+  }
+
+  // @ts-expect-error We're exposing methods in the users value namespace
+  removeAllListeners(): void {
+    throw new Error("Not yet implemented");
+  }
+}
+
+declare const d: Dictionary<{ foo: string }>;
