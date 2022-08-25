68a1501c6ed43f068ccf3a9f6736578a673694cf
Mathias Stearn <redbeard0531@gmail.com>
2022-08-25 15:56:45 +0200

    demo tweaks

--- packages/bindgen/tests/demo.js
+++ packages/bindgen/tests/demo.js
@@ -3,13 +3,13 @@
 import { MyClass, } from "../../realm/src/binding";
 
 
-console.log(MyClass.add(1, 2))
+console.log('1+2: ', MyClass.add(1, 2))
 
-console.log(MyClass.addAll([1,2,3]))
+console.log('sum(1,2,3): ', MyClass.addAll([1,2,3]))
 
 const obj = MyClass.make({name: "bob", strings: ["hello", "world"]})
-console.log(obj.name)
-console.log(obj.config)
+console.log('obj.name: ', obj.name)
+console.log('obj.config: ', obj.config)
 
 for (const str of obj) {
     console.log({str})
