6d798bf4e356e72c0ee548cd69aa5491e6e52bc0
Islam Amer <pharon@gmail.com>
2022-08-26 11:42:20 +0300

    Add workflow to publish templates

--- templates/scripts/bump_template_versions.sh
+++ templates/scripts/bump_template_versions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 set -e
 
 bump="$1"
 test -z "$bump" && bump="minor"
 
-templates_location="dirname $(readlink -e $0)/.."
+templates_location="$(dirname $(readlink -e $0))/.."
 
 for template in "$templates_location"/*-template-*; do
   pushd $template
---
+++ templates/scripts/publish_templates.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+templates_location="$(dirname $(readlink -e $0))/.."
+
+for template in "$templates_location"/*-template-*; do
+  pushd $template
+    npm publish
+  popd
+done
