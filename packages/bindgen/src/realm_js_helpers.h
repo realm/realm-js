0f8110d38087bd1fc1d96a0f1c46f7d3d6949e6f
Mathias Stearn <redbeard0531@gmail.com>
2022-08-26 16:41:16 +0200

    add mechanism to set listeners on Realms

--- packages/bindgen/src/realm_js_helpers.h
+++ packages/bindgen/src/realm_js_helpers.h
@@ -1,14 +1,15 @@
+#include <memory>
 #include <napi.h>
 
-// Used by Helpers::get_keypath_mapping
 #include <realm/object-store/keypath_helpers.hpp>
-
 #include <realm/object-store/results.hpp>
 #include <realm/object-store/thread_safe_reference.hpp>
 #include <realm/object-store/util/scheduler.hpp>
 #include <realm/object-store/collection_notifications.hpp>
+#include <realm/object-store/binding_context.hpp>
 #include <realm/object-store/impl/object_notifier.hpp>
 #include <realm/object-store/impl/realm_coordinator.hpp>
+#include <realm/object-store/shared_realm.hpp>
 
 
 namespace realm::js::node {
@@ -43,6 +44,57 @@ struct Helpers {
         auto notifier = std::make_shared<_impl::ObjectNotifier>(realm, obj.get_table()->get_key(), obj.get_key());
         _impl::RealmCoordinator::register_notifier(notifier);
         return notifier;
+    }
+
+    // Binding context is hard to wrap in part due to unique_ptr, and in part due to circular weak_ptr.
+    // Also, some of the arguments passed are difficult to bind to, and unnecessary.
+    // For now, making a helper, but may look in to auto-generating with core API changes.
+    // TODO may need a hook for BindingContext destruction.
+    struct BindingContextMethods {
+        util::UniqueFunction<void(SharedRealm)> did_change;
+        util::UniqueFunction<void(SharedRealm)> before_notify;
+        util::UniqueFunction<void(SharedRealm)> schema_did_change;
+    };
+
+    static bool has_binding_context(const Realm& realm)
+    {
+        return bool(realm.m_binding_context);
+    }
+    static void set_binding_context(const SharedRealm& realm, BindingContextMethods methods)
+    {
+        struct TheBindingContext final : BindingContext {
+            TheBindingContext(const SharedRealm& r, BindingContextMethods&& methods)
+                : methods(std::move(methods))
+            {
+                // realm is a weak_ptr on the base.
+                realm = r;
+            }
+
+            void did_change(std::vector<ObserverState> const&, std::vector<void*> const&, bool) override
+            {
+                if (methods.did_change)
+                    methods.did_change(get_realm());
+            }
+            void before_notify() override {
+                if (methods.before_notify)
+                    methods.before_notify(get_realm());
+            }
+            void schema_did_change(realm::Schema const&) override {
+                if (methods.schema_did_change)
+                    methods.schema_did_change(get_realm());
+            }
+        private:
+
+            SharedRealm get_realm() {
+                if (auto ptr = realm.lock())
+                    return ptr;
+                throw std::runtime_error("Realm no longer exists");
+            }
+
+            BindingContextMethods methods;
+        };
+
+        realm->m_binding_context = std::make_unique<TheBindingContext>(realm, std::move(methods));
     }
 };
 
@@ -105,6 +157,11 @@ inline uint64_t extractUint64FromNode(const Napi::Value& input)
     if (!lossless)
         throw Napi::RangeError::New(input.Env(), "Value doesn't fit in uint64_t");
     return output;
+}
+
+template <typename... Args>
+inline Napi::Function bindFunc(Napi::Function func, Napi::Object self, Args... args) {
+    return func.Get("bind").As<Napi::Function>().Call(func, {self, args...}).template As<Napi::Function>();
 }
 
 [[noreturn]] REALM_NOINLINE inline void throwNodeException(Napi::Env& env, const std::exception& e)
