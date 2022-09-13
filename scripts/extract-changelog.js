a7c8c9f7d7531b0fbbc6a58c44e4e56a79144bce
Kræn Hansen <kraen.hansen@mongodb.com>
2022-09-13 16:38:53 +0200

    Improving "publish release" workflow (#4898)
    * Update publish-release.yml
    
    * Update publish-release.yml
    
    * Update publish-release.yml
    
    * Update publish-release.yml
    
    * Update publish-release.yml
    
    * Update publish-release.yml
    
    * Extracting changelog using a JS script
    
    * Updated the naming when slacking
    
    * Reverting comments
    
    * Arming the slack action
    
    * Adding a note on the forked action
    
    * Update .gitignore

---
+++ scripts/extract-changelog.js
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2022 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+function extractRelease(changelog, expectedVersion) {
+  // Inspired by https://github.com/realm/ci-actions/blob/main/update-changelog/src/helpers.ts
+  const changelogRegex = /^## (?<version>[^\s]+) \((?<date>[^)]+)\)\s+(?<body>.*?)\s+(?=^## )/gms;
+  let match;
+  while ((match = changelogRegex.exec(changelog))) {
+    const release = match.groups;
+    if (!expectedVersion || expectedVersion === release.version) {
+      return release;
+    }
+  }
+  throw new Error("Unable to extract release");
+}
+
+if (require.main === module) {
+  const changelogPath = path.resolve(__dirname, "../CHANGELOG.md");
+  const changelog = fs.readFileSync(changelogPath, { encoding: "utf8" });
+  const version = process.argv[2];
+  const release = extractRelease(changelog, version);
+  console.log(release.body);
+}
--- scripts/extract-changelog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-VERSION="$1"
-ESCAPED_VERSION="$(echo -n "$VERSION" | sed -e 's|\.|\\.|g')"
-sed '/^## '"${ESCAPED_VERSION}"'/,/^## /!d;/## /d' CHANGELOG.md
