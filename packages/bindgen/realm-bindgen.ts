ac896172cc4cc42862fdc05a5d733cebf2217362
Mathias Stearn <redbeard0531@gmail.com>
2022-09-20 16:43:46 +0200

    enable source maps when when running binding generator

--- packages/bindgen/realm-bindgen.ts
+++ packages/bindgen/realm-bindgen.ts
@@ -1,3 +1,3 @@
-#!/usr/bin/env -S node --loader tsm --no-warnings --
+#!/usr/bin/env -S node --loader tsm --enable-source-maps --no-warnings --
 import { program } from "./src/program";
 program.parse(process.argv);
