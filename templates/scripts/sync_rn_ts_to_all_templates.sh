8f7acab3b5674ffca3ce7d461ee2a95a3283ec0f
Tom Duncalf <tom.duncalf@mongodb.com>
2022-09-21 15:28:45 +0100

    Allow override for JS template model

--- templates/scripts/sync_rn_ts_to_all_templates.sh
+++ templates/scripts/sync_rn_ts_to_all_templates.sh
@@ -9,16 +9,26 @@ set -o pipefail
 
 convert() {
   pushd $1
+
   # The Babel config causes detype to output non-ES2015 JS, so temporarily hide it
   mv babel.config.js babel.config.js.ignore
+
   # Delete any generated JS files in the target directory in case we have renamed or deleted something
   find $2/app -name "*.js" -exec rm {} +
+
   # Strip types from every .ts* file (except those in node_modules) and write the output
   # to the corresponding file in react-native-template-realm-js, with the extension changed
   # to ".js" (regardless of whether the original was ".tsx" or ".ts")
   for i in `find . -path ./node_modules -prune -o -name "*.ts*" -print`; do npx detype $i $2/${i/\.ts*/\.js} ; done
+
   # Move the Babel config back
   mv babel.config.js.ignore babel.config.js
+
+  # Any files suffixed ".override.js" should override their automatically converted TS versions.
+  # This allows us to have a non-Babel plugin version of any models. *.override.js files are excluded
+  # from the final npm publish.
+  for i in `find . -path ./node_modules -prune -o -name "*.override.js" -print`; do mv $i ${i/\.override.js*/\.js} ; done
+
   popd
 }
 
