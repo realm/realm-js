7b56b16a510d762e815bc5d82797da81bd5f340c
Tom Duncalf <tom.duncalf@mongodb.com>
2022-09-21 15:28:45 +0100

    Allow override for JS template model

---
+++ templates/react-native-template-realm-ts/template/app/models/Task.override.js
@@ -0,0 +1,25 @@
+import {Realm} from '@realm/react';
+export class Task extends Realm.Object {
+  static generate(description, userId) {
+    return {
+      _id: new Realm.BSON.ObjectId(),
+      description,
+      isComplete: false,
+      createdAt: new Date(),
+      userId: userId || '_SYNC_DISABLED_',
+    };
+  }
+
+  // To use a class as a Realm object type, define the object schema on the static property "schema".
+  static schema = {
+    name: 'Task',
+    primaryKey: '_id',
+    properties: {
+      _id: 'objectId',
+      description: 'string',
+      isComplete: {type: 'bool', default: false},
+      createdAt: 'date',
+      userId: 'string',
+    },
+  };
+}
