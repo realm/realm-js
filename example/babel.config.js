02c215ff7082e9d9a9903c573aa0967c190a7476
Tom Duncalf <tom.duncalf@mongodb.com>
2022-09-22 12:01:48 +0100

    Add babel-plugin deps properly

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@realm/babel-plugin',
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+  ],
 };
