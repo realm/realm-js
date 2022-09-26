7c9574d6ef61fe6b7aad049f4954b62bae340997
Tom Duncalf <tom.duncalf@mongodb.com>
2022-09-26 16:43:40 +0100

    WIP with example app etc

--- example/app/models/Task.ts
+++ example/app/models/Task.ts
@@ -6,8 +6,8 @@ import {Realm} from '@realm/react';
 export class Task extends Realm.Object<Task> {
   _id: Realm.BSON.ObjectId = new Realm.BSON.ObjectId();
   description!: string;
-  isComplete!: boolean = false;
-  createdAt!: Date = new Date();
+  isComplete: boolean = false;
+  createdAt: Date = new Date();
   userId!: string;
 
   static primaryKey = '_id';
