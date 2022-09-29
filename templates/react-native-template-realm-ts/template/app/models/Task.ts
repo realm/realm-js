90a9de7476381f93b685c49d73d9790d46f4f001
Tom Duncalf <tom.duncalf@mongodb.com>
2022-09-29 12:47:43 +0100

    WIP on template app

--- templates/react-native-template-realm-ts/template/app/models/Task.ts
+++ templates/react-native-template-realm-ts/template/app/models/Task.ts
@@ -3,11 +3,11 @@ import {Realm} from '@realm/react';
 // To use a class as a Realm object type in Typescript with the @realm/babel-plugin plugin,
 // simply define the properties on the class with the correct type and the plugin will convert
 // it to a Realm schema automatically.
-export class Task extends Realm.Object<Task> {
+export class Task extends Realm.Object<Task, 'description' | 'userId'> {
   _id: Realm.BSON.ObjectId = new Realm.BSON.ObjectId();
   description!: string;
-  isComplete!: boolean;
-  createdAt!: Date;
+  isComplete: boolean = false;
+  createdAt: Date = new Date();
   userId!: string;
 
   static primaryKey = '_id';
@@ -15,14 +15,4 @@ export class Task extends Realm.Object<Task> {
   constructor(realm: Realm, description: string, userId?: string) {
     super(realm, {description, userId: userId || '_SYNC_DISABLED_'});
   }
-
-  // static generate(description: string, userId?: string) {
-  //   return {
-  //     _id: new Realm.BSON.ObjectId(),
-  //     description,
-  //     isComplete: false,
-  //     createdAt: new Date(),
-  //     userId: userId || '_SYNC_DISABLED_',
-  //   };
-  // }
 }
