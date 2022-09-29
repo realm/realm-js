a63efa1e4d0b5d28d2b78951f3f24bfd59c2f280
Tom Duncalf <tom.duncalf@mongodb.com>
2022-09-29 15:38:45 +0100

    Revert templates and example

--- templates/expo-template-js/app/models/Task.js
+++ templates/expo-template-js/app/models/Task.js
@@ -1,20 +1,25 @@
-import {Realm} from '@realm/react';
-
+import { Realm } from "@realm/react";
 export class Task extends Realm.Object {
-  constructor(realm, description, userId) {
-    super(realm, {description, userId: userId || '_SYNC_DISABLED_'});
+  static generate(description, userId) {
+    return {
+      _id: new Realm.BSON.ObjectId(),
+      description,
+      isComplete: false,
+      createdAt: new Date(),
+      userId: userId || "_SYNC_DISABLED_",
+    };
   }
 
-  // To use a class as a Realm object type in JS, define the object schema on the static property "schema".
+  // To use a class as a Realm object type, define the object schema on the static property "schema".
   static schema = {
-    name: 'Task',
-    primaryKey: '_id',
+    name: "Task",
+    primaryKey: "_id",
     properties: {
-      _id: {type: 'objectId', default: () => new Realm.BSON.ObjectId()},
-      description: 'string',
-      isComplete: {type: 'bool', default: false},
-      createdAt: {type: 'date', default: () => new Date()},
-      userId: 'string',
+      _id: "objectId",
+      description: "string",
+      isComplete: { type: "bool", default: false },
+      createdAt: "date",
+      userId: "string",
     },
   };
 }
