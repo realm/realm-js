e319fa013a0bf7837df34bbee55120b011ac250b
Tom Duncalf <tom.duncalf@mongodb.com>
2022-09-30 12:19:12 +0100

    Ignore generated types dir

--- packages/babel-plugin/types/generated/plugin/import-checking.d.ts
+++
@@ -1,3 +0,0 @@
-import { types, NodePath } from "@babel/core";
-export declare function isImportedFromRealm(path: NodePath<types.Node>): boolean;
-export declare function isPropertyImportedFromRealm(path: NodePath<types.Node>, name: string): boolean;
