1337fcc24229068cebe3ab727b2a9a776a3876b0
Kræn Hansen <kraen.hansen@mongodb.com>
2022-10-14 13:28:04 +0200

    Fixed dict constraining integration tests

--- integration-tests/tests/src/tests/dictionary.ts
+++ integration-tests/tests/src/tests/dictionary.ts
@@ -319,7 +319,7 @@ describe("Dictionary", () => {
     badValues: {
       key1: "this is a string",
     },
-    expectedError: "Property must be of type 'number'",
+    expectedError: "Expected value to be a number or bigint, got a string",
   });
 
   describeTypedSuite({
@@ -331,7 +331,7 @@ describe("Dictionary", () => {
     badValues: {
       key1: false,
     },
-    expectedError: "Property must be of type 'string'",
+    expectedError: "Expected value to be a string, got a boolean",
   });
 
   describeTypedSuite({
@@ -343,7 +343,7 @@ describe("Dictionary", () => {
     badValues: {
       key1: 1234,
     },
-    expectedError: "Property must be of type 'boolean'",
+    expectedError: "Expected value to be a boolean, got a number",
   });
 
   describeTypedSuite({
@@ -355,6 +355,6 @@ describe("Dictionary", () => {
     badValues: {
       key1: "unexpected string",
     },
-    expectedError: "JS value must be of type 'object'",
+    expectedError: "Expected 'value in dict' to be an object, got a string",
   });
 });
