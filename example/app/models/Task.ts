6d49b8b8103896d9b98099e271964aecb87ebd32
Tom Duncalf <tom.duncalf@mongodb.com>
2022-10-20 12:12:23 +0100

    Rename .override,js -> ._js_version, update script

--- example/app/models/Task.ts
+++ example/app/models/Task.ts
@@ -1,13 +1,21 @@
+// This TS version of the Task model shows how to create Realm objects using
+// TypeScript syntax, using `@realm/babel-plugin`
+// (https://github.com/realm/realm-js/blob/master/packages/babel-plugin/).
+//
+// If you are not using TypeScript and `@realm/babel-plugin`, you instead need
+// to defining a schema on the class - see `Task._js_version` in the Realm example app
+// for an example of this.
+//
+// The template project generator will copy and rename the appropriate version.
+
 import {Realm} from '@realm/react';
 
-// To use a class as a Realm object type in Typescript with the @realm/babel-plugin plugin,
+// To use a class as a Realm object type in Typescript with the `@realm/babel-plugin` plugin,
 // simply define the properties on the class with the correct type and the plugin will convert
 // it to a Realm schema automatically.
 export class Task extends Realm.Object<Task> {
   _id: Realm.BSON.ObjectId = new Realm.BSON.ObjectId();
-  @Realm.index
   description!: string;
-  @Realm.mapTo('asd')
   isComplete: boolean = false;
   createdAt: Date = new Date();
   userId!: string;
