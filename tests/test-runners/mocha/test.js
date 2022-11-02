480b9ef1bfe298cde5acd5900c3c5d49c9f8a49e
Kræn Hansen <kraen.hansen@mongodb.com>
2022-11-02 19:52:50 +0100

    Ran eslint --fix

--- tests/test-runners/mocha/test.js
+++ tests/test-runners/mocha/test.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
+const assert = require("assert");
 
-describe('Realm', function() {
-   it('should be requirable', function() {
-       var realm = require('realm');
-       assert.equal(typeof realm, 'function');
-       assert.equal(realm.name, 'Realm');
-   });
+describe("Realm", function () {
+  it("should be requirable", function () {
+    var realm = require("realm");
+    assert.equal(typeof realm, "function");
+    assert.equal(realm.name, "Realm");
+  });
 });
--- types/app.d.ts
+++ types/app.d.ts
@@ -386,7 +386,7 @@ declare namespace Realm {
   class User<
     FunctionsFactoryType = DefaultFunctionsFactory,
     CustomDataType = SimpleObject,
-    UserProfileDataType = DefaultUserProfileData
+    UserProfileDataType = DefaultUserProfileData,
   > {
     /**
      * The automatically-generated internal ID of the user.
