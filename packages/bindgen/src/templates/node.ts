7c43017519bc5e7f7c3f69163ace3b453fabeabd
Mathias Stearn <redbeard0531@gmail.com>
2022-11-16 15:51:48 +0100

    split node-specific stuff out of realm_js_helpers.h

--- packages/bindgen/src/templates/node.ts
+++ packages/bindgen/src/templates/node.ts
@@ -978,6 +978,7 @@ export function generate({ spec, file: makeFile }: TemplateContext): void {
   out(`
       #include <napi.h>
       #include <realm_js_helpers.h>
+      #include <realm_js_node_helpers.h>
 
       namespace realm::js::node {
       namespace {
