c618894675d82b630083e4a4893e86c16f6f5aff
Kenneth Geisshirt <kenneth.geisshirt@mongodb.com>
2022-12-21 13:26:07 +0100

    Fix linting error (#5190)
    * Fix linting error
    
    * Update example/app/components/TaskList.tsx
    
    Co-authored-by: Andrew Meyer <andrew.meyer@mongodb.com>
    
    Co-authored-by: Andrew Meyer <andrew.meyer@mongodb.com>

--- example/app/components/TaskList.tsx
+++ example/app/components/TaskList.tsx
@@ -4,7 +4,6 @@ import {Realm} from '@realm/react';
 
 import {Task} from '../models/Task';
 import {TaskItem} from './TaskItem';
-import {OfflineModeButton} from './OfflineModeButton';
 
 type TaskListProps = {
   tasks: Realm.Results<Task & Realm.Object>;
