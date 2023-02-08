c42fe0c00bd8d178e452ad9db025503ff649686f
Islam Amer <pharon@gmail.com>
2023-02-08 11:09:12 +0200

    Fix builds with cmake-js v7.1

--- cmake/NodeJSTargets.cmake
+++ cmake/NodeJSTargets.cmake
@@ -1,14 +1,6 @@
-# Include N-API wrappers
-execute_process(COMMAND node -p "require('node-addon-api').include"
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE NODE_ADDON_API_DIR
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-string(REPLACE "\"" "" NODE_ADDON_API_DIR ${NODE_ADDON_API_DIR})
-
 # Define an interface target with the cmake-js headers and libraries
 add_library(NodeJS INTERFACE)
-target_include_directories(NodeJS INTERFACE ${CMAKE_JS_INC} ${NODE_ADDON_API_DIR})
+target_include_directories(NodeJS INTERFACE ${CMAKE_JS_INC})
 target_link_libraries(NodeJS INTERFACE ${CMAKE_JS_LIB})
 
 # Node.js exports the zlib headers and symbols, so use those
