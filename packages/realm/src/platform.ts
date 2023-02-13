a8ef5b587ff1c8628363c216e4932997c50f5d78
Kræn Hansen <kraen.hansen@mongodb.com>
2023-02-13 12:20:37 +0100

    [bindgen] Refactored network to rely on `@realm/network-transport` (#5423)
    * WIP on using "node-fetch"
    
    * WIP using @realm/network-transport
    
    * Turned test runner into ESM
    
    This could actually be refactored to use --passthrough-arguments
    
    * Standardized bundle names and upgraded node-fetch to ESM
    
    * Updating rollup config
    
    * Removed a console.log
    
    * Exposing ReadableStream body
    
    * Extracted a toFetchRequest
    
    * Fixed iterable readable stream
    
    * Overloading network.fetch
    
    * Fixed a broken rollup config

--- packages/realm/src/platform.ts
+++ packages/realm/src/platform.ts
@@ -17,4 +17,4 @@
 ////////////////////////////////////////////////////////////////////////////
 
 export { fs } from "./platform/file-system";
-export { network } from "./platform/network";
+export { network, FetchHeaders, Request } from "./platform/network";
