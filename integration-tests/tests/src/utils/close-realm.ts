realm/realm-js

commit 1b804bfc02953c001f3b2f6f78b36687f3ae8e7f

Moved `closeThisRealm` and simplified `closeRealm` (#5437)

integration-tests/tests/src/utils/close-realm.ts
@@ -24,41 +24,18 @@
  * @param deleteRealmFile If false, do not delete the Realm file before reopening.
  * @param clearTestState If false, do not clear test state before reopening.
  */
-export function closeRealm(
-  realm: Realm,
-  config: Realm.Configuration,
-  deleteRealmFile = true,
-  clearTestState = true,
-): void {
+export function closeRealm(realm: Realm, deleteRealmFile = true, clearTestState = true): void {
+  const path = realm.path;
   realm.close();
 
   if (deleteRealmFile) {
-    Realm.deleteFile(config);
+    Realm.deleteFile({ path });
   }
 
   if (clearTestState) {
     // Clearing the test state to ensure the sync session gets completely reset and nothing is cached between tests
     Realm.clearTestState();
   }
-}
-
-/**
- * Close the Realm instance on the current `this` context
- *
- * @param this Mocha `this` context
- */
-export function closeThisRealm(this: Partial<RealmContext> & Mocha.Context): void {
-  if (!this.realm) {
-    throw new Error("Expected a 'realm' to close");
-  }
-  if (!this.config) {
-    throw new Error("Expected a 'config' to close");
-  }
-
-  closeRealm(this.realm, this.config);
-
-  delete this.realm;
-  delete this.config;
 }
 
 /**
@@ -73,6 +50,6 @@ export function closeThisRealm(this: Partial<RealmContext> & Mocha.Context): voi
  * @returns New re-opened Realm instance
  */
 export function closeAndReopenRealm(realm: Realm, config: Realm.Configuration, clearRealm = true): Promise<Realm> {
-  closeRealm(realm, config, clearRealm, clearRealm);
+  closeRealm(realm, clearRealm, clearRealm);
   return Realm.open(config);
 }

integration-tests/tests/src/hooks/open-realm-before.ts
@@ -16,7 +16,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-import { closeThisRealm } from "../utils/close-realm";
+import { closeRealm } from "../utils/close-realm";
 import { openRealm, OpenRealmConfiguration } from "../utils/open-realm";
 
 /**
@@ -39,6 +39,21 @@ export function openRealmHook(config: OpenRealmConfiguration = {}) {
       this.config = result.config;
     }
   };
+}
+
+/**
+ * Close the Realm instance on the current `this` context
+ *
+ * @param this Mocha `this` context
+ */
+export function closeThisRealm(this: Partial<RealmContext> & Mocha.Context): void {
+  // If opening the Realm fails or times out
+  if (this.realm) {
+    closeRealm(this.realm);
+  }
+  // Get rid of the Realm in any case
+  delete this.realm;
+  delete this.config;
 }
 
 export function openRealmBeforeEach(config: OpenRealmConfiguration = {}): void {

integration-tests/tests/src/tests/sync/flexible.ts
@@ -268,9 +268,7 @@ describe.skipIf(environment.missingServer, "Flexible sync", function () {
 
           async function openRealm(user: Realm.User, extraConfig: ExtraConfig = {}) {
             const config = getSuccessConfig(user, extraConfig);
-            const realm = await Realm.open(config);
-
-            return { realm, config };
+            return Realm.open(config);
           }
 
           async function testSuccess(
@@ -278,14 +276,14 @@ describe.skipIf(environment.missingServer, "Flexible sync", function () {
             extraConfig: Partial<FlexibleSyncConfiguration["initialSubscriptions"]> = {},
             closeRealmAfter = true,
           ) {
-            const { realm, config } = await openRealm(user, extraConfig);
+            const realm = await openRealm(user, extraConfig);
 
             try {
               expect(realm.subscriptions).to.have.length(1);
               expect(realm.subscriptions.state).to.equal(Realm.App.Sync.SubscriptionsState.Complete);
             } finally {
               if (closeRealmAfter) {
-                closeRealm(realm, config);
+                closeRealm(realm);
               }
             }
 
@@ -293,12 +291,11 @@ describe.skipIf(environment.missingServer, "Flexible sync", function () {
           }
 
           it("returns a promise", async function (this: RealmContext) {
-            const result = openRealm(this.user, {});
+            const result = openRealm(this.user);
             try {
               expect(result).to.be.instanceOf(Promise);
             } finally {
-              const { realm, config } = await result;
-              closeRealm(realm, config);
+              closeRealm(await result);
             }
           });
 
@@ -311,7 +308,7 @@ describe.skipIf(environment.missingServer, "Flexible sync", function () {
             expect(realm.subscriptions).to.have.length(1);
             expect(realm.subscriptions.state).to.equal(Realm.App.Sync.SubscriptionsState.Complete);
 
-            closeRealm(realm, config);
+            closeRealm(realm);
           });
 
           it("updates the subscriptions on first open if rerunOnOpen is undefined", async function (this: RealmContext) {
@@ -333,10 +330,10 @@ describe.skipIf(environment.missingServer, "Flexible sync", function () {
             await realm.subscriptions.update((subs) => subs.removeAll());
             realm.close();
 
-            const { realm: realm2, config } = await openRealm(this.user, {});
+            const realm2 = await openRealm(this.user);
             expect(realm2.subscriptions).to.have.length(0);
 
-            closeRealm(realm2, config);
+            closeRealm(realm2);
           });
 
           it("does not update the subscriptions on second open if rerunOnOpen is false", async function (this: RealmContext) {
@@ -346,10 +343,10 @@ describe.skipIf(environment.missingServer, "Flexible sync", function () {
             await realm.subscriptions.update((subs) => subs.removeAll());
             realm.close();
 
-            const { realm: realm2, config } = await openRealm(this.user, {});
+            const realm2 = await openRealm(this.user);
             expect(realm2.subscriptions).to.have.length(0);
 
-            closeRealm(realm2, config);
+            closeRealm(realm2);
           });
 
           it("does update the subscriptions on second open if rerunOnOpen is true", async function (this: RealmContext) {

commit f0ffa3c65deec6a479b3eab37d6438e5a74f454c

Derive the config when deleting.
This is a fix for failures on iOS, introduced by #5437.

integration-tests/tests/src/utils/close-realm.ts
@@ -16,6 +16,19 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
+import Realm, { Configuration } from "realm";
+
+function deriveConfig(realm: Realm): Configuration {
+  const { path, syncSession } = realm;
+  if (syncSession) {
+    return { sync: syncSession.config };
+  } else if (path) {
+    return { path };
+  } else {
+    return {};
+  }
+}
+
 /**
  * Close a Realm instance, and optionally delete its Realm file and clear the test state
  *
@@ -25,11 +38,11 @@
  * @param clearTestState If false, do not clear test state before reopening.
  */
 export function closeRealm(realm: Realm, deleteRealmFile = true, clearTestState = true): void {
-  const path = realm.path;
+  const config = deriveConfig(realm);
   realm.close();
 
   if (deleteRealmFile) {
-    Realm.deleteFile({ path });
+    Realm.deleteFile(config);
   }
 
   if (clearTestState) {