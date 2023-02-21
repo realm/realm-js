b00c2e6a8f8fdc36a4b08bda8713f91457f297c7
Andrew Meyer <andrew.meyer@mongodb.com>
2023-02-21 16:35:23 +0100

    Try and get example app to work with bindgen

--- example/metro.config.js
+++ example/metro.config.js
@@ -3,4 +3,13 @@ const config = require('@realm/metro-config')(__dirname);
 // Prevent .js versions of files from being loaded in preference to TS
 config.resolver.sourceExts = ['json', 'ts', 'tsx', 'js', 'jsx'];
 
-module.exports = config;
+module.exports = {
+	transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+	...config};
