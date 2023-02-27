9959888156408f43def77b1e932f3e0d5a238d78
LJ <81748770+elle-j@users.noreply.github.com>
2023-02-27 08:08:58 +0100

    Export SSL types.

--- packages/realm/src/Realm.ts
+++ packages/realm/src/Realm.ts
@@ -130,6 +130,9 @@ import {
   RenameEvent,
   ReplaceEvent,
   Results,
+  SSLConfiguration,
+  SSLVerifyCallback,
+  SSLVerifyObject,
   SessionState,
   SessionStopPolicy,
   SortDescriptor,
@@ -1275,7 +1278,11 @@ type ResultsType<T = unknown> = Results<T>;
 type SessionStateType = SessionState;
 type SessionStopPolicyType = SessionStopPolicy;
 type SetType<T = unknown> = RealmSet<T>;
+type SSLConfigurationType = SSLConfiguration;
+type SSLVerifyCallbackType = SSLVerifyCallback;
+type SSLVerifyObjectType = SSLVerifyObject;
 type SyncErrorType = SyncError;
+type SyncSessionType = SyncSession;
 type TypesType = typeof Types;
 type UpdateModeType = UpdateMode;
 type UserStateType = UserState;
@@ -1294,7 +1301,6 @@ type SubscriptionOptionsType = SubscriptionOptions;
 type SubscriptionSetType = SubscriptionSet;
 type SubscriptionsStateType = SubscriptionsState;
 type SubscriptionType = Subscription;
-type SyncSessionType = SyncSession;
 
 type ObjectIdType = BSON.ObjectId;
 type Decimal128Type = BSON.Decimal128;
@@ -1495,6 +1501,12 @@ export declare namespace Realm {
     // SSLConfiguration,
     /** @deprecated Please use named imports */
     SortDescriptor,
+    /** @deprecated Please use named imports */
+    SSLConfigurationType as SSLConfiguration,
+    /** @deprecated Please use named imports */
+    SSLVerifyCallbackType as SSLVerifyCallback,
+    /** @deprecated Please use named imports */
+    SSLVerifyObjectType as SSLVerifyObject,
     /** @deprecated Please use named imports */
     SyncConfiguration,
     /** @deprecated Please use named imports */
