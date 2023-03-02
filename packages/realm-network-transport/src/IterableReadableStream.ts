51bebb7e47eb94c446e5e6e3b569ec4542558793
Kræn Hansen <kraen.hansen@mongodb.com>
2023-03-02 16:59:35 +0100

    Fixed response.body async iterator on React Native

--- packages/realm-network-transport/src/IterableReadableStream.ts
+++ packages/realm-network-transport/src/IterableReadableStream.ts
@@ -18,9 +18,12 @@
 
 import { FetchResponse, ReadableStream } from ".";
 
+// Falling back on a known string used in code transpiled by Babel
+const asyncIteratorSymbol = Symbol.asyncIterator || "@@asyncIterator";
+
 const READABLE_STREAM_HANDLER: ProxyHandler<ReadableStream> = {
   get(target, prop, receiver) {
-    if (prop === Symbol.asyncIterator) {
+    if (prop === asyncIteratorSymbol) {
       return () => {
         const reader = target.getReader();
         return {
@@ -44,7 +47,7 @@ const READABLE_STREAM_HANDLER: ProxyHandler<ReadableStream> = {
  * @returns An async iterator.
  */
 export function makeStreamIterable(stream: ReadableStream): ReadableStream {
-  if (Symbol.asyncIterator in stream) {
+  if (asyncIteratorSymbol in stream) {
     return stream;
   } else if ("getReader" in stream) {
     return new Proxy(stream, READABLE_STREAM_HANDLER);
