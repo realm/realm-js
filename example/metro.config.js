2969d1f05ca9ece2e7f4e3cf12bcf7863f72766c
Andrew Meyer <andrew.meyer@mongodb.com>
2023-03-03 13:56:43 +0100

    Fix the example app
    * unify react native version so that everything lands in the root project
    * point react native configs towards the root project
    * make small amount of changes in app so babel-plugin doesn't explode

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,4 @@
-const config = require('@realm/metro-config')(__dirname);
+const config = require('@realm/metro-config')({projectRoot: __dirname});
 
 // Prevent .js versions of files from being loaded in preference to TS
 config.resolver.sourceExts = ['json', 'ts', 'tsx', 'js', 'jsx'];
