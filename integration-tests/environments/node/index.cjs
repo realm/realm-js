f1986fcb12b7712313558811ef357e35461bb5cb
Kræn Hansen <kraen.hansen@mongodb.com>
2023-03-22 11:21:34 +0100

    Add commonjs test script for the Node environment

---
+++ integration-tests/environments/node/index.cjs
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2019 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+const os = require("node:os");
+const { Client } = require("mocha-remote-client");
+const fs = require("fs-extra");
+const path = require("path");
+
+const client = new Client({
+  title: `Node.js v${process.versions.node} on ${os.platform()} (via CommonJS)`,
+  tests(context) {
+    // Exposing the Realm constructor as a global
+    global.fs = fs;
+    global.path = path;
+    global.environment = { ...context, node: true };
+
+    // Add the integration test suite
+    require("@realm/integration-tests");
+    // Load the Node.js specific part of the integration tests
+    require("@realm/integration-tests/node");
+  },
+});
+
+client.on("error", (err) => {
+  console.error("Failure from Mocha Remote Client:", err);
+  process.exitCode = 1;
+});
+
+global.client = client;
+
+// TODO: Setup a watch to re-run when the tests change
