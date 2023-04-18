61916b14910453c13d64ca9db3347209ba763da4
Kenneth Geisshirt <k@zigzak.net>
2023-04-18 14:28:55 +0200

    Update version information

--- scripts/changelog-header.sh
+++ scripts/changelog-header.sh
@@ -17,8 +17,8 @@ CHANGELOG=$(cat <<EOF
 * None
 
 ### Compatibility
-* React Native >= v0.71.0
-* Realm Studio v13.0.0.
+* React Native >= v0.71.3
+* Realm Studio v14.0.0.
 * File format: generates Realms with format v23 (reads and upgrades file format v5 or later for non-synced Realm, upgrades file format v10 or later for synced Realms).
 
 ### Internal
