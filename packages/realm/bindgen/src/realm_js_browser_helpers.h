975fabe000511e8c066e1eef3089e7e686291ac4
Yavor Georgiev <fealebenpae@gmail.com>
2023-05-25 17:53:02 +0200

    don’t use EM_JS for Uint8Array copy

--- packages/realm/bindgen/src/realm_js_browser_helpers.h
+++ packages/realm/bindgen/src/realm_js_browser_helpers.h
@@ -20,7 +20,7 @@ REALM_NOINLINE inline emscripten::val toEmscriptenException(const std::exception
 {
     return emscripten::val::global("Error")(std::string(e.what()));
 }
-REALM_NOINLINE inline emscripten::val toEmscriptenException(const std::exception_ptr& e)
+REALM_NOINLINE inline emscripten::val toEmscriptenException(const std::exception_ptr& e) noexcept
 {
     try {
         std::rethrow_exception(e);
@@ -32,11 +32,6 @@ REALM_NOINLINE inline emscripten::val toEmscriptenException(const std::exception
         return emscripten::val::global("Error")(std::string("Unknown error"));
     }
 }
-EM_JS(void, copy_buffer_to_typed_view, (emscripten::EM_VAL buffer_handle, emscripten::EM_VAL view_handle), {
-    const buffer = Emval.toValue(buffer_handle);
-    const view = Emval.toValue(view_handle);
-    view.set(new Uint8Array(buffer));
-});
 // Allocate a new C++ buffer big enough to fit the JS buffer
 // Create a JS memory view around the C++ buffer
 // Call TypedArray.prototype.set to efficiently copy the JS buffer into the C++ buffer via the view
@@ -47,7 +42,7 @@ REALM_NOINLINE inline std::string toBinaryData(const emscripten::val array_buffe
     buf.resize(array_buffer["byteLength"].as<int32_t>());
 
     emscripten::val mv(emscripten::typed_memory_view(buf.length(), buf.data()));
-    copy_buffer_to_typed_view(array_buffer.as_handle(), mv.as_handle());
+    mv.call<void>("set", emscripten::val::global("Uint8Array").new_(array_buffer));
 
     return buf;
 
@@ -60,7 +55,7 @@ REALM_NOINLINE inline OwnedBinaryData toOwnedBinaryData(const emscripten::val ar
     std::unique_ptr<char[]> buf(new char[length]);
 
     emscripten::val mv(emscripten::typed_memory_view(length, buf.get()));
-    copy_buffer_to_typed_view(array_buffer.as_handle(), mv.as_handle());
+    mv.call<void>("set", emscripten::val::global("Uint8Array").new_(array_buffer));
 
     return OwnedBinaryData(std::move(buf), length);
 
