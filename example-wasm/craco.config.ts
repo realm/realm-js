cca765be3c1b72fcc4d31e67cad87b5b41e69792
LJ <81748770+elle-j@users.noreply.github.com>
2023-06-01 12:32:58 +0200

    Set up and configure React app.

---
+++ example-wasm/craco.config.ts
@@ -0,0 +1,46 @@
+import { CracoConfig } from "@craco/types";
+import { loaderByName, getLoaders } from "@craco/craco";
+import * as path from "path";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+
+const config: CracoConfig = {
+    babel: {
+        presets: [
+            ["@babel/preset-typescript", {
+                allowDeclareFields: true
+            }]
+        ]
+    },
+    webpack: {
+        configure(config, context) {
+            const { hasFoundAny, matches } = getLoaders(
+                config,
+                loaderByName('babel-loader')
+            );
+            console.assert(hasFoundAny);
+            matches[0].loader!.include = [
+                matches[0].loader!.include as string,
+                path.resolve(__dirname, '../packages')
+            ];
+
+            // needed to import source-map-support
+            if (config.resolve!.plugins) {
+                config.resolve!.plugins.pop();
+            }
+            config.plugins = [
+                ...config.plugins || [],
+                new NodePolyfillPlugin()
+            ];
+
+            config.experiments = {
+                topLevelAwait: true,
+                ...config.experiments
+            };
+            return config;
+        },
+    },
+    devServer: {
+        open: "chrome",
+    }
+};
+export default config;
---
+++ example-wasm/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'source-map-support/register';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
