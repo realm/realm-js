484b6d68d8171b944b7f5a15dbf61e8cfe3e580e
LJ <81748770+elle-j@users.noreply.github.com>
2023-06-01 12:51:39 +0200

    Add 'TaskScreen' screen/page.

---
+++ example-wasm/src/screens/TaskScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { AddTaskForm } from '../components/AddTaskForm';
+import { IntroText } from '../components/IntroText';
+import { TaskList } from '../components/TaskList';
+import { useTaskManager } from '../hooks/useTaskManager';
+
+export function TaskScreen() {
+  const {
+    tasks,
+    addTask,
+    toggleTaskStatus,
+    deleteTask,
+  } = useTaskManager();
+
+  return (
+    <div>
+      <AddTaskForm onSubmit={addTask} />
+      {tasks.length === 0 ? (
+        <IntroText />
+      ) : (
+        <TaskList
+          tasks={tasks}
+          onToggleTaskStatus={toggleTaskStatus}
+          onDeleteTask={deleteTask}
+        />
+      )}
+    </div>
+  );
+}
