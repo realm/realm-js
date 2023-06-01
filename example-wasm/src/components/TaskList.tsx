f9b98e093293d1577d7e4a48631fc8275d7c784d
LJ <81748770+elle-j@users.noreply.github.com>
2023-06-01 12:40:27 +0200

    Add 'TaskList' component.

---
+++ example-wasm/src/components/TaskList.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Realm from 'realm';
+
+import { Task } from '../models/Task';
+import { TaskItem } from './TaskItem';
+
+type TaskListProps = {
+  tasks: Realm.Results<Task>;
+  onToggleTaskStatus: (task: Task /*& Realm.Object*/) => void;
+  onDeleteTask: (task: Task /*& Realm.Object*/) => void;
+};
+
+export function TaskList({ tasks, onToggleTaskStatus, onDeleteTask }: TaskListProps) {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskItem
+          key={task._id.toHexString()}
+          task={task}
+          onToggleStatus={() => onToggleTaskStatus(task)}
+          onDelete={() => onDeleteTask(task)}
+        />
+      ))}
+    </div>
+  );
+}
