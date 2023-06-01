5d2b0d99cd710fa8d0e50a6da022addf87332b7f
LJ <81748770+elle-j@users.noreply.github.com>
2023-06-01 12:39:01 +0200

    Add 'TaskItem' component.

---
+++ example-wasm/src/components/TaskItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Task } from '../models/Task';
+
+type TaskItemProps = {
+  task: Task;
+  onToggleStatus: () => void;
+  onDelete: () => void;
+};
+
+// TODO: Memoize
+export function TaskItem({ task, onToggleStatus, onDelete }: TaskItemProps) {
+  return (
+    <div>
+      <button onClick={onToggleStatus}>
+        {task.isComplete ? '✓' : '○'}
+      </button>
+      <p>
+        {task.description}
+      </p>
+      <button onClick={onDelete}>
+        Delete
+      </button>
+    </div>
+  );
+}
