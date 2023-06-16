9dd7cad8dac89297f7f462c13541b0ec01e827c8
LJ <81748770+elle-j@users.noreply.github.com>
2023-06-16 16:36:58 +0200

    Update 'ErrorPage'.

--- examples/example-wasm/src/pages/ErrorPage.tsx
+++ examples/example-wasm/src/pages/ErrorPage.tsx
@@ -1,9 +1,25 @@
+import { isRouteErrorResponse, useNavigate, useRouteError } from 'react-router-dom';
+
+import styles from '../styles/ErrorPage.module.css';
+
 export function ErrorPage() {
+  const navigate = useNavigate();
+  const error = useRouteError();
+
   return (
-    <div>
-      <p>
-        Error Screen
-      </p>
+    <div className={styles.container}>
+      <h1>
+        Oh no, an error occurred..
+      </h1>
+      {isRouteErrorResponse(error) && (
+        <>
+          <p>{`Status: ${error.status} ${error.statusText}`}</p>
+          {error.data?.message && <p>{error.data.message}</p>}
+        </>
+      )}
+      <button className={styles.button} onClick={() => navigate('/')}>
+        Go back
+      </button>
     </div>
   );
 }
