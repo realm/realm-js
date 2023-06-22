b1a79ad426e7463baf660b5b46fb8f50d44257a9
Andrew Meyer <andrew.meyer@mongodb.com>
2023-06-22 16:08:24 +0200

    Try out rn72 in the example

---
+++ examples/react-native/metro.config.js
@@ -0,0 +1,16 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+	resolver: {
+		unstable_enableSymlinks: true,
+		unstable_enablePackageExports: true,
+	},
+}
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
