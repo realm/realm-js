c51627aa80c0d417b1a7cad8e77d5cdf907af750
Kenneth Geisshirt <k@zigzak.net>
2023-06-27 13:55:54 +0200

    Revert "Don't set default directory when specifying baseFilePath"
    This reverts commit 1524ebd30b4374257db66363b17a32c09dca557a.

--- packages/realm/src/app-services/App.ts
+++ packages/realm/src/app-services/App.ts
@@ -201,11 +201,10 @@ export class App<FunctionsFactoryType = DefaultFunctionsFactory, CustomDataType 
     assert.boolean(multiplexSessions, "multiplexSessions");
     if (baseFilePath !== undefined) {
       assert.string(baseFilePath, "baseFilePath");
-      fs.ensureDirectoryForFile(fs.joinPaths(baseFilePath, "mongodb-realm"));
-    } else {
-      fs.ensureDirectoryForFile(fs.joinPaths(fs.getDefaultDirectoryPath(), "mongodb-realm"));
+      fs.setDefaultDirectoryPath(baseFilePath);
     }
-    
+
+    fs.ensureDirectoryForFile(fs.joinPaths(fs.getDefaultDirectoryPath(), "mongodb-realm"));
     // TODO: This used getSharedApp in the legacy SDK, but it's failing AppTests
     this.internal = binding.App.getUncachedApp(
       {
