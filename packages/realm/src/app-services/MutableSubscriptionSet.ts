7caab2592e0773070675587d1aa24704af892929
Andrew Meyer <andrew.meyer@mongodb.com>
2023-08-22 13:11:36 +0200

    Update packages/realm/src/app-services/MutableSubscriptionSet.ts
    Co-authored-by: LJ <81748770+elle-j@users.noreply.github.com>

--- packages/realm/src/app-services/MutableSubscriptionSet.ts
+++ packages/realm/src/app-services/MutableSubscriptionSet.ts
@@ -67,7 +67,7 @@ export type SubscriptionOptions = {
   throwOnUpdate?: boolean;
   /**
    * Specifies how to wait or not wait for subscribed objects to be downloaded.
-   * By default, the behavior is `WaitForSync.Never`.
+   * @default WaitForSync.FirstTime
    */
   behavior?: WaitForSync;
   /**
