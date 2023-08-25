af9ea94e15c4a184dde74817281ff60a3cde0930
LJ <81748770+elle-j@users.noreply.github.com>
2023-08-25 13:49:51 +0200

    Add copyright headers.

--- examples/example-node-connection-and-error/src/index.ts
+++ examples/example-node-connection-and-error/src/index.ts
@@ -1,9 +1,30 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2023 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
 import { register, logIn, logOut, openRealm } from "./realm-auth";
 import { addDummyData, updateDummyData, deleteDummyData, getStore } from "./realm-query";
 
 const exampleEmail = "john@doe.com";
 const examplePassword = "123456";
 
+/**
+ * Illustrates the flow of using a synced Realm.
+ */
 async function main(): Promise<void> {
   let success = await register(exampleEmail, examplePassword);
   if (!success) {
