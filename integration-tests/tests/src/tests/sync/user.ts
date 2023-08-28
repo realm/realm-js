647f202266ae7afecbfed25d8f85bf263184a465
Kenneth Geisshirt <k@zigzak.net>
2023-08-28 21:06:44 +0200

    uncomment function

--- integration-tests/tests/src/tests/sync/user.ts
+++ integration-tests/tests/src/tests/sync/user.ts
@@ -509,6 +509,34 @@ describe.skipIf(environment.missingServer, "User", () => {
           };
           `,
         })
+        // .customFunctionAuth(
+        //   `
+        //   exports = async function (loginPayload) {
+        //     // Get a handle for the app.users collection
+        //     const users = context.services.get("mongodb").db("app").collection("users");
+
+        //     // Parse out custom data from the FunctionCredential
+
+        //     const { username, secret } = loginPayload;
+
+        //     if (secret !== "v3ry-s3cret") {
+        //       throw new Error("Ah ah ah, you didn't say the magic word");
+        //     }
+        //     // Query for an existing user document with the specified username
+
+        //     const user = await users.findOne({ username });
+
+        //     if (user) {
+        //       // If the user document exists, return its unique ID
+        //       return user._id.toString();
+        //     } else {
+        //       // If the user document does not exist, create it and then return its unique ID
+        //       const result = await users.insertOne({ username });
+        //       return result.insertedId.toString();
+        //     }
+        //   };
+        // `,
+        // )
         .function({
           name: "resetFunc",
           private: false,
@@ -558,34 +586,6 @@ describe.skipIf(environment.missingServer, "User", () => {
     importAppBefore(
       buildAppConfig("with-custom-function")
         .anonAuth()
-        .customFunctionAuth(
-          `
-          exports = async function (loginPayload) {
-            // Get a handle for the app.users collection
-            const users = context.services.get("mongodb").db("app").collection("users");
-
-            // Parse out custom data from the FunctionCredential
-
-            const { username, secret } = loginPayload;
-
-            if (secret !== "v3ry-s3cret") {
-              throw new Error("Ah ah ah, you didn't say the magic word");
-            }
-            // Query for an existing user document with the specified username
-
-            const user = await users.findOne({ username });
-
-            if (user) {
-              // If the user document exists, return its unique ID
-              return user._id.toString();
-            } else {
-              // If the user document does not exist, create it and then return its unique ID
-              const result = await users.insertOne({ username });
-              return result.insertedId.toString();
-            }
-          };
-        `,
-        )
         .function({
           can_evaluate: {},
           name: "sumFunc",
