02051561f5751a7887d11209ab4095fe080abfd1
LJ <81748770+elle-j@users.noreply.github.com>
2023-08-29 16:16:40 +0200

    Refactor test.

--- integration-tests/tests/src/tests/linking-objects.ts
+++ integration-tests/tests/src/tests/linking-objects.ts
@@ -106,111 +106,37 @@ class Name extends Realm.Object<Name> {
   };
 }
 
+class Manufacturer extends Realm.Object<Manufacturer> {
+  _id!: Realm.BSON.ObjectId;
+  name!: string;
+  cars!: Realm.List<Car>;
+
+  static schema: Realm.ObjectSchema = {
+    name: "Manufacturer",
+    properties: {
+      _id: "objectId",
+      name: "string",
+      cars: "Car[]",
+    },
+  };
+}
+
+class Car extends Realm.Object<Car> {
+  _id!: Realm.BSON.ObjectId;
+  model!: string;
+  miles?: number;
+
+  static schema: Realm.ObjectSchema = {
+    name: "Car",
+    properties: {
+      _id: "objectId",
+      model: "string",
+      miles: "int?",
+    },
+  };
+}
+
 describe("Linking objects", () => {
-  // Temporary test suite, will reorder once debugged.
-  describe("DEBUG_LINKING_OBJECTS", () => {
-    class Manufacturer extends Realm.Object {
-      _id!: Realm.BSON.ObjectId;
-      name!: string;
-      cars!: Realm.List<Car>;
-      // cars!: Realm.OrderedCollection<Car>;
-
-      static schema: Realm.ObjectSchema = {
-        name: "Manufacturer",
-        properties: {
-          _id: {
-            type: "objectId",
-          },
-          name: {
-            type: "string",
-          },
-          // A manufacturer that may have many cars
-          cars: {
-            type: "list",
-            objectType: "Car",
-          },
-          // cars: {
-          //   type: "linkingObjects",
-          //   objectType: "Car",
-          //   property: "manufacturer",
-          // },
-        },
-      };
-    }
-
-    class Car extends Realm.Object {
-      _id!: Realm.BSON.ObjectId;
-      model!: string;
-      miles?: number;
-      manufacturer!: Realm.OrderedCollection<Manufacturer>;
-
-      static schema: Realm.ObjectSchema = {
-        name: "Car",
-        properties: {
-          _id: {
-            type: "objectId",
-          },
-          model: {
-            type: "string",
-          },
-          miles: {
-            type: "int",
-            optional: true,
-          },
-          // Also test without this field.
-          manufacturer: {
-            type: "linkingObjects",
-            objectType: "Manufacturer",
-            property: "cars",
-          },
-        },
-      };
-    }
-    openRealmBeforeEach({ schema: [Manufacturer, Car] });
-
-    it("debugging linking objects", async function (this: RealmContext) {
-      expect(this.realm.isClosed).to.be.false;
-
-      this.realm.write(() => {
-        const sentra = this.realm.create(Car, {
-          _id: new Realm.BSON.ObjectID(),
-          model: "Sentra",
-          miles: 1000,
-        });
-
-        const pathfinder = this.realm.create(Car, {
-          _id: new Realm.BSON.ObjectID(),
-          model: "Pathfinder",
-          miles: 254,
-        });
-
-        this.realm.create(Manufacturer, {
-          _id: new Realm.BSON.ObjectID(),
-          name: "Nissan",
-          cars: [sentra, pathfinder],
-        });
-      });
-
-      const manufacturers: Realm.Results<Manufacturer> = this.realm.objects(Manufacturer);
-
-      const manufacturerCars = manufacturers[0].cars;
-
-      // Get the Manufacturer who makes the Car
-      const carObjects = this.realm.objects<Car>(Car);
-      const car = carObjects[0];
-      const linkedManufacturer = car.linkingObjects(Manufacturer, "cars")[0];
-      // const linkedManufacturer: Manufacturer = car.linkingObjects<Manufacturer>("Manufacturer", "cars")[0];
-
-      console.log({ linkingObjectsCount: car.linkingObjectsCount() });
-      console.log({ linkedManufacturer });
-      console.log({ manufacturer: car.manufacturer[0].name });
-
-      expect(manufacturerCars[0].model).to.equal("Sentra");
-      expect(manufacturerCars[1].model).to.equal("Pathfinder");
-      expect(linkedManufacturer.name).to.equal("Nissan");
-    });
-  });
-
   describe("Same class", () => {
     openRealmBeforeEach({ schema: [Person] });
     it("add and delete propagate to linking object", function (this: RealmContext) {
@@ -357,7 +283,7 @@ describe("Linking objects", () => {
     });
   });
   describe("across different classes", () => {
-    openRealmBeforeEach({ schema: [LanguageSchema, CountrySchema] });
+    openRealmBeforeEach({ schema: [LanguageSchema, CountrySchema, Manufacturer, Car] });
     it("count operation", function (this: RealmContext) {
       this.realm.write(() => {
         const english = this.realm.create(LanguageSchema.name, { name: "English" });
@@ -406,6 +332,37 @@ describe("Linking objects", () => {
       const notSpokenMethod2 = languages.filtered("@links.Country.languages.@count == 0"); // links of a specific relationship are 0
       expect(notSpokenMethod2.length).equals(1);
       expect(notSpokenMethod2[0].name).equals("Latin");
+    });
+
+    it("can retrieve all linking objects", async function (this: RealmContext) {
+      // Create a manufacturer with 2 cars.
+      this.realm.write(() => {
+        const sentra = this.realm.create(Car, {
+          _id: new Realm.BSON.ObjectID(),
+          model: "Sentra",
+          miles: 1000,
+        });
+
+        const pathfinder = this.realm.create(Car, {
+          _id: new Realm.BSON.ObjectID(),
+          model: "Pathfinder",
+          miles: 500,
+        });
+
+        this.realm.create(Manufacturer, {
+          _id: new Realm.BSON.ObjectID(),
+          name: "Nissan",
+          cars: [sentra, pathfinder],
+        });
+      });
+
+      const car = this.realm.objects(Car)[0];
+      expect(car.linkingObjectsCount()).to.equal(1);
+
+      const linkedManufacturer = car.linkingObjects(Manufacturer, "cars")[0];
+      expect(linkedManufacturer.name).to.equal("Nissan");
+      expect(linkedManufacturer.cars[0].model).to.equal("Sentra");
+      expect(linkedManufacturer.cars[1].model).to.equal("Pathfinder");
     });
   });
 });
