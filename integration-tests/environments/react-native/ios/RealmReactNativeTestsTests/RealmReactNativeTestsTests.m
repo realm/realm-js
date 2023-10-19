4abe04e328ac7b79fda5c7b305583a6680522543
Andrew Meyer <andrew.meyer@mongodb.com>
2023-10-19 10:21:32 +0200

    Upgrade React Native everywhere (#6199)
    Co-authored-by: LJ <81748770+elle-j@users.noreply.github.com>

--- integration-tests/environments/react-native/ios/RealmReactNativeTestsTests/RealmReactNativeTestsTests.m
+++ integration-tests/environments/react-native/ios/RealmReactNativeTestsTests/RealmReactNativeTestsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RnDiffAppTests : XCTestCase
+@interface RealmReactNativeTestsTests : XCTestCase
 
 @end
 
-@implementation RnDiffAppTests
+@implementation RealmReactNativeTestsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
