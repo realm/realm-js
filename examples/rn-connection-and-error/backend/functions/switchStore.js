90fc1ff46ff02873007ed9aec93425dd10cd9d28
LJ <81748770+elle-j@users.noreply.github.com>
2023-11-22 12:14:39 +0100

    [backend] Update function comments.

--- examples/rn-connection-and-error/backend/functions/switchStore.js
+++ examples/rn-connection-and-error/backend/functions/switchStore.js
@@ -1,19 +1,16 @@
 /* eslint-disable */
 
 /**
- * Toggle between two stores.  This is run as the system user, since it will need to create
- * a second store if one does not exist.  The userId is provided as a string.
+ * Toggle between two stores. This is run as the system user, since it will need to create
+ * a second store if one does not exist. The userId is provided as a string.
  */
 exports = async function switchStore() {
   const userId = context.user.id;
   const customUserDataCollection = context.services.get("mongodb-atlas").db("StoreDemo").collection("Users");
-  const storeCollection = context.services.get("mongodb-atlas").db("StoreDemo").collection("Store");
 
   try {
-
-    const userDoc = await customUserDataCollection.findOne({userId: userId});
-    const userStore = userDoc.storeId;
-    const store = await getOrCreateStore(storeCollection, userStore);
+    const userDoc = await customUserDataCollection.findOne({userId});
+    const store = await getOrCreateStore(userDoc.storeId);
     await customUserDataCollection.updateOne({
       userId,
     },
@@ -29,15 +26,15 @@ exports = async function switchStore() {
 // Returns a different store than the current `userStore`.
 // This will help toggle between stores.  It will create a second store
 // if one does not already exist.
-async function getOrCreateStore(storeCollection, userStore){
-    const stores = await context.functions.execute('getAllStores')
-    if(userStore.toString() === stores[0]._id.toString()){
-      if(!stores[1]){
-        const store = await context.functions.execute('createNewStore')
+async function getOrCreateStore(userStoreId) {
+    const stores = await context.functions.execute("getAllStores");
+    if (userStoreId.toString() === stores[0]?._id.toString()) {
+      if (!stores[1]) {
+        const store = await context.functions.execute("createNewStore");
         return store;
       }
       return stores[1];
     }
 
-    return stores[0];
+    return stores[0] || await context.functions.execute("createNewStore");
 }
