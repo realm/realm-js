2adc11d3e0159ca829679b3eff998f76721f44c8
Kræn Hansen <kraen.hansen@mongodb.com>
2024-01-11 10:22:50 +0100

    Upgrading integration-test app

--- integration-tests/environments/react-native/babel.config.js
+++ integration-tests/environments/react-native/babel.config.js
@@ -16,5 +16,5 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ['module:@react-native/babel-preset'],
 };
