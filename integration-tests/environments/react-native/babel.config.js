0b323620352c66f1f35f04982b2e9a3b62fa6b27
Kræn Hansen <kraen.hansen@mongodb.com>
2024-01-11 10:24:32 +0100

    Applied fixes from eslint

--- integration-tests/environments/react-native/babel.config.js
+++ integration-tests/environments/react-native/babel.config.js
@@ -16,5 +16,5 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
 };
