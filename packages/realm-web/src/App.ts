73cd28f4d6438077be37c85966878fd9c16def88
Kræn Hansen <kraen.hansen@mongodb.com>
2024-04-04 13:07:01 +0200

    Updating base url

--- packages/realm-web/src/App.ts
+++ packages/realm-web/src/App.ts
@@ -35,7 +35,7 @@ type SimpleObject = Record<string, unknown>;
 /**
  * Default base url to prefix all requests if no baseUrl is specified in the configuration.
  */
-export const DEFAULT_BASE_URL = "https://realm.mongodb.com";
+export const DEFAULT_BASE_URL = "https://services.cloud.mongodb.com";
 
 /**
  * Configuration to pass as an argument when constructing an app.
