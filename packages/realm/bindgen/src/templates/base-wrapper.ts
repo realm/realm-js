ec9f30faae6cfeba7a003984824cb58eca86618b
Kræn Hansen <kraen.hansen@mongodb.com>
2024-04-15 14:22:45 +0200

    Injecting Symbol_for (#6616)

--- packages/realm/bindgen/src/templates/base-wrapper.ts
+++ packages/realm/bindgen/src/templates/base-wrapper.ts
@@ -84,6 +84,7 @@ export function generate({ spec: boundSpec }: TemplateContext, out: Outputter): 
     "Decimal128",
     "EJSON_parse: EJSON.parse",
     "EJSON_stringify: EJSON.stringify",
+    "Symbol_for: Symbol.for",
   ];
 
   for (const cls of spec.classes) {
