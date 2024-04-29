2711dd8568ca27d653655789fb8df7784354b394
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2024-04-29 11:29:55 +0200

    Logging to file

--- integration-tests/tests/src/hooks/import-app-before.ts
+++ integration-tests/tests/src/hooks/import-app-before.ts
@@ -138,15 +138,32 @@ export function importAppBefore(config: AppConfig | { config: AppConfig }, sdkCo
         throw new Error("Expected at most 1 database name in the config");
       }
 
-      Realm.App.Sync.setLogLevel(this.app, syncLogLevel);
+      const dirName = "/Users/papafe/Desktop/";
+      const filePath = path.join(dirName, "logs.txt"); // Specify your log file path here
+
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error("Failed to delete file:", err);
+        } else {
+          console.log("File successfully deleted.");
+        }
+      });
+
+      Realm.App.Sync.setLogLevel(this.app, "trace");
       // Set a default logger as Android does not forward stdout
       Realm.App.Sync.setLogger(this.app, (level, message) => {
         const time = new Date().toISOString().split("T")[1].replace("Z", "");
         const magentaTime = `\x1b[35m${time}`;
         const greenLogLevel = `\x1b[32m${REALM_LOG_LEVELS[level].toUpperCase()}`;
         const whiteMessage = `\x1b[37m${message}}`;
+        const logMessage = `${time}: ${REALM_LOG_LEVELS[level].toUpperCase()}:\t${message}\n`;
+        fs.appendFile(filePath, logMessage, (err) => {
+          if (err) {
+            console.error("Error writing to log file:", err);
+          }
+        });
 
-        console.log(`${magentaTime}: ${greenLogLevel}:\t${whiteMessage}`);
+        // console.log(`${magentaTime}: ${greenLogLevel}:\t${whiteMessage}`);
       });
     }
   });
