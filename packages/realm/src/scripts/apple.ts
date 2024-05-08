bdd92b67c7e5a9dc6a45fc7700da250783fb4897
Kræn Hansen <kraen.hansen@mongodb.com>
2024-05-08 09:55:08 +0200

    Splitting building frameworks from creating Xcframework

--- packages/realm/src/scripts/apple.ts
+++ packages/realm/src/scripts/apple.ts
@@ -49,6 +49,7 @@ const REALM_CORE_DEPENDENCIES = fs.readFileSync(REALM_CORE_DEPENDENCIES_PATH, "u
 const REALM_CORE_VERSION_MATCH = REALM_CORE_DEPENDENCIES.match(/^VERSION: ?(.+)$/m);
 assert(REALM_CORE_VERSION_MATCH, "Failed to determine Realm Core version");
 const REALM_CORE_VERSION = REALM_CORE_VERSION_MATCH[1];
+const REALM_CORE_PRODUCTS_INSTALL_PATH = "Products/usr/local/lib";
 
 const INCLUDE_PATH = path.resolve(PACKAGE_PATH, "react-native/ios/include");
 const XCFRAMEWORK_PATH = path.resolve(PACKAGE_PATH, "react-native/ios/realm-js.xcframework");
@@ -67,19 +68,20 @@ function getDestinations(platform: XcodeSDKName) {
   return destinations;
 }
 
-function getDeveloperDir() {
-  return env.DEVELOPER_DIR || execSync("xcode-select --print-path", { encoding: "utf8" }).trim();
-}
-
 type GenerateXcodeProjectOptions = {
   cmakePath: string;
 };
 
-function generateXcodeProject({ cmakePath }: GenerateXcodeProjectOptions) {
+export function ensureBuildDirectory() {
   // Ensure the build directory exists
   if (!fs.existsSync(REALM_CORE_BUILD_PATH)) {
+    console.log("Creating build directory:", REALM_CORE_BUILD_PATH);
     fs.mkdirSync(REALM_CORE_BUILD_PATH);
   }
+}
+
+export function generateXcodeProject({ cmakePath }: GenerateXcodeProjectOptions) {
+  assert(fs.existsSync(REALM_CORE_BUILD_PATH), `Expected Xcode project in '${REALM_CORE_BUILD_PATH}'`);
   // Generate Xcode project
   spawnSync(
     cmakePath,
@@ -101,85 +103,111 @@ function generateXcodeProject({ cmakePath }: GenerateXcodeProjectOptions) {
   );
 }
 
-type BuildXcframeworkOptions = {
-  platforms: readonly XcodeSDKName[];
+type BuildFrameworkOptions = {
+  platform: XcodeSDKName;
   configuration: "Release" | "Debug" | "MinSizeRel" | "RelWithDebInfo";
 };
 
-function buildXcframework({ platforms, configuration }: BuildXcframeworkOptions) {
+export function buildFramework({ platform, configuration }: BuildFrameworkOptions) {
   assert(fs.existsSync(REALM_CORE_BUILD_PATH), `Expected Xcode project in '${REALM_CORE_BUILD_PATH}'`);
 
-  const archivePaths = platforms.map((platform) => {
-    console.log(`Building archive for '${platform}'`);
-    const archivePath = path.join(REALM_CORE_BUILD_PATH, platform + ".xcarchive");
-    xcode.archive({
-      cwd: REALM_CORE_BUILD_PATH,
-      scheme: "ALL_BUILD",
-      configuration,
-      sdkName: platform,
-      archivePath,
-      destinations: getDestinations(platform),
-    });
-    assert(fs.existsSync(archivePath), `Expected xcodebuild to emit ${archivePath}`);
-    return archivePath;
+  console.log(`Building archive for '${platform}'`);
+  const archivePath = path.join(REALM_CORE_BUILD_PATH, platform + ".xcarchive");
+  xcode.archive({
+    cwd: REALM_CORE_BUILD_PATH,
+    scheme: "ALL_BUILD",
+    configuration,
+    sdkName: platform,
+    archivePath,
+    destinations: getDestinations(platform),
   });
+  assert(fs.existsSync(archivePath), `Expected xcodebuild to emit ${archivePath}`);
 
   console.log(`Using libtool to combine libraries into a single ${REALM_CORE_COMBINED_LIBRARY_NAME}`);
-  for (const archivePath of archivePaths) {
-    const archiveInstallPath = path.join(archivePath, "Products/usr/local/lib");
-    const inputPaths = fs
-      .readdirSync(archiveInstallPath)
-      .filter((name) => !REALM_CORE_LIBRARY_NAMES_DENYLIST.includes(name))
-      .map((name) => path.join(archiveInstallPath, name));
-    xcode.libtool({
-      outputPath: path.join(archiveInstallPath, REALM_CORE_COMBINED_LIBRARY_NAME),
-      inputPaths,
-    });
+  const archiveInstallPath = path.join(archivePath, REALM_CORE_PRODUCTS_INSTALL_PATH);
+  const inputPaths = fs
+    .readdirSync(archiveInstallPath)
+    .filter((name) => !REALM_CORE_LIBRARY_NAMES_DENYLIST.includes(name))
+    .map((name) => path.join(archiveInstallPath, name));
+  const combinedArchivePath = path.join(archiveInstallPath, REALM_CORE_COMBINED_LIBRARY_NAME);
+  xcode.libtool({
+    outputPath: combinedArchivePath,
+    inputPaths,
+  });
+  assert(fs.existsSync(combinedArchivePath), `Expected libtool to emit ${archivePath}`);
+
+  return archivePath;
+}
+
+export function collectHeaders() {
+  fs.rmSync(INCLUDE_PATH, { recursive: true, force: true });
+
+  const srcPath = path.join(REALM_CORE_PATH, "src");
+  const sourceHeaderPaths = globSync(["**/*.h", "**/*.hpp"], {
+    cwd: srcPath,
+    ignore: [
+      "external/**",
+      "win32/**",
+      /* c-api */
+      "realm.h",
+      "realm/object-store/c_api/**",
+      /* executables */
+      "realm/exec/**",
+      /* executables */
+      "realm/exec/**",
+    ],
+  });
+  console.log(`Copying ${sourceHeaderPaths.length} headers from ${srcPath} into ${INCLUDE_PATH}`);
+  for (const headerPath of sourceHeaderPaths) {
+    // Create any parent directories
+    fs.mkdirSync(path.join(INCLUDE_PATH, path.dirname(headerPath)), { recursive: true });
+    fs.copyFileSync(path.join(srcPath, headerPath), path.join(INCLUDE_PATH, headerPath));
   }
 
-  // Collect generated headers from the build
-  fs.rmSync(INCLUDE_PATH, { recursive: true, force: true });
+  // Collect generated headers from the build directory
   const buildSrcPath = path.join(REALM_CORE_BUILD_PATH, "src");
-  const headerPaths = globSync(["**/*.h", "**/*.hpp"], {
+  const buildHeaderPaths = globSync(["**/*.h", "**/*.hpp"], {
     cwd: buildSrcPath,
     ignore: ["external/**"],
   });
-  for (const headerPath of headerPaths) {
+  console.log(`Copying ${buildHeaderPaths.length} generated headers from ${buildSrcPath} into ${INCLUDE_PATH}`);
+  for (const headerPath of buildHeaderPaths) {
     // Create any parent directories
     fs.mkdirSync(path.join(INCLUDE_PATH, path.dirname(headerPath)), { recursive: true });
     fs.copyFileSync(path.join(buildSrcPath, headerPath), path.join(INCLUDE_PATH, headerPath));
   }
+}
 
+export function collectArchivePaths() {
+  const archivePaths = globSync([path.join("*.xcarchive")], {
+    cwd: REALM_CORE_BUILD_PATH,
+    ignore: ["external/**"],
+    absolute: true,
+  });
+  // Filter the archives by the existence of the combined library
+  return archivePaths.filter((archivePath) => {
+    const combinedLibraryPath = path.join(
+      archivePath,
+      REALM_CORE_PRODUCTS_INSTALL_PATH,
+      REALM_CORE_COMBINED_LIBRARY_NAME,
+    );
+    return fs.existsSync(combinedLibraryPath);
+  });
+}
+
+type CreateXCFrameworkOptions = {
+  archivePaths: string[];
+};
+
+export function createXCFramework({ archivePaths }: CreateXCFrameworkOptions) {
+  console.log(`Creating an xcframework from ${archivePaths.length} archives`);
   // Delete any existing xcframework to prevent
   // “librealm-combined.a” couldn’t be copied to “...” because an item with the same name already exists
   // Ideally, it would only be necessary to delete the specific platform+arch, to allow selectively building from source.
   fs.rmSync(XCFRAMEWORK_PATH, { recursive: true, force: true });
-
   xcode.createXcframework({
     archivePaths,
     libraryNames: [REALM_CORE_COMBINED_LIBRARY_NAME],
     outputPath: XCFRAMEWORK_PATH,
-  });
-}
-
-export function buildApple(platforms: readonly XcodeSDKName[], configuration: BuildConfiguration) {
-  assert(fs.existsSync(REALM_CORE_PATH), `Expected Realm Core at '${REALM_CORE_PATH}'`);
-
-  const { CMAKE_PATH: cmakePath = execSync("which cmake", { encoding: "utf8" }).trim() } = env;
-  const developerDir = getDeveloperDir();
-  console.log(`Building for Apple platforms: ${platforms.join(", ")} using`, {
-    cmakePath,
-    developerDir,
-    REALM_CORE_PATH,
-    REALM_CORE_VERSION,
-    REALM_CORE_BUILD_PATH,
-    REALM_CORE_CMAKE_TOOLCHAIN_PATH,
-  });
-
-  generateXcodeProject({ cmakePath });
-
-  buildXcframework({
-    platforms,
-    configuration,
   });
 }
--- packages/realm/src/scripts/build.ts
+++ packages/realm/src/scripts/build.ts
@@ -20,7 +20,7 @@
 /* eslint-env node */
 
 import assert from "node:assert";
-import { spawnSync } from "node:child_process";
+import { execSync, spawnSync } from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 
@@ -28,7 +28,14 @@ import { Option, program } from "@commander-js/extra-typings";
 import { globSync } from "glob";
 
 import { XcodeSDKName } from "./xcode";
-import { buildApple } from "./apple";
+import {
+  buildFramework,
+  collectArchivePaths,
+  collectHeaders,
+  createXCFramework,
+  ensureBuildDirectory,
+  generateXcodeProject,
+} from "./apple";
 
 export { program };
 
@@ -47,25 +54,26 @@ const SUPPORTED_APPLE_PLATFORMS = [
 
 const { env } = process;
 
-function getDefaultApplePlatform(): readonly XcodeSDKName[] {
-  const { TARGET_DEVICE_PLATFORM_NAME } = env;
-  // Only defaulting to the environment variable if it's actually supported, as it circumvents validation
-  if (TARGET_DEVICE_PLATFORM_NAME && SUPPORTED_APPLE_PLATFORMS.includes(TARGET_DEVICE_PLATFORM_NAME as XcodeSDKName)) {
-    return [TARGET_DEVICE_PLATFORM_NAME as XcodeSDKName];
-  } else {
-    return SUPPORTED_APPLE_PLATFORMS;
-  }
-}
-
 const applePlatformOption = new Option("--platform <name...>", "Platform to build for")
   .makeOptionMandatory()
-  .choices(SUPPORTED_APPLE_PLATFORMS)
-  .default(getDefaultApplePlatform());
+  .choices([...SUPPORTED_APPLE_PLATFORMS, "all", "none"] as const)
+  .default(["all"] as const);
 
 const configurationOption = new Option("--configuration <name>", "Build configuration")
   .makeOptionMandatory()
   .choices(["Release", "Debug", "MinSizeRel", "RelWithDebInfo"] as const)
   .default("Release" as const);
+
+function validatePlatforms(values: readonly (XcodeSDKName | "all" | "none")[]): readonly XcodeSDKName[] {
+  if (values.includes("none")) {
+    assert(values.length === 1, "Expected 'none' to be the only platform");
+    return [];
+  } else if (values.includes("all")) {
+    return SUPPORTED_APPLE_PLATFORMS;
+  } else {
+    return values as readonly XcodeSDKName[];
+  }
+}
 
 const PACKAGE_PATH = path.resolve(__dirname, "../..");
 
@@ -103,6 +111,21 @@ function actionWrapper<Args extends unknown[]>(action: (...args: Args) => Promis
       }
     }
   };
+}
+
+function group<ReturnType>(title: string, callback: () => ReturnType) {
+  try {
+    if (env.GITHUB_ACTIONS === "true") {
+      console.log(`::group::${title}`);
+    }
+    return callback();
+  } finally {
+    if (env.GITHUB_ACTIONS === "true") {
+      console.log("::endgroup::");
+    } else {
+      console.log();
+    }
+  }
 }
 
 program.name("build-realm");
@@ -152,9 +175,58 @@ program
   .description("Build native code for Apple platforms")
   .addOption(applePlatformOption)
   .addOption(configurationOption)
+  .option("--skip-creating-xcframework", "Skip creating an xcframework from all frameworks in the build directory")
+  .option("--skip-collecting-headers", "Skip collecting headers from the build directory and copy them to the SDK")
   .action(
-    actionWrapper(({ platform, configuration }) => {
-      buildApple(platform, configuration);
+    actionWrapper(({ platform: rawPlatforms, configuration, skipCollectingHeaders, skipCreatingXcframework }) => {
+      assert(fs.existsSync(REALM_CORE_PATH), `Expected Realm Core at '${REALM_CORE_PATH}'`);
+      const { CMAKE_PATH: cmakePath = execSync("which cmake", { encoding: "utf8" }).trim() } = env;
+      const platforms = validatePlatforms(rawPlatforms);
+
+      ensureBuildDirectory();
+
+      group("Generate Xcode project", () => {
+        if (platforms.length > 0) {
+          generateXcodeProject({ cmakePath });
+        } else {
+          console.log("Skipped generating Xcode project (no platforms to build for)");
+        }
+      });
+
+      const producedArchivePaths = platforms.map((platform) => {
+        return group(`Build ${platform} / ${configuration}`, () => {
+          return buildFramework({ platform, configuration });
+        });
+      });
+
+      group(`Collecting headers`, () => {
+        if (skipCollectingHeaders) {
+          console.log("Skipped collecting headers");
+        } else {
+          collectHeaders();
+        }
+      });
+
+      // Collect the absolute paths of all available archives in the build directory
+      // This allows for splitting up the invocation of the build command
+      const archivePaths = collectArchivePaths();
+      for (const producedArchivePath of producedArchivePaths) {
+        // As a sanity check, we ensure all produced archives are passed as input for the creation of the xcframework
+        assert(
+          archivePaths.includes(producedArchivePath),
+          `Expected produced archive '${producedArchivePath}' to be one of the collected paths`,
+        );
+      }
+
+      group(`Creating xcframework`, () => {
+        if (skipCreatingXcframework) {
+          console.log("Skipped creating Xcframework");
+        } else {
+          createXCFramework({ archivePaths });
+        }
+      });
+
+      console.log("Great success! 🥳");
     }),
   );
 
