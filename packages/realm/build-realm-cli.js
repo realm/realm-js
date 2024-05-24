d2f821fbd4ef85bab6d2d894662deb61873e30fc
Kræn Hansen <kraen.hansen@mongodb.com>
2024-05-24 14:34:06 +0200

    Getting rid of the build-realm-cli

--- packages/realm/build-realm-cli.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-// The main purpose of this file is provide the shebang on the first line of this file
-// and persist the executable permission.
-const { program } = require("./dist/scripts/build");
-program.parse();
