a0d2a9483fc47da39b8e6a1eed7a1be0c8ea0ce8
Kræn Hansen <kraen.hansen@mongodb.com>
2024-06-24 11:55:05 +0200

    Scaffolding for the "browser" platform

---
+++ packages/realm/src/platform/browser/index.ts
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2024 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+// import "./binding";
+// import "./fs";
+// import "./device-info";
+// import "./sync-proxy-config";
+// import "./custom-inspect";
+// import "./garbage-collection";
+
+console.log("Hello from Realm!");
+
+import { Realm } from "../../Realm";
+export = Realm;
