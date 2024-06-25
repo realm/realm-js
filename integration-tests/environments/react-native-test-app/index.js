21ab681a9d1ed333b84e80656635341c3be2686e
Kræn Hansen <kraen.hansen@mongodb.com>
2024-06-25 17:02:04 +0200

    Enabling encoding polyfill again

--- integration-tests/environments/react-native-test-app/index.js
+++ integration-tests/environments/react-native-test-app/index.js
@@ -17,11 +17,11 @@
 ////////////////////////////////////////////////////////////////////////////
 
 import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
-// import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
+import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
 // import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
 
 polyfillReadableStream();
-// polyfillEncoding();
+polyfillEncoding();
 // polyfillFetch();
 
 import { AppRegistry } from "react-native";
