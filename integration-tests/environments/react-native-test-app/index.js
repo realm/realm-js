7b93afa309fd638a4d74c9faa056c501518dbddd
Kræn Hansen <kraen.hansen@mongodb.com>
2024-06-25 16:06:53 +0200

    Disabled applying polyfills

--- integration-tests/environments/react-native-test-app/index.js
+++ integration-tests/environments/react-native-test-app/index.js
@@ -16,13 +16,13 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
-import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
-import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
+// import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
+// import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
+// import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
 
-polyfillReadableStream();
-polyfillEncoding();
-polyfillFetch();
+// polyfillReadableStream();
+// polyfillEncoding();
+// polyfillFetch();
 
 import { AppRegistry } from "react-native";
 import App from "./App";
