d7a95733ee3978f992ce3bcea1974e1096565fbb
LJ <81748770+elle-j@users.noreply.github.com>
2024-07-10 08:21:21 +0200

    Update schema prop name.

--- integration-tests/tests/src/tests/observable.ts
+++ integration-tests/tests/src/tests/observable.ts
@@ -694,7 +694,7 @@ describe("Observable", () => {
             age: "int?",
             friends: "Person[]",
             bestFriend: "Person?",
-            address: "EmbeddedAddress?",
+            embeddedAddress: "EmbeddedAddress?",
           },
         },
         {
