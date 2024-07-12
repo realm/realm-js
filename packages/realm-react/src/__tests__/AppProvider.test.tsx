2217f791327a9294c25f8aed4c895d4175569187
Gagik Amaryan <gagik.amaryan@mongodb.com>
2024-07-12 08:52:00 +0200

    Add AppProvider tests

--- packages/realm-react/src/__tests__/AppProvider.test.tsx
+++ packages/realm-react/src/__tests__/AppProvider.test.tsx
@@ -21,6 +21,7 @@ import { Button, Text, View } from "react-native";
 import { act, fireEvent, render, renderHook, waitFor } from "@testing-library/react-native";
 
 import { AppProvider, useApp } from "../AppProvider";
+import { Realm } from "realm";
 
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
@@ -42,72 +43,124 @@ describe("AppProvider", () => {
     );
   });
 
-  it("handle state changes to its configuration", async () => {
-    const AppComponent = () => {
-      const app = useApp();
-      return <Text testID="appId">{app.id}</Text>;
-    };
-    const App = () => {
-      const [id, setId] = useState("someId");
-      return (
-        <>
-          <View testID="firstRealmProvider">
-            <AppProvider id={id}>
-              <AppComponent />
-            </AppProvider>
-          </View>
-          <Button testID="changeId" title="change app id" onPress={() => setId("newId")} />
-        </>
-      );
-    };
-    const { getByTestId } = render(<App />);
-    const schemaNameContainer = await waitFor(() => getByTestId("appId"));
-    const changeSchemaButton = getByTestId("changeId");
+  describe("with an App Configuration", function () {
+    it("handle state changes to its configuration", async () => {
+      const AppComponent = () => {
+        const app = useApp();
+        return <Text testID="appId">{app.id}</Text>;
+      };
+      const App = () => {
+        const [id, setId] = useState("someId");
+        return (
+          <>
+            <View testID="firstRealmProvider">
+              <AppProvider id={id}>
+                <AppComponent />
+              </AppProvider>
+            </View>
+            <Button testID="changeId" title="change app id" onPress={() => setId("newId")} />
+          </>
+        );
+      };
+      const { getByTestId } = render(<App />);
+      const schemaNameContainer = await waitFor(() => getByTestId("appId"));
+      const changeSchemaButton = getByTestId("changeId");
 
-    expect(schemaNameContainer).toHaveTextContent("someId");
+      expect(schemaNameContainer).toHaveTextContent("someId");
 
-    await act(async () => {
-      fireEvent.press(changeSchemaButton);
-      await new Promise<void>((resolve) => setTimeout(resolve, 0));
+      await act(async () => {
+        fireEvent.press(changeSchemaButton);
+        await new Promise<void>((resolve) => setTimeout(resolve, 0));
+      });
+
+      // Changing the realm provider configuration will cause a remount
+      // of the child component.  Therefore it must be retrieved again
+      const newSchemaNameContainer = getByTestId("appId");
+
+      expect(newSchemaNameContainer).toHaveTextContent("newId");
     });
 
-    // Changing the realm provider configuration will cause a remount
-    // of the child component.  Therefore it must be retrieved again
-    const newSchemaNameContainer = getByTestId("appId");
+    it("can access realm through realmRef as a forwarded ref", async () => {
+      const AppComponent = () => {
+        const app = useApp();
+        return <Text testID="appId">{app.id}</Text>;
+      };
+      const App = () => {
+        const [id, setId] = useState("");
+        const appRef = useRef<Realm.App | null>(null);
+        return (
+          <>
+            <View testID="firstRealmProvider">
+              <AppProvider id={"testId"} appRef={appRef}>
+                <AppComponent />
+              </AppProvider>
+            </View>
+            <Button testID="toggleAppRef" title="toggle app ref" onPress={() => setId(appRef?.current?.id ?? "")} />
+            {appRef.current && <Text testID="appIdText">{id}</Text>}
+          </>
+        );
+      };
+      const { getByTestId } = render(<App />);
+      const toggleAppRef = getByTestId("toggleAppRef");
 
-    expect(newSchemaNameContainer).toHaveTextContent("newId");
+      // Wait a tick for the app reference to be set by the provider.  Then force a rerender.
+      await act(async () => {
+        await new Promise<void>((resolve) => setTimeout(resolve, 0));
+        fireEvent.press(toggleAppRef);
+      });
+
+      const appIdText = await waitFor(() => getByTestId("appIdText"));
+
+      expect(appIdText).toHaveTextContent("testId");
+    });
   });
-  it("can access realm through realmRef as a forwarded ref", async () => {
-    const AppComponent = () => {
-      const app = useApp();
-      return <Text testID="appId">{app.id}</Text>;
-    };
-    const App = () => {
-      const [id, setId] = useState("");
-      const appRef = useRef<Realm.App | null>(null);
-      return (
-        <>
-          <View testID="firstRealmProvider">
-            <AppProvider id={"testId"} appRef={appRef}>
-              <AppComponent />
-            </AppProvider>
-          </View>
-          <Button testID="toggleAppRef" title="toggle app ref" onPress={() => setId(appRef?.current?.id ?? "")} />
-          {appRef.current && <Text testID="appIdText">{id}</Text>}
-        </>
-      );
-    };
-    const { getByTestId } = render(<App />);
-    const toggleAppRef = getByTestId("toggleAppRef");
 
-    // Wait a tick for the app reference to be set by the provider.  Then force a rerender.
-    await act(async () => {
-      await new Promise<void>((resolve) => setTimeout(resolve, 0));
-      fireEvent.press(toggleAppRef);
+  describe("with an App instance", function () {
+    it("handle passing an app and changing it", async () => {
+      const appInstanceId = "appInstanceId";
+      const differentAppInstanceId = "differentAppInstanceId";
+      const appInstance = new Realm.App({
+        id: appInstanceId,
+      });
+
+      const AppComponent = () => {
+        const app = useApp();
+        return <Text testID="appId">{app.id}</Text>;
+      };
+
+      const App = () => {
+        const [currentApp, setCurrentApp] = useState(appInstance);
+        return (
+          <>
+            <View testID="firstRealmProvider">
+              <AppProvider appInstance={currentApp}>
+                <AppComponent />
+              </AppProvider>
+            </View>
+            <Button
+              testID="changeId"
+              title="change app id"
+              onPress={() => setCurrentApp(new Realm.App({ id: differentAppInstanceId }))}
+            />
+          </>
+        );
+      };
+      const { getByTestId } = render(<App />);
+      const schemaNameContainer = await waitFor(() => getByTestId("appId"));
+      const changeSchemaButton = getByTestId("changeId");
+
+      expect(schemaNameContainer).toHaveTextContent(appInstanceId);
+
+      await act(async () => {
+        fireEvent.press(changeSchemaButton);
+        await new Promise<void>((resolve) => setTimeout(resolve, 0));
+      });
+
+      // Changing the realm provider configuration will cause a remount
+      // of the child component.  Therefore it must be retrieved again
+      const newSchemaNameContainer = getByTestId("appId");
+
+      expect(newSchemaNameContainer).toHaveTextContent(differentAppInstanceId);
     });
-
-    const appIdText = await waitFor(() => getByTestId("appIdText"));
-
-    expect(appIdText).toHaveTextContent("testId");
   });
 });
