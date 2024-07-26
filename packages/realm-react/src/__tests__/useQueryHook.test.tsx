3a01e35f466359dd2d61441ef5b0867af066ed0a
Mike Hernas <m@hern.as>
2024-07-26 22:39:25 +0200

    Rever package.json change and fix test name

--- packages/realm-react/src/__tests__/useQueryHook.test.tsx
+++ packages/realm-react/src/__tests__/useQueryHook.test.tsx
@@ -102,7 +102,7 @@ describe("useQuery", () => {
     expect(result.current.length).toBe(3);
   });
 
-  describe("passing an type as 3rd argument", () => {
+  describe("passing function as 1st argument, deps as 2nd and options as 3rd", () => {
     it("can filter objects via a 'query' property", () => {
       const { result, renders } = profileHook(() =>
         useQuery<IDog>((dogs) => dogs.filtered("age > 10"), [], { type: "dog" }),
