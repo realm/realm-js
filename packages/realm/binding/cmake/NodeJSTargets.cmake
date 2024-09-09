53bf552cc74b0bab9354788a839dcda691bf9437
Kræn Hansen <kraen.hansen@mongodb.com>
2024-09-09 12:35:13 +0200

    Removed mention of ZLIB::ZLIB from NodeJSTargets.cmake

--- packages/realm/binding/cmake/NodeJSTargets.cmake
+++ packages/realm/binding/cmake/NodeJSTargets.cmake
@@ -10,9 +10,3 @@ string(REPLACE "\"" "" NODE_ADDON_API_DIR ${NODE_ADDON_API_DIR})
 add_library(NodeJS INTERFACE)
 target_include_directories(NodeJS INTERFACE ${CMAKE_JS_INC} ${NODE_ADDON_API_DIR})
 target_link_libraries(NodeJS INTERFACE ${CMAKE_JS_LIB})
-
-# Node.js exports the zlib headers and symbols, so use those
-# realm-core depends on the ZLIB::ZLIB target - if we don't define it ourselves it'll search globally for a zlib library
-if(NOT WIN32)
-  add_library(ZLIB::ZLIB ALIAS NodeJS)
-endif()
