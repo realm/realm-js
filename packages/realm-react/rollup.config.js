18187f3358f37912bb50a96f0bc08320c9ba2edc
Kræn Hansen <kraen.hansen@mongodb.com>
2025-08-10 20:35:11 +0200

    Upgrade GHA workflows to use Node v22 (#7030)
    * Bump Node version in GHA workflows
    
    * Use "with" instead of "assert"

--- packages/realm-react/rollup.config.js
+++ packages/realm-react/rollup.config.js
@@ -20,7 +20,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 export default [
   {
